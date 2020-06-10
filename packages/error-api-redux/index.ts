5451a889b76ca87c674c5c4d66fa145f24b5f345
Tor Røneid <tor.roneid@nav.no>
2020-06-10 11:35:49 +0200

    Feature/panel refaktorering (#1184)
    * NOJIRA Endra oppsett for fakta og prosess-steg panela + gjort om fleire pakker til typescript

---
+++ packages/error-api-redux/index.ts
@@ -0,0 +1,3 @@
+import errorHandler from './src/ErrorHandler';
+
+export default errorHandler;
---
+++ packages/form/index.ts
@@ -0,0 +1,15 @@
+export { default as CheckboxField } from './src/CheckboxField';
+export { default as DatepickerField } from './src/DatepickerField';
+export { default as DecimalField } from './src/DecimalField';
+export { default as InputField } from './src/InputField';
+export { default as NavFieldGroup } from './src/NavFieldGroup';
+export { default as RadioGroupField } from './src/RadioGroupField';
+export { default as RadioOption } from './src/RadioOption';
+export { default as SelectField } from './src/SelectField';
+export { default as TextAreaField } from './src/TextAreaField';
+export { default as PeriodpickerField } from './src/PeriodpickerField';
+
+export {
+  behandlingForm, behandlingFormValueSelector, hasBehandlingFormErrorsOfType, isBehandlingFormDirty, getBehandlingFormName,
+  isBehandlingFormSubmitting, getBehandlingFormValues, getBehandlingFormInitialValues, getBehandlingFormSyncErrors, getBehandlingFormPrefix,
+} from './src/behandlingForm';
