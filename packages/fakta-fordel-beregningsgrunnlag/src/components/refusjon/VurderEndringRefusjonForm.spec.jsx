mrsladek/fpsak-frontend

TFP-3535: Frontend nytt steg i beregning

packages/fakta-fordel-beregningsgrunnlag/src/components/refusjon/VurderEndringRefusjonForm.spec.jsx
@@ -1,0 +1,104 @@
+import { expect } from 'chai';
+import { transformValues } from './VurderEndringRefusjonForm';
+import { lagNøkkel } from './VurderEndringRefusjonRad';
+
+const BEGRUNNELSE_FIELD = 'VURDER_REFUSJON_BERGRUNN_BEGRUNNELSE';
+
+const lagAndel = (agNavn, agOrgnr, arbId, dato) => ({
+  aktivitetStatus: {
+    kode: 'AT',
+  },
+  nyttRefusjonskravFra: dato,
+  arbeidsgiverId: {
+    arbeidsgiverOrgnr: agOrgnr,
+  },
+  arbeidsgiverNavn: agNavn,
+  internArbeidsforholdRef: arbId,
+  tidligereUtbetalinger: [],
+});
+
+const lagBG = (andeler) => ({
+  refusjonTilVurdering: {
+    andeler,
+  },
+});
+
+describe('<VurderEndringRefusjonForm>', () => {
+  it('Skal utføre transformeValues korrekt med et ref.krav hos en AG', () => {
+    const andel = lagAndel('Biri bakeri og saueoppdrett', '999999999', undefined, '01.09.2018');
+    const values = {};
+    values[lagNøkkel(andel)] = '01.10.2018';
+    values[BEGRUNNELSE_FIELD] = 'En begrunnelse';
+    const transformed = transformValues(values, lagBG([andel]));
+    const expected = {
+      kode: '5059',
+      begrunnelse: 'En begrunnelse',
+      fastsatteAndeler: [
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: undefined,
+          fastsattRefusjonFra: '01.10.2018',
+        },
+      ],
+    };
+    expect(transformed).to.deep.equal(expected);
+  });
+
+  it('Skal utføre transformeValues korrekt med to ref.krav hos en AG', () => {
+    const andel1 = lagAndel('Biri bakeri og saueoppdrett', '999999999', 'REF-1', '01.09.2018');
+    const andel2 = lagAndel('Biri bakeri og saueoppdrett', '999999999', 'REF-2', '01.10.2018');
+    const values = {};
+    values[lagNøkkel(andel1)] = '01.10.2018';
+    values[lagNøkkel(andel2)] = '01.11.2018';
+    values[BEGRUNNELSE_FIELD] = 'En begrunnelse';
+    const transformed = transformValues(values, lagBG([andel1, andel2]));
+    const expected = {
+      kode: '5059',
+      begrunnelse: 'En begrunnelse',
+      fastsatteAndeler: [
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-1',
+          fastsattRefusjonFra: '01.10.2018',
+        },
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-2',
+          fastsattRefusjonFra: '01.11.2018',
+        },
+      ],
+    };
+    expect(transformed).to.deep.equal(expected);
+  });
+  it('Skal utføre transformeValues korrekt med to ref.krav hos forskjellig AG', () => {
+    const andel1 = lagAndel('Biri bakeri og saueoppdrett', '999999999', 'REF-1', '01.09.2018');
+    const andel2 = lagAndel('Biri bakeri og griseoppdrett', '999999998', 'REF-1', '01.10.2018');
+    const values = {};
+    values[lagNøkkel(andel1)] = '01.10.2018';
+    values[lagNøkkel(andel2)] = '01.11.2018';
+    values[BEGRUNNELSE_FIELD] = 'En begrunnelse';
+    const transformed = transformValues(values, lagBG([andel1, andel2]));
+    const expected = {
+      kode: '5059',
+      begrunnelse: 'En begrunnelse',
+      fastsatteAndeler: [
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-1',
+          fastsattRefusjonFra: '01.10.2018',
+        },
+        {
+          arbeidsgiverOrgnr: '999999998',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-1',
+          fastsattRefusjonFra: '01.11.2018',
+        },
+      ],
+    };
+    expect(transformed).to.deep.equal(expected);
+  });
+});

packages/fakta-fordel-beregningsgrunnlag/src/components/refusjon/VurderEndringRefusjonRad.jsx
@@ -1,0 +1,89 @@
+import React from 'react';
+import { dateFormat, hasValidDate, required } from '@fpsak-frontend/utils';
+import { Column, Row } from 'nav-frontend-grid';
+import { DatepickerField } from '@fpsak-frontend/form';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import { Normaltekst } from 'nav-frontend-typografi';
+import { refusjonAndelTilVurderingPropType } from '../../propTypes/beregningsgrunnlagPropType';
+import styles from './vurderEndringRefusjonRad.less';
+
+const visningsnavn = (andel) => {
+  if (andel.arbeidsgiverNavn) {
+    return andel.arbeidsgiverNavn;
+  } if (andel.arbeidsgiverId && andel.arbeidsgiverId.arbeidsgiverOrgnr) {
+    return andel.arbeidsgiverId.arbeidsgiverOrgnr;
+  } if (andel.arbeidsgiverId && andel.arbeidsgiverId.arbeidsgiverAktørId) {
+    return andel.arbeidsgiverId.arbeidsgiverAktørId;
+  }
+  return undefined;
+};
+
+const FIELD_KEY = 'REFUSJON_ENDRING_DATO';
+
+export const lagNøkkel = (andel) => `${FIELD_KEY}${andel.arbeidsgiverNavn}${andel.aktivitetStatus.kode}${andel.internArbeidsforholdRef}
+${andel.arbeidsgiverId.arbeidsgiverOrgnr}${andel.arbeidsgiverId.arbeidsgiverAktørId}`;
+
+export const VurderEndringRefusjonRad = ({
+  refusjonAndel,
+  readOnly,
+}) => {
+  if (!refusjonAndel) {
+    return null;
+  }
+  const navn = visningsnavn(refusjonAndel);
+  return (
+    <>
+      <Row>
+        <Column xs="8">
+          <FormattedMessage
+            id="BeregningInfoPanel.RefusjonBG.Arbeidsgiver"
+            values={{
+              ag: navn,
+              dato: dateFormat(refusjonAndel.nyttRefusjonskravFra),
+              b: (...chunks) => <b>{chunks}</b>,
+            }}
+          />
+        </Column>
+      </Row>
+      <Row>
+        <Column xs="4">
+          <Normaltekst className={styles.marginTopp}>
+            <FormattedMessage
+              id="BeregningInfoPanel.RefusjonBG.RefusjonFra"
+            />
+          </Normaltekst>
+        </Column>
+        <Column xs="4">
+          <DatepickerField
+            name={lagNøkkel(refusjonAndel)}
+            readOnly={readOnly}
+            validate={[required, hasValidDate]}
+            isEdited={!!refusjonAndel.fastsattNyttRefusjonskravFra}
+          />
+        </Column>
+      </Row>
+    </>
+  );
+};
+
+VurderEndringRefusjonRad.buildInitialValues = (refusjonAndel) => (refusjonAndel.fastsattNyttRefusjonskravFra);
+
+VurderEndringRefusjonRad.transformValues = (values, andel) => {
+  const nøkkel = lagNøkkel(andel);
+  const fastsattDato = values[nøkkel];
+  return {
+    arbeidsgiverOrgnr: andel.arbeidsgiverId.arbeidsgiverOrgnr,
+    arbeidsgiverAktoerId: andel.arbeidsgiverId.arbeidsgiverAktørId,
+    internArbeidsforholdRef: andel.internArbeidsforholdRef,
+    fastsattRefusjonFra: fastsattDato,
+  };
+};
+
+VurderEndringRefusjonRad.propTypes = {
+  refusjonAndel: refusjonAndelTilVurderingPropType,
+  readOnly: PropTypes.bool.isRequired,
+};
+
+export default VurderEndringRefusjonRad;

packages/fakta-fordel-beregningsgrunnlag/src/propTypes/beregningsgrunnlagPropType.jsx
@@ -72,6 +72,30 @@ const faktaOmFordelingPropType = PropTypes.shape({
   fordelBeregningsgrunnlag: fordelBeregningsgrunnlagPropType,
 });
 
+const tidligereUtbetalingPropType = PropTypes.shape({
+  fom: PropTypes.string,
+  tom: PropTypes.string,
+  erTildeltRefusjon: PropTypes.bool,
+});
+
+export const refusjonAndelTilVurderingPropType = PropTypes.shape({
+  aktivitetStatus: kodeverkObjektPropType,
+  nyttRefusjonskravFra: PropTypes.string,
+  fastsattNyttRefusjonskravFra: PropTypes.string,
+  arbeidsgiverId: PropTypes.shape({
+    arbeidsgiverOrgnr: PropTypes.string,
+    arbeidsgiverAktørId: PropTypes.string,
+  }),
+  arbeidsgiverNavn: PropTypes.string,
+  internArbeidsforholdRef: PropTypes.string,
+  eksternArbeidsforholdRef: PropTypes.string,
+  tidligereUtbetalinger: PropTypes.arrayOf(tidligereUtbetalingPropType),
+});
+
+const refusjonTilVurderingPropType = PropTypes.shape({
+  andeler: PropTypes.arrayOf(refusjonAndelTilVurderingPropType),
+});
+
 const beregningsgrunnlagPropType = PropTypes.shape({
   aktivitetStatus: PropTypes.arrayOf(PropTypes.shape({
     aktivitetStatus: kodeverkObjektPropType,
@@ -119,6 +143,7 @@ const beregningsgrunnlagPropType = PropTypes.shape({
   })),
   skjaeringstidspunktBeregning: PropTypes.string,
   faktaOmFordeling: faktaOmFordelingPropType,
+  refusjonTilVurdering: refusjonTilVurderingPropType,
 });
 
 export default beregningsgrunnlagPropType;

navikt/fp-frontend

packages/fakta-fordel-beregningsgrunnlag/src/components/refusjon/VurderEndringRefusjonForm.spec.jsx
@@ -1,0 +1,104 @@
+import { expect } from 'chai';
+import { transformValues } from './VurderEndringRefusjonForm';
+import { lagNøkkel } from './VurderEndringRefusjonRad';
+
+const BEGRUNNELSE_FIELD = 'VURDER_REFUSJON_BERGRUNN_BEGRUNNELSE';
+
+const lagAndel = (agNavn, agOrgnr, arbId, dato) => ({
+  aktivitetStatus: {
+    kode: 'AT',
+  },
+  nyttRefusjonskravFom: dato,
+  arbeidsgiverId: {
+    arbeidsgiverOrgnr: agOrgnr,
+  },
+  arbeidsgiverNavn: agNavn,
+  internArbeidsforholdRef: arbId,
+  tidligereUtbetalinger: [],
+});
+
+const lagBG = (andeler) => ({
+  refusjonTilVurdering: {
+    andeler,
+  },
+});
+
+describe('<VurderEndringRefusjonForm>', () => {
+  it('Skal utføre transformeValues korrekt med et ref.krav hos en AG', () => {
+    const andel = lagAndel('Biri bakeri og saueoppdrett', '999999999', undefined, '01.09.2018');
+    const values = {};
+    values[lagNøkkel(andel)] = '01.10.2018';
+    values[BEGRUNNELSE_FIELD] = 'En begrunnelse';
+    const transformed = transformValues(values, lagBG([andel]));
+    const expected = {
+      kode: '5059',
+      begrunnelse: 'En begrunnelse',
+      fastsatteAndeler: [
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: undefined,
+          fastsattRefusjonFra: '01.10.2018',
+        },
+      ],
+    };
+    expect(transformed).to.deep.equal(expected);
+  });
+
+  it('Skal utføre transformeValues korrekt med to ref.krav hos en AG', () => {
+    const andel1 = lagAndel('Biri bakeri og saueoppdrett', '999999999', 'REF-1', '01.09.2018');
+    const andel2 = lagAndel('Biri bakeri og saueoppdrett', '999999999', 'REF-2', '01.10.2018');
+    const values = {};
+    values[lagNøkkel(andel1)] = '01.10.2018';
+    values[lagNøkkel(andel2)] = '01.11.2018';
+    values[BEGRUNNELSE_FIELD] = 'En begrunnelse';
+    const transformed = transformValues(values, lagBG([andel1, andel2]));
+    const expected = {
+      kode: '5059',
+      begrunnelse: 'En begrunnelse',
+      fastsatteAndeler: [
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-1',
+          fastsattRefusjonFra: '01.10.2018',
+        },
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-2',
+          fastsattRefusjonFra: '01.11.2018',
+        },
+      ],
+    };
+    expect(transformed).to.deep.equal(expected);
+  });
+  it('Skal utføre transformeValues korrekt med to ref.krav hos forskjellig AG', () => {
+    const andel1 = lagAndel('Biri bakeri og saueoppdrett', '999999999', 'REF-1', '01.09.2018');
+    const andel2 = lagAndel('Biri bakeri og griseoppdrett', '999999998', 'REF-1', '01.10.2018');
+    const values = {};
+    values[lagNøkkel(andel1)] = '01.10.2018';
+    values[lagNøkkel(andel2)] = '01.11.2018';
+    values[BEGRUNNELSE_FIELD] = 'En begrunnelse';
+    const transformed = transformValues(values, lagBG([andel1, andel2]));
+    const expected = {
+      kode: '5059',
+      begrunnelse: 'En begrunnelse',
+      fastsatteAndeler: [
+        {
+          arbeidsgiverOrgnr: '999999999',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-1',
+          fastsattRefusjonFra: '01.10.2018',
+        },
+        {
+          arbeidsgiverOrgnr: '999999998',
+          arbeidsgiverAktoerId: undefined,
+          internArbeidsforholdRef: 'REF-1',
+          fastsattRefusjonFra: '01.11.2018',
+        },
+      ],
+    };
+    expect(transformed).to.deep.equal(expected);
+  });
+});

packages/fakta-fordel-beregningsgrunnlag/src/components/refusjon/VurderEndringRefusjonRad.jsx
@@ -1,0 +1,93 @@
+import React from 'react';
+import {
+  dateFormat, hasValidDate, required, dateAfterOrEqual,
+} from '@fpsak-frontend/utils';
+import { Column, Row } from 'nav-frontend-grid';
+import { DatepickerField } from '@fpsak-frontend/form';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import { Normaltekst } from 'nav-frontend-typografi';
+import { refusjonAndelTilVurderingPropType } from '../../propTypes/beregningsgrunnlagPropType';
+import styles from './vurderEndringRefusjonRad.less';
+
+const visningsnavn = (andel) => {
+  if (andel.arbeidsgiverNavn) {
+    return andel.arbeidsgiverNavn;
+  } if (andel.arbeidsgiverId && andel.arbeidsgiverId.arbeidsgiverOrgnr) {
+    return andel.arbeidsgiverId.arbeidsgiverOrgnr;
+  } if (andel.arbeidsgiverId && andel.arbeidsgiverId.arbeidsgiverAktørId) {
+    return andel.arbeidsgiverId.arbeidsgiverAktørId;
+  }
+  return undefined;
+};
+
+const FIELD_KEY = 'REFUSJON_ENDRING_DATO';
+
+export const lagNøkkel = (andel) => `${FIELD_KEY}${andel.arbeidsgiverNavn}${andel.aktivitetStatus.kode}${andel.internArbeidsforholdRef}
+${andel.arbeidsgiverId.arbeidsgiverOrgnr}${andel.arbeidsgiverId.arbeidsgiverAktørId}`;
+
+export const VurderEndringRefusjonRad = ({
+  refusjonAndel,
+  readOnly,
+  skjæringstidspunkt,
+}) => {
+  if (!refusjonAndel) {
+    return null;
+  }
+  const navn = visningsnavn(refusjonAndel);
+  return (
+    <>
+      <Row>
+        <Column xs="8">
+          <FormattedMessage
+            id="BeregningInfoPanel.RefusjonBG.Arbeidsgiver"
+            values={{
+              ag: navn,
+              dato: dateFormat(refusjonAndel.nyttRefusjonskravFom),
+              b: (...chunks) => <b>{chunks}</b>,
+            }}
+          />
+        </Column>
+      </Row>
+      <Row>
+        <Column xs="4">
+          <Normaltekst className={styles.marginTopp}>
+            <FormattedMessage
+              id="BeregningInfoPanel.RefusjonBG.RefusjonFra"
+            />
+          </Normaltekst>
+        </Column>
+        <Column xs="4">
+          <DatepickerField
+            name={lagNøkkel(refusjonAndel)}
+            readOnly={readOnly}
+            validate={[required, hasValidDate, dateAfterOrEqual(skjæringstidspunkt)]}
+            isEdited={!!refusjonAndel.fastsattNyttRefusjonskravFom}
+          />
+        </Column>
+      </Row>
+    </>
+  );
+};
+
+VurderEndringRefusjonRad.buildInitialValues = (refusjonAndel) => (refusjonAndel.fastsattNyttRefusjonskravFom);
+
+VurderEndringRefusjonRad.transformValues = (values, andel) => {
+  const nøkkel = lagNøkkel(andel);
+  const fastsattDato = values[nøkkel];
+  return {
+    arbeidsgiverOrgnr: andel.arbeidsgiverId.arbeidsgiverOrgnr,
+    arbeidsgiverAktoerId: andel.arbeidsgiverId.arbeidsgiverAktørId,
+    internArbeidsforholdRef: andel.internArbeidsforholdRef,
+    fastsattRefusjonFom: fastsattDato,
+  };
+};
+
+VurderEndringRefusjonRad.propTypes = {
+  refusjonAndel: refusjonAndelTilVurderingPropType,
+  readOnly: PropTypes.bool.isRequired,
+  skjæringstidspunkt: PropTypes.string.isRequired,
+};
+
+export default VurderEndringRefusjonRad;

packages/fakta-fordel-beregningsgrunnlag/src/propTypes/beregningsgrunnlagPropType.jsx
@@ -72,6 +72,30 @@ const faktaOmFordelingPropType = PropTypes.shape({
   fordelBeregningsgrunnlag: fordelBeregningsgrunnlagPropType,
 });
 
+const tidligereUtbetalingPropType = PropTypes.shape({
+  fom: PropTypes.string,
+  tom: PropTypes.string,
+  erTildeltRefusjon: PropTypes.bool,
+});
+
+export const refusjonAndelTilVurderingPropType = PropTypes.shape({
+  aktivitetStatus: kodeverkObjektPropType,
+  nyttRefusjonskravFom: PropTypes.string,
+  fastsattNyttRefusjonskravFom: PropTypes.string,
+  arbeidsgiverId: PropTypes.shape({
+    arbeidsgiverOrgnr: PropTypes.string,
+    arbeidsgiverAktørId: PropTypes.string,
+  }),
+  arbeidsgiverNavn: PropTypes.string,
+  internArbeidsforholdRef: PropTypes.string,
+  eksternArbeidsforholdRef: PropTypes.string,
+  tidligereUtbetalinger: PropTypes.arrayOf(tidligereUtbetalingPropType),
+});
+
+const refusjonTilVurderingPropType = PropTypes.shape({
+  andeler: PropTypes.arrayOf(refusjonAndelTilVurderingPropType),
+});
+
 const beregningsgrunnlagPropType = PropTypes.shape({
   aktivitetStatus: PropTypes.arrayOf(PropTypes.shape({
     aktivitetStatus: kodeverkObjektPropType,
@@ -119,6 +143,7 @@ const beregningsgrunnlagPropType = PropTypes.shape({
   })),
   skjaeringstidspunktBeregning: PropTypes.string,
   faktaOmFordeling: faktaOmFordelingPropType,
+  refusjonTilVurdering: refusjonTilVurderingPropType,
 });
 
 export default beregningsgrunnlagPropType;