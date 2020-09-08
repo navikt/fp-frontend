deed39e38956388bbb9b7cf1f9a60874794180fb
Petter Ekern <petter.ekern@nav.no>
2020-09-08 11:53:25 +0200

    FIX: Legger til ny overskrift i tabell

--- packages/fakta-fordel-beregningsgrunnlag/src/components/refusjon/TidligereUtbetalingRad.jsx
+++ packages/fakta-fordel-beregningsgrunnlag/src/components/refusjon/TidligereUtbetalingRad.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Normaltekst } from 'nav-frontend-typografi';
-import { Table, TableColumn, TableRow } from '@fpsak-frontend/shared-components';
+import { TableColumn, TableRow } from '@fpsak-frontend/shared-components';
 import { dateFormat, TIDENES_ENDE } from '@fpsak-frontend/utils';
+import { Column, Row } from 'nav-frontend-grid';
 
 import { refusjonAndelTilVurderingPropType } from '../../propTypes/beregningsgrunnlagPropType';
-import styles from './tidligereUtbetalingRad.less';
 
 const visningsnavn = (andel) => {
   if (andel.arbeidsgiverNavn) {
@@ -18,17 +18,17 @@ const visningsnavn = (andel) => {
   return undefined;
 };
 
-const tidligereUtbetaling = (utbetaling) => {
-  if (!utbetaling) {
-    return undefined;
-  }
-  if (utbetaling.erTildeltRefusjon) {
-    return visningsnavn(andel);
-  }
-  return <FormattedMessage id="BeregningInfoPanel.RefusjonBG.Direkteutbetaling" />;
-};
+const utbetalingTil = (utbetalinger, andelsnavn) => utbetalinger.map((utbetaling) => (
+  <Row>
+    <Column>
+      {utbetaling && utbetaling.erTildeltRefusjon
+        ? <Normaltekst>{andelsnavn}</Normaltekst>
+        : <FormattedMessage id="BeregningInfoPanel.RefusjonBG.Direkteutbetaling" />}
+    </Column>
+  </Row>
+));
 
-const tidligereUtbetalingDato = (utbetaling) => {
+const lagPeriode = (utbetaling) => {
   if (!utbetaling) {
     return undefined;
   }
@@ -41,6 +41,14 @@ const tidligereUtbetalingDato = (utbetaling) => {
   );
 };
 
+const perioder = (utbetalinger) => utbetalinger.map((utbetaling) => (
+  <Row>
+    <Column>
+      {lagPeriode(utbetaling)}
+    </Column>
+  </Row>
+));
+
 export const TidligereUtbetalingRad = ({
   refusjonAndel,
 }) => (
@@ -49,18 +57,10 @@ export const TidligereUtbetalingRad = ({
       {visningsnavn(refusjonAndel)}
     </TableColumn>
     <TableColumn>
-      <Table headerTextCodes={[]} noHover classNameTable={styles.utbetalingTabell}>
-        { refusjonAndel.tidligereUtbetalinger.map((utbetaling) => (
-          <TableRow key={utbetaling.fom + utbetaling.erTildeltRefusjon}>
-            <TableColumn>
-              <Normaltekst>{tidligereUtbetaling(utbetaling)}</Normaltekst>
-            </TableColumn>
-            <TableColumn>
-              <Normaltekst>{tidligereUtbetalingDato(utbetaling)}</Normaltekst>
-            </TableColumn>
-          </TableRow>
-        ))}
-      </Table>
+      {utbetalingTil(refusjonAndel.tidligereUtbetalinger, visningsnavn(refusjonAndel))}
+    </TableColumn>
+    <TableColumn>
+      {perioder(refusjonAndel.tidligereUtbetalinger)}
     </TableColumn>
   </TableRow>
 );
