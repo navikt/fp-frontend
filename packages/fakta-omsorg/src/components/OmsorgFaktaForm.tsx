167ec9181c7718bc15cd288460898e2793ea5f6b
Geir Roger Moen <geir.roger.moen@nav.no>
2020-10-26 09:29:05 +0100

    PFP-9259 Lint-fix

--- packages/fakta-omsorg/src/components/OmsorgFaktaForm.tsx
+++ packages/fakta-omsorg/src/components/OmsorgFaktaForm.tsx
@@ -73,10 +73,10 @@ const OmsorgFaktaForm: FunctionComponent<OwnProps> & StaticFunctions = ({
             ? <FormattedMessage id="OmsorgFaktaForm.OppgittAleneomsorg" />
             : (
               <FormattedMessage
-                  id="OmsorgFaktaForm.OppgittIkkeAleneomsorg"
-                  values={{
-                    b: (chunks: any) => <b>{chunks}</b>,
-                  }}
+                id="OmsorgFaktaForm.OppgittIkkeAleneomsorg"
+                values={{
+                  b: (chunks: any) => <b>{chunks}</b>,
+                }}
               />
             )}
         </Normaltekst>
@@ -108,10 +108,10 @@ const OmsorgFaktaForm: FunctionComponent<OwnProps> & StaticFunctions = ({
             ? <FormattedMessage id="OmsorgFaktaForm.OppgittOmsorg" />
             : (
               <FormattedMessage
-                  id="OmsorgFaktaForm.OppgittIkkeOmsorg"
-                  values={{
-                    b: (chunks: any) => <b>{chunks}</b>,
-                  }}
+                id="OmsorgFaktaForm.OppgittIkkeOmsorg"
+                values={{
+                  b: (chunks: any) => <b>{chunks}</b>,
+                }}
               />
             )}
         </Normaltekst>
