9914dedd9c737faa4c1f93e33ff35a9932bdfb6d
tor-nav <tor.roneid@nav.no>
2020-10-27 12:36:34 +0100

    FIX div

--- packages/storybook/stories/prosess/AvregningProsessIndex.stories.tsx
+++ packages/storybook/stories/prosess/AvregningProsessIndex.stories.tsx
@@ -4,6 +4,7 @@ import {
   withKnobs, object, boolean,
 } from '@storybook/addon-knobs';
 
+import mottakerTyper from '@fpsak-frontend/kodeverk/src/mottakerTyper';
 import tilbakekrevingVidereBehandling from '@fpsak-frontend/kodeverk/src/tilbakekrevingVidereBehandling';
 import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
@@ -40,8 +41,7 @@ const simuleringResultat = {
     ingenPerioderMedAvvik: false,
     perioderPerMottaker: [{
       mottakerType: {
-        kode: 'BRUKER',
-        navn: null,
+        kode: mottakerTyper.ARBG,
         kodeverk: 'MOTTAKER_TYPE',
       },
       mottakerIdentifikator: '123',
