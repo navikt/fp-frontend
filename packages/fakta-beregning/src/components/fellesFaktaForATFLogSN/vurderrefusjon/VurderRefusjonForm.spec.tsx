20a86faee72f95eba43e535c83b2c87db90f9379
Petter Ekern <petter.ekern@nav.no>
2020-12-14 21:20:36 +0100

    TFP-3962: Retter feil i vurder sent refusjonskrav form som brukte visningsnavn som fieldname

--- packages/fakta-beregning/src/components/fellesFaktaForATFLogSN/vurderrefusjon/VurderRefusjonForm.spec.tsx
+++ packages/fakta-beregning/src/components/fellesFaktaForATFLogSN/vurderrefusjon/VurderRefusjonForm.spec.tsx
@@ -57,11 +57,11 @@ describe('<VurderRefusjonForm>', () => {
   it('skal bygge initial values', () => {
     const senRefusjonkravListe = [
       { arbeidsgiverVisningsnavn: 'Arbeidsgiveren (8279312213) AS', erRefusjonskravGyldig: true, arbeidsgiverId: '8279312213' },
-      { arbeidsgiverVisningsnavn: 'Arbeidsgiverto (45345345345) AS', erRefusjonskravGyldig: false, arbeidsgiverId: '8279312213' },
+      { arbeidsgiverVisningsnavn: 'Arbeidsgiverto (45345345345) AS', erRefusjonskravGyldig: false, arbeidsgiverId: '45345345345' },
     ];
     const initialValues = VurderRefusjonForm.buildInitialValues([VURDER_REFUSJONSKRAV_SOM_HAR_KOMMET_FOR_SENT], senRefusjonkravListe);
-    expect(initialValues[lagFieldName('Arbeidsgiveren (8279312213) AS')]).to.equal(true);
-    expect(initialValues[lagFieldName('Arbeidsgiverto (45345345345) AS')]).to.equal(false);
+    expect(initialValues[lagFieldName('8279312213')]).to.equal(true);
+    expect(initialValues[lagFieldName('45345345345')]).to.equal(false);
   });
 
   it('skal bygge transform values', () => {
@@ -70,8 +70,8 @@ describe('<VurderRefusjonForm>', () => {
       { arbeidsgiverId: '45345345345', arbeidsgiverVisningsnavn: 'Arbeidsgiverto (45345345345) AS', erRefusjonskravGyldig: false },
     ];
     const values = {};
-    values[lagFieldName('Arbeidsgiveren (8279312213) AS')] = false;
-    values[lagFieldName('Arbeidsgiverto (45345345345) AS')] = true;
+    values[lagFieldName('8279312213')] = false;
+    values[lagFieldName('45345345345')] = true;
     const transformedValues = VurderRefusjonForm.transformValues(senRefusjonkravListe)(values);
     expect(transformedValues.refusjonskravGyldighet.length).to.equal(2);
     expect(transformedValues.refusjonskravGyldighet[0].arbeidsgiverId).to.equal('8279312213');
