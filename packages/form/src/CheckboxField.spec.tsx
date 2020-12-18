c9949c1bdd105a2df967cae7189f6b084d685b7b
tor-nav <tor.roneid@nav.no>
2020-12-18 13:00:46 +0100

    FIX div

--- packages/form/src/CheckboxField.spec.tsx
+++ packages/form/src/CheckboxField.spec.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { mountFieldComponent } from '@fpsak-frontend/utils-test/src/redux-form-test-helper';
-import chai, { expect } from 'chai';
 import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
 
 import { RenderCheckboxField } from './CheckboxField';
-
-chai.use(sinonChai);
 
 describe('<CheckboxField>', () => {
   it('skal kalle onChange med boolsk verdi for checked', () => {
@@ -17,16 +13,16 @@ describe('<CheckboxField>', () => {
 
     checkbox.simulate('change', { target: { checked: true } });
 
-    expect(onChange.called).is.true;
+    expect(onChange.called).toBe(true);
     const { args } = onChange.getCalls()[0];
-    expect(args).has.length(1);
-    expect(args[0]).is.true;
+    expect(args).toHaveLength(1);
+    expect(args[0]).toBe(true);
 
     checkbox.simulate('change', { target: { checked: false } });
 
     const args2 = onChange.getCalls()[0].args;
-    expect(args2).has.length(1);
-    expect(args2[0]).is.true;
+    expect(args2).toHaveLength(1);
+    expect(args2[0]).toBe(true);
   });
 
   it('skal initialisere checked med verdi fra input', () => {
@@ -34,12 +30,12 @@ describe('<CheckboxField>', () => {
     const wrapperTrue = mountFieldComponent(<RenderCheckboxField />, { value: true });
     const checkboxTrue = wrapperTrue.find('input');
 
-    expect(checkboxTrue.props().checked).to.be.true;
+    expect(checkboxTrue.props().checked).toBe(true);
 
     // @ts-ignore Fiks
     const wrapperFalse = mountFieldComponent(<RenderCheckboxField />, { value: false });
     const checkboxFalse = wrapperFalse.find('input');
 
-    expect(checkboxFalse.props().checked).to.be.false;
+    expect(checkboxFalse.props().checked).toBe(false);
   });
 });
