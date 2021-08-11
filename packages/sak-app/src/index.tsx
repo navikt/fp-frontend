69e7f93204b71176f8475065a5cc1b8ba7ebfe6d
Tor Røneid <tor.roneid@nav.no>
2021-08-11 14:00:37 +0200

    FIX polyfill norsk datetime og numberformat (#1685)

--- packages/sak-app/src/index.tsx
+++ packages/sak-app/src/index.tsx
@@ -8,6 +8,7 @@ import {
 
 import { RestApiErrorProvider, RestApiProvider } from '@fpsak-frontend/rest-api-hooks';
 
+import polyfill from './polyfill';
 import AppIndex from './app/AppIndex';
 
 const app = document.getElementById('app');
@@ -21,47 +22,50 @@ if (app === null) {
 const release = VERSION;
 const environment = window.location.hostname;
 const isDevelopment = process.env.NODE_ENV === 'development';
-init({
-  dsn: isDevelopment ? 'http://dev@localhost:9000/1' : 'https://d1b7de8cc42949569da03849b47d3ea1@sentry.gc.nav.no/17',
-  release,
-  environment,
-  integrations: [new Integrations.Breadcrumbs({ console: false })],
-  beforeSend: (event: Event, hint: EventHint) => {
-    const exception = hint.originalException;
-    // @ts-ignore
-    if (exception.isAxiosError) {
-      // @ts-ignore
-      const requestUrl = new URL(exception.request.responseURL);
-      // eslint-disable-next-line no-param-reassign
-      event.fingerprint = [
-        '{{ default }}',
-        // @ts-ignore
-        String(exception.name),
-        // @ts-ignore
-        String(exception.message),
-        String(requestUrl.pathname),
-      ];
-      // eslint-disable-next-line no-param-reassign
-      event.extra = event.extra ? event.extra : {};
-      // @ts-ignore
-      // eslint-disable-next-line no-param-reassign
-      event.extra.callId = exception.response.config.headers['Nav-Callid'];
-    }
-    return event;
-  },
-});
 
-const history = createBrowserHistory({
-  basename: '/fpsak/',
-});
+polyfill().then(() => {
+  init({
+    dsn: isDevelopment ? 'http://dev@localhost:9000/1' : 'https://d1b7de8cc42949569da03849b47d3ea1@sentry.gc.nav.no/17',
+    release,
+    environment,
+    integrations: [new Integrations.Breadcrumbs({ console: false })],
+    beforeSend: (event: Event, hint: EventHint) => {
+      const exception = hint.originalException;
+      // @ts-ignore
+      if (exception.isAxiosError) {
+        // @ts-ignore
+        const requestUrl = new URL(exception.request.responseURL);
+        // eslint-disable-next-line no-param-reassign
+        event.fingerprint = [
+          '{{ default }}',
+          // @ts-ignore
+          String(exception.name),
+          // @ts-ignore
+          String(exception.message),
+          String(requestUrl.pathname),
+        ];
+        // eslint-disable-next-line no-param-reassign
+        event.extra = event.extra ? event.extra : {};
+        // @ts-ignore
+        // eslint-disable-next-line no-param-reassign
+        event.extra.callId = exception.response.config.headers['Nav-Callid'];
+      }
+      return event;
+    },
+  });
 
-render(
-  <Router history={history}>
-    <RestApiProvider>
-      <RestApiErrorProvider>
-        <AppIndex />
-      </RestApiErrorProvider>
-    </RestApiProvider>
-  </Router>,
-  app,
-);
+  const history = createBrowserHistory({
+    basename: '/fpsak/',
+  });
+
+  render(
+    <Router history={history}>
+      <RestApiProvider>
+        <RestApiErrorProvider>
+          <AppIndex />
+        </RestApiErrorProvider>
+      </RestApiProvider>
+    </Router>,
+    app,
+  );
+});
