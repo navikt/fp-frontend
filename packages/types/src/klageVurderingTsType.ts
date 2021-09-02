f5d56c87a5b803de510c1d1a990405df15d38492
Tor Røneid <tor.roneid@nav.no>
2021-09-02 14:00:22 +0200

    FAGSYSTEM-182324 Fiks visning av påklagd vedtak i klage-panel (#1757)

--- packages/types/src/klageVurderingTsType.ts
+++ packages/types/src/klageVurderingTsType.ts
@@ -17,7 +17,7 @@ type KlageVurdering = Readonly<{
     avvistArsaker: {
       navn?: string;
     }[];
-    vedtakBehandlingUuid: string;
+    paKlagdBehandlingUuid: string;
     paklagdBehandlingType: Kodeverk;
     begrunnelse: string;
     erKlagerPart: boolean;
@@ -29,7 +29,7 @@ type KlageVurdering = Readonly<{
     avvistArsaker: {
       navn?: string;
     }[];
-    vedtakBehandlingUuid: string;
+    paKlagdBehandlingUuid: string;
     paklagdBehandlingType: Kodeverk;
     begrunnelse: string;
     erKlagerPart: boolean;
