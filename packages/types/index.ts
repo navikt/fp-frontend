332c71a1e9b6634b8d3a872c15100d6dffb37c8f
tor-nav <tor.roneid@nav.no>
2021-09-09 11:52:25 +0200

    FIX export av typer

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,8 +1,8 @@
-export { default as Aksjonspunkt } from './src/aksjonspunktTsType';
-export { default as Arbeidsforhold, ArbeidsforholdPermisjon } from './src/arbeidsforholdTsType';
-export { default as Behandling } from './src/behandlingTsType';
-export { default as BehandlingAppKontekst, BehandlingÅrsak } from './src/behandlingAppKontekstTsType';
-export { default as Behandlingsresultat } from './src/behandlingsresultatTsType';
+export type { default as Aksjonspunkt } from './src/aksjonspunktTsType';
+export type { default as Arbeidsforhold, ArbeidsforholdPermisjon } from './src/arbeidsforholdTsType';
+export type { default as Behandling } from './src/behandlingTsType';
+export type { default as BehandlingAppKontekst, BehandlingÅrsak } from './src/behandlingAppKontekstTsType';
+export type { default as Behandlingsresultat } from './src/behandlingsresultatTsType';
 export type {
   default as Beregningsgrunnlag,
   BeregningsgrunnlagAndel,
@@ -30,9 +30,9 @@ export type {
   VurderBesteberegning,
   RefusjonskravSomKommerForSentListe,
 } from './src/beregningsgrunnlagFaktaTsType';
-export { Månedsgrunnlag, BesteberegningInntekt, Besteberegninggrunnlag } from './src/besteberegningTsTypes';
+export type { Månedsgrunnlag, BesteberegningInntekt, Besteberegninggrunnlag } from './src/besteberegningTsTypes';
 export type { default as BeregningsgrunnlagArbeidsforhold } from './src/beregningsgrunnlagArbeidsforholdTsType';
-export {
+export type {
   RefusjonTilVurderingAndel,
   TidligereUtbetalinger,
   FordelBeregningsgrunnlagPeriode,
@@ -41,72 +41,72 @@ export {
   PerioderMedGraderingEllerRefusjon,
   FaktaOmFordeling,
 } from './src/beregningsgrunnlagFordelingTsType';
-export { default as Feriepengegrunnlag, FeriepengegrunnlagAndel } from './src/feriepengegrunnlagTsType';
-export { default as BeregningsresultatEs } from './src/beregningsresultatEsTsType';
-export { default as BeregningsresultatFp, BeregningsresultatPeriode, BeregningsresultatPeriodeAndel } from './src/beregningsresultatFpTsType';
-export { default as Dokument } from './src/dokumentTsType';
-export { default as FaktaArbeidsforhold } from './src/faktaArbeidsforholdTsType';
-export { default as InntektArbeidYtelse, IAYInntektsmelding, RelatertTilgrensedYtelse } from './src/inntektArbeidYtelseTsType';
-export { default as Kodeverk } from './src/kodeverkTsType';
-export { default as KodeverkMedNavn } from './src/kodeverkMedNavnTsType';
-export { default as AlleKodeverk } from './src/kodeverkAlleTsType';
-export { default as AlleKodeverkTilbakekreving } from './src/kodeverkAlleTilbakekrevingTsType';
-export { default as NavAnsatt } from './src/navAnsattTsType';
-export {
+export type { default as Feriepengegrunnlag, FeriepengegrunnlagAndel } from './src/feriepengegrunnlagTsType';
+export type { default as BeregningsresultatEs } from './src/beregningsresultatEsTsType';
+export type { default as BeregningsresultatFp, BeregningsresultatPeriode, BeregningsresultatPeriodeAndel } from './src/beregningsresultatFpTsType';
+export type { default as Dokument } from './src/dokumentTsType';
+export type { default as FaktaArbeidsforhold } from './src/faktaArbeidsforholdTsType';
+export type { default as InntektArbeidYtelse, IAYInntektsmelding, RelatertTilgrensedYtelse } from './src/inntektArbeidYtelseTsType';
+export type { default as Kodeverk } from './src/kodeverkTsType';
+export type { default as KodeverkMedNavn } from './src/kodeverkMedNavnTsType';
+export type { default as AlleKodeverk } from './src/kodeverkAlleTsType';
+export type { default as AlleKodeverkTilbakekreving } from './src/kodeverkAlleTilbakekrevingTsType';
+export type { default as NavAnsatt } from './src/navAnsattTsType';
+export type {
   default as SimuleringResultat, DetaljertSimuleringResultat, Mottaker, SimuleringResultatRad, SimuleringResultatPerFagområde,
 } from './src/simuleringResultatTsType';
-export { default as Soknad, ManglendeVedleggSoknad, UtlandsoppholdPeriode } from './src/soknadTsType';
-export {
+export type { default as Soknad, ManglendeVedleggSoknad, UtlandsoppholdPeriode } from './src/soknadTsType';
+export type {
   default as UttaksresultatPeriode, PeriodeSoker, PeriodeSokerAktivitet, ArsakKodeverk,
 } from './src/uttaksresultatPeriodeTsType';
-export {
+export type {
   default as UttakStonadskontoer, Stonadskonto, AktivitetSaldo, AktivitetIdentifikator,
 } from './src/uttakStonadskontoerTsType';
-export { default as Vilkar } from './src/vilkarTsType';
-export { default as Ytelsefordeling } from './src/ytelsefordelingTsType';
-export { FamilieHendelse, FamilieHendelseSamling, AvklartBarn } from './src/familieHendelseTsType';
-export { default as Fagsak } from './src/fagsakTsType';
-export { default as FagsakPersoner, FagsakPerson, FagsakHendelse } from './src/fagsakPersonerTsType';
-export {
+export type { default as Vilkar } from './src/vilkarTsType';
+export type { default as Ytelsefordeling } from './src/ytelsefordelingTsType';
+export type { FamilieHendelse, FamilieHendelseSamling, AvklartBarn } from './src/familieHendelseTsType';
+export type { default as Fagsak } from './src/fagsakTsType';
+export type { default as FagsakPersoner, FagsakPerson, FagsakHendelse } from './src/fagsakPersonerTsType';
+export type {
   default as Medlemskap, MedlemPeriode, MedlemskapPeriode, Oppholdstillatelse, PersonopplysningMedlem,
 } from './src/medlemskapTsType';
-export { default as Risikoklassifisering } from './src/risikoklassifiseringTsType';
+export type { default as Risikoklassifisering } from './src/risikoklassifiseringTsType';
 export type { default as AnkeVurdering } from './src/ankeVurderingTsType';
 export type { default as KlageVurdering, KlageVurderingResultat } from './src/klageVurderingTsType';
-export { default as Innsyn, InnsynDokument, InnsynVedtaksdokument } from './src/innsynTsType';
-export { default as UttakKontrollerFaktaPerioderWrapper, UttakKontrollerFaktaPerioder } from './src/uttakKontrollerFaktaPerioderTsType';
-export {
+export type { default as Innsyn, InnsynDokument, InnsynVedtaksdokument } from './src/innsynTsType';
+export type { default as UttakKontrollerFaktaPerioderWrapper, UttakKontrollerFaktaPerioder } from './src/uttakKontrollerFaktaPerioderTsType';
+export type {
   default as Opptjening, OpptjeningAktivitet, FastsattOpptjening, FastsattOpptjeningAktivitet,
 } from './src/opptjeningTsType';
-export { default as TilbakekrevingValg } from './src/tilbakekrevingValgTsType';
-export { default as UttakPeriodeGrense } from './src/uttakPeriodeGrenseTsType';
-export { default as TotrinnskontrollAksjonspunkt, TotrinnskontrollArbeidsforhold, OpptjeningAktiviteter } from './src/totrinnskontrollAksjonspunktTsType';
-export { default as TotrinnskontrollSkjermlenkeContext } from './src/totrinnskontrollSkjermlenkeContextTsType';
-export {
+export type { default as TilbakekrevingValg } from './src/tilbakekrevingValgTsType';
+export type { default as UttakPeriodeGrense } from './src/uttakPeriodeGrenseTsType';
+export type { default as TotrinnskontrollAksjonspunkt, TotrinnskontrollArbeidsforhold, OpptjeningAktiviteter } from './src/totrinnskontrollAksjonspunktTsType';
+export type { default as TotrinnskontrollSkjermlenkeContext } from './src/totrinnskontrollSkjermlenkeContextTsType';
+export type {
   default as Historikkinnslag, HistorikkInnslagDokumentLink, HistorikkinnslagDel, HistorikkinnslagEndretFelt,
   HistorikkInnslagOpplysning, HistorikkInnslagAksjonspunkt,
 } from './src/historikkinnslagTsType';
-export { default as ArbeidsgiverOpplysningerPerId, ArbeidsgiverOpplysningerWrapper, ArbeidsgiverOpplysninger } from './src/arbeidsgiverOpplysningerTsType';
-export { default as BeregningsresultatTilbakekreving, BeregningResultatPeriode } from './src/beregningsresultatTilbakekrevingTsType';
-export { default as FeilutbetalingPerioderWrapper, FeilutbetalingPeriode } from './src/feilutbetalingPerioderTsType';
-export { default as FeilutbetalingFakta } from './src/feilutbetalingFaktaTsType';
-export { default as UttakKontrollerAktivitetskrav } from './src/uttakKontrollerAktivitetskravTsType';
-export { default as Aktor } from './src/aktorTsType';
+export type { default as ArbeidsgiverOpplysningerPerId, ArbeidsgiverOpplysningerWrapper, ArbeidsgiverOpplysninger } from './src/arbeidsgiverOpplysningerTsType';
+export type { default as BeregningsresultatTilbakekreving, BeregningResultatPeriode } from './src/beregningsresultatTilbakekrevingTsType';
+export type { default as FeilutbetalingPerioderWrapper, FeilutbetalingPeriode } from './src/feilutbetalingPerioderTsType';
+export type { default as FeilutbetalingFakta } from './src/feilutbetalingFaktaTsType';
+export type { default as UttakKontrollerAktivitetskrav } from './src/uttakKontrollerAktivitetskravTsType';
+export type { default as Aktor } from './src/aktorTsType';
 export type { Kjønnkode } from './src/Kjonnkode';
 export { KjønnkodeEnum } from './src/Kjonnkode';
-export { default as Personoversikt, PersonopplysningerBasis } from './src/personoversiktTsType';
+export type { default as Personoversikt, PersonopplysningerBasis } from './src/personoversiktTsType';
 export type { default as Personadresse } from './src/personadresseTsType';
-export { default as AksessRettigheter, Aksess } from './src/aksessRettigheterTsType';
-export { default as StandardFaktaPanelProps } from './src/standardFaktaPanelPropsTsType';
-export { default as StandardFaktaPanelPropsTilbakekreving } from './src/standardFaktaPanelPropsTilbakekrevingTsType';
-export { default as StandardProsessPanelProps } from './src/standardProsessPanelPropsTsType';
-export { default as StandardProsessPanelPropsTilbakekreving } from './src/standardProsessPanelPropsTilbakekrevingTsType';
-export { default as Verge } from './src/vergeTsType';
-export { default as FeilutbetalingAarsak } from './src/feilutbetalingAarsakTsType';
-export { default as DetaljerteFeilutbetalingsperioder, DetaljertFeilutbetalingPeriode } from './src/detaljerteFeilutbetalingsperioderTsType';
-export { default as VilkarsVurdertePerioderWrapper, VilkarsVurdertPeriode, AktsomhetInfo } from './src/vilkarsVurdertePerioderTsType';
-export { default as Vedtaksbrev } from './src/vedtaksbrevTsType';
-export { default as VedtaksbrevAvsnitt } from './src/vedtaksbrevAvsnittTsType';
+export type { default as AksessRettigheter, Aksess } from './src/aksessRettigheterTsType';
+export type { default as StandardFaktaPanelProps } from './src/standardFaktaPanelPropsTsType';
+export type { default as StandardFaktaPanelPropsTilbakekreving } from './src/standardFaktaPanelPropsTilbakekrevingTsType';
+export type { default as StandardProsessPanelProps } from './src/standardProsessPanelPropsTsType';
+export type { default as StandardProsessPanelPropsTilbakekreving } from './src/standardProsessPanelPropsTilbakekrevingTsType';
+export type { default as Verge } from './src/vergeTsType';
+export type { default as FeilutbetalingAarsak } from './src/feilutbetalingAarsakTsType';
+export type { default as DetaljerteFeilutbetalingsperioder, DetaljertFeilutbetalingPeriode } from './src/detaljerteFeilutbetalingsperioderTsType';
+export type { default as VilkarsVurdertePerioderWrapper, VilkarsVurdertPeriode, AktsomhetInfo } from './src/vilkarsVurdertePerioderTsType';
+export type { default as Vedtaksbrev } from './src/vedtaksbrevTsType';
+export type { default as VedtaksbrevAvsnitt } from './src/vedtaksbrevAvsnittTsType';
 export type {
   default as FodselOgTilrettelegging, ArbeidsforholdFodselOgTilrettelegging, ArbeidsforholdTilretteleggingDato, Permisjon,
 } from './src/fodselOgTilretteleggingTsType';
