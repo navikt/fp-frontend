c3b71b3377b0bd51fd142886d23b3fd48510d172
tor-nav <tor.roneid@nav.no>
2021-09-28 11:08:45 +0200

    div

--- packages/sak-historikk/src/HistorikkSakIndex.spec.tsx
+++ packages/sak-historikk/src/HistorikkSakIndex.spec.tsx
@@ -4,7 +4,9 @@ import { composeStories } from '@storybook/testing-react';
 import userEvent from '@testing-library/user-event';
 import * as stories from './HistorikkSakIndex.stories';
 
-const { BehandlingIkkeErValgt, BehandlingErValgt } = composeStories(stories);
+const {
+  BehandlingIkkeErValgt, BehandlingErValgt, HistorikkinnslagAktivitetskrav, HistorikkinnslagForeldelse, HistorikkinnslagTilbakekreving,
+} = composeStories(stories);
 
 describe('<HistorikkSakIndex>', () => {
   it('skal vise historikk uten behandlingsfilter når ingen behandlinger er valgt', async () => {
@@ -38,5 +40,38 @@ describe('<HistorikkSakIndex>', () => {
     expect(snakkebobler[0]).toHaveTextContent(/19.09.2019 - 12:16/);
     expect(snakkebobler[1]).toHaveTextContent(/18.09.2019 - 15:25/);
     expect(snakkebobler[2]).toHaveTextContent(/18.09.2019 - 13:12/);
+  });
+
+  it('skal vise historikkinnslag for aktivitetskrav', async () => {
+    render(<HistorikkinnslagAktivitetskrav />);
+    expect(await screen.findByText('Fakta om aktivitetskrav')).toBeInTheDocument();
+    expect(screen.getByText('19.09.2019 - 12:16 // Søker')).toBeInTheDocument();
+    expect(screen.getByText(/Perioden/)).toBeInTheDocument();
+    expect(screen.getByText(/05.01.2021 - 19.01.2021/)).toBeInTheDocument();
+    expect(screen.getByText(/er avklart til/)).toBeInTheDocument();
+    expect(screen.getByText(/Mor er i aktivitet/)).toBeInTheDocument();
+    expect(screen.getByText('Dette er en begrunnelse')).toBeInTheDocument();
+  });
+
+  it('skal vise historikkinnslag for foreldelse', async () => {
+    render(<HistorikkinnslagForeldelse />);
+    expect(await screen.findByText('19.09.2019 - 12:16 // Søker')).toBeInTheDocument();
+    expect(screen.getByText(/Manuell vurdering/)).toBeInTheDocument();
+    expect(screen.getByText(/av perioden 10.10.2018-./)).toBeInTheDocument();
+    expect(screen.getByText(/endret fra Ja til/)).toBeInTheDocument();
+    expect(screen.getByText(/Nei/)).toBeInTheDocument();
+    expect(screen.getByText(/er satt til/)).toBeInTheDocument();
+    expect(screen.getByText(/Dette er en ny verdi/)).toBeInTheDocument();
+  });
+
+  it('skal vise historikkinnslag for tilbakekreving', async () => {
+    render(<HistorikkinnslagTilbakekreving />);
+    expect(await screen.findByText('19.09.2019 - 12:16 // Søker')).toBeInTheDocument();
+    expect(screen.getByText(/Vurdering/)).toBeInTheDocument();
+    expect(screen.getByText(/av perioden 10.10.2018-./)).toBeInTheDocument();
+    expect(screen.getByText(/endret fra Ja til/)).toBeInTheDocument();
+    expect(screen.getByText(/Nei/)).toBeInTheDocument();
+    expect(screen.getByText(/er satt til/)).toBeInTheDocument();
+    expect(screen.getByText(/Dette er ny verdi/)).toBeInTheDocument();
   });
 });
--- packages/sak-historikk/src/HistorikkSakIndex.stories.tsx
+++ packages/sak-historikk/src/HistorikkSakIndex.stories.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 
-import { Historikkinnslag } from '@fpsak-frontend/types';
-import { alleKodeverk } from '@fpsak-frontend/storybook-utils';
+import { skjermlenkeCodes } from '@fpsak-frontend/konstanter';
+import { Historikkinnslag, HistorikkinnslagDel } from '@fpsak-frontend/types';
+import { alleKodeverk, withRouter } from '@fpsak-frontend/storybook-utils';
+import historikkOpplysningTypeCodes from './kodeverk/historikkOpplysningTypeCodes';
 import HistorikkSakIndex from './HistorikkSakIndex';
+import historikkEndretFeltType from './kodeverk/historikkEndretFeltType';
+import historikkinnslagType from './kodeverk/historikkinnslagType';
 
-const history = [{
+const historikkInnslag = [{
   behandlingUuid: '999951',
   type: {
     kode: 'NYE_REGOPPLYSNINGER',
@@ -119,21 +123,27 @@ const locationMock = {
 export default {
   title: 'sak/sak-historikk',
   component: HistorikkSakIndex,
+  decorators: [withRouter],
 };
 
 const Template: Story<{
   valgtBehandlingUuid?: string;
+  historikkFpSak?: Historikkinnslag[],
+  historikkFpTilbake?: Historikkinnslag[],
 }> = ({
   valgtBehandlingUuid,
+  historikkFpSak = [],
+  historikkFpTilbake = [],
 }) => (
   <div style={{
     width: '600px', backgroundColor: 'white', padding: '30px',
   }}
   >
     <HistorikkSakIndex
-      historikkFpSak={history}
-      historikkFpTilbake={[]}
+      historikkFpSak={historikkFpSak}
+      historikkFpTilbake={historikkFpTilbake}
       alleKodeverkFpSak={alleKodeverk as any}
+      alleKodeverkFpTilbake={alleKodeverk as any}
       saksnummer="2"
       getBehandlingLocation={() => locationMock}
       createLocationForSkjermlenke={() => locationMock}
@@ -143,8 +153,180 @@ const Template: Story<{
 );
 
 export const BehandlingIkkeErValgt = Template.bind({});
+BehandlingIkkeErValgt.args = {
+  historikkFpSak: historikkInnslag,
+};
 
 export const BehandlingErValgt = Template.bind({});
 BehandlingErValgt.args = {
+  historikkFpSak: historikkInnslag,
+  valgtBehandlingUuid: '999951',
+};
+
+export const HistorikkinnslagAktivitetskrav = Template.bind({});
+HistorikkinnslagAktivitetskrav.args = {
+  historikkFpSak: [{
+    behandlingUuid: '999951',
+    type: {
+      kode: historikkinnslagType.AVKLART_AKTIVITETSKRAV,
+      kodeverk: 'HISTORIKKINNSLAG_TYPE',
+    },
+    aktoer: {
+      kode: 'SOKER',
+      kodeverk: 'HISTORIKK_AKTOER',
+    },
+    kjoenn: {
+      kode: 'K',
+      kodeverk: 'BRUKER_KJOENN',
+    },
+    opprettetAv: 'Srvengangsstonad',
+    opprettetTidspunkt: '2019-09-19T12:16:14.499',
+    dokumentLinks: [],
+    historikkinnslagDeler: [{
+      begrunnelseFritekst: 'Dette er en begrunnelse',
+      opplysninger: [{
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.UTTAK_PERIODE_FOM.kode,
+          kodeverk: 'HISTORIKK_OPPLYSNING_TYPE',
+        },
+        tilVerdi: '05.01.2021',
+      }, {
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.UTTAK_PERIODE_TOM.kode,
+          kodeverk: 'HISTORIKK_OPPLYSNING_TYPE',
+        },
+        tilVerdi: '19.01.2021',
+      }],
+      skjermlenke: {
+        kode: skjermlenkeCodes.FAKTA_OM_AKTIVITETSKRAV.kode,
+        kodeverk: 'SKJERMLENKE_TYPE',
+      },
+      endredeFelter: [{
+        endretFeltNavn: {
+          kode: 'AKTIVITETSKRAV_AVKLARING',
+          kodeverk: 'HISTORIKK_ENDRET_FELT_TYPE',
+        },
+        klNavn: 'HISTORIKK_ENDRET_FELT_TYPE',
+        fraVerdi: undefined,
+        tilVerdi: 'I_AKTIVITET',
+        klTilVerdi: 'AKTIVITETSKRAV_AVKLARING',
+      }],
+    }] as HistorikkinnslagDel[],
+  }],
+  valgtBehandlingUuid: '999951',
+};
+
+export const HistorikkinnslagForeldelse = Template.bind({});
+HistorikkinnslagForeldelse.args = {
+  historikkFpTilbake: [{
+    behandlingUuid: '999951',
+    type: {
+      kode: historikkinnslagType.FORELDELSE,
+      kodeverk: 'HISTORIKKINNSLAG_TYPE',
+    },
+    aktoer: {
+      kode: 'SOKER',
+      kodeverk: 'HISTORIKK_AKTOER',
+    },
+    kjoenn: {
+      kode: 'K',
+      kodeverk: 'BRUKER_KJOENN',
+    },
+    opprettetAv: 'Srvengangsstonad',
+    opprettetTidspunkt: '2019-09-19T12:16:14.499',
+    dokumentLinks: [],
+    historikkinnslagDeler: [{
+      skjermlenke: {
+        kode: 'FORELDELSE',
+      },
+      endredeFelter: [{
+        endretFeltNavn: {
+          kode: 'feltkode',
+        },
+        fraVerdi: 'Ja',
+        tilVerdi: 'Nei',
+      }, {
+        endretFeltNavn: {
+          kode: 'Anna feltkode',
+        },
+        tilVerdi: 'Dette er en ny verdi',
+      }],
+      opplysninger: [{
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.PERIODE_FOM.kode,
+          kodeverk: '',
+        },
+        tilVerdi: '10.10.2018',
+      }, {
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.PERIODE_TOM.kode,
+          tilVerdi: '10.12.2018',
+        },
+      }],
+    }] as HistorikkinnslagDel[],
+  }],
+  valgtBehandlingUuid: '999951',
+};
+
+export const HistorikkinnslagTilbakekreving = Template.bind({});
+HistorikkinnslagTilbakekreving.args = {
+  historikkFpTilbake: [{
+    behandlingUuid: '999951',
+    type: {
+      kode: historikkinnslagType.TILBAKEKREVING,
+      kodeverk: 'HISTORIKKINNSLAG_TYPE',
+    },
+    aktoer: {
+      kode: 'SOKER',
+      kodeverk: 'HISTORIKK_AKTOER',
+    },
+    kjoenn: {
+      kode: 'K',
+      kodeverk: 'BRUKER_KJOENN',
+    },
+    opprettetAv: 'Srvengangsstonad',
+    opprettetTidspunkt: '2019-09-19T12:16:14.499',
+    dokumentLinks: [],
+    historikkinnslagDeler: [{
+      skjermlenke: {
+        kode: 'TILBAKEKREVING',
+      },
+      endredeFelter: [{
+        endretFeltNavn: {
+          kode: historikkEndretFeltType.ER_VILKARENE_TILBAKEKREVING_OPPFYLT,
+        },
+        fraVerdi: 'Ja',
+        tilVerdi: 'Nei',
+      }, {
+        endretFeltNavn: {
+          kode: 'Anna feltkode',
+        },
+        tilVerdi: 'Dette er ny verdi',
+      }],
+      opplysninger: [{
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.PERIODE_FOM.kode,
+          kodeverk: '',
+        },
+        tilVerdi: '10.10.2018',
+      }, {
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.PERIODE_TOM.kode,
+          tilVerdi: '10.12.2018',
+        },
+      }, {
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.TILBAKEKREVING_OPPFYLT_BEGRUNNELSE.kode,
+          tilVerdi: 'test',
+        },
+      },
+      {
+        opplysningType: {
+          kode: historikkOpplysningTypeCodes.SÆRLIG_GRUNNER_BEGRUNNELSE.kode,
+          tilVerdi: 'test',
+        },
+      }],
+    }] as HistorikkinnslagDel[],
+  }],
   valgtBehandlingUuid: '999951',
 };
