97b0490b650ca6ecd7207846729046a3c6716db6
tor-nav <tor.roneid@nav.no>
2021-09-29 16:01:45 +0200

    div

--- packages/rest-api-hooks/src/local-data/useRestApi.tsx
+++ packages/rest-api-hooks/src/local-data/useRestApi.tsx
@@ -42,6 +42,10 @@ const getUseRestApi = (requestApi: RequestApi) => function useRestApi<T, P>(
   const [data, setData] = useState<RestApiData<T>>(DEFAULT_STATE);
 
   useEffect(() => {
+    console.log(requestApi.endpointConfigList);
+    console.log(requestApi.hasPath(key.name));
+    console.log(key.name);
+    console.log(requestApi.links);
     if (requestApi.hasPath(key.name) && !allOptions.suspendRequest) {
       setData((oldState) => ({
         state: RestApiState.LOADING,
