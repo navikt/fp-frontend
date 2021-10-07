3ce0b0a89c18e1953d92577721309a092cac15ff
tor-nav <tor.roneid@nav.no>
2021-10-07 15:09:03 +0200

    div

--- packages/behandling-tilbakekreving/src/prosessPaneler/ForeldelseProsessStegInitPanel.spec.tsx
+++ packages/behandling-tilbakekreving/src/prosessPaneler/ForeldelseProsessStegInitPanel.spec.tsx
@@ -1,48 +1,148 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
-import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
-import ForeldelseProsessIndex from '@fpsak-frontend/prosess-foreldelse';
-import { ProsessDefaultInitPanel, ProsessDefaultInitPanelProps, ProsessPanelInitProps } from '@fpsak-frontend/behandling-felles';
-import { RestApiState } from '@fpsak-frontend/rest-api-hooks';
+import tilbakekrevingKodeverkTyper from '@fpsak-frontend/kodeverk/src/tilbakekrevingKodeverkTyper';
 import navBrukerKjonn from '@fpsak-frontend/kodeverk/src/navBrukerKjonn';
 import {
-  Aksjonspunkt, AlleKodeverkTilbakekreving, Behandling, FeilutbetalingPerioderWrapper, StandardProsessPanelProps,
+  AlleKodeverkTilbakekreving, Behandling, Aksjonspunkt, FeilutbetalingPerioderWrapper,
 } from '@fpsak-frontend/types';
+import RestApiMock from '@fpsak-frontend/utils-test/src/rest/RestApiMock';
+import aksjonspunktCodesTilbakekreving from '@fpsak-frontend/kodeverk/src/aksjonspunktCodesTilbakekreving';
+import aksjonspunktStatus from '@fpsak-frontend/kodeverk/src/aksjonspunktStatus';
+import * as Felles from '@fpsak-frontend/behandling-felles/src/utils/prosess/useStandardProsessPanelProps';
+import behandlingType from '@fpsak-frontend/kodeverk/src/behandlingType';
+import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
+import { alleKodeverk } from '@fpsak-frontend/storybook-utils';
+import foreldelseVurderingType from '@fpsak-frontend/kodeverk/src/foreldelseVurderingType';
 
 import { requestTilbakekrevingApi, TilbakekrevingBehandlingApiKeys } from '../data/tilbakekrevingBehandlingApi';
 import ForeldelseProsessStegInitPanel from './ForeldelseProsessStegInitPanel';
 
-type INIT_DATA = {
-  aksjonspunkter: Aksjonspunkt[];
-  perioderForeldelse: FeilutbetalingPerioderWrapper;
-}
+const behandling = {
+  uuid: 'test-uuid',
+  versjon: 1,
+  behandlingÅrsaker: [],
+  type: {
+    kode: behandlingType.FORSTEGANGSSOKNAD,
+    kodeverk: '',
+  },
+} as Behandling;
+
+// @ts-ignore Fiks
+const kodeverk = alleKodeverk as AlleKodeverk;
+
+const alleKodeverkTilbakekreving = {
+  [tilbakekrevingKodeverkTyper.FORELDELSE_VURDERING]: [
+    {
+      kode: foreldelseVurderingType.FORELDET,
+      navn: 'Foreldet',
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+    {
+      kode: foreldelseVurderingType.TILLEGGSFRIST,
+      navn: 'Ikke foreldet, med tilleggsfrist',
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+    {
+      kode: foreldelseVurderingType.IKKE_FORELDET,
+      navn: 'Ikke foreldet',
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+  ],
+} as AlleKodeverkTilbakekreving;
+
+const perioderForeldelse = {
+  perioder: [{
+    fom: '2019-01-01',
+    tom: '2019-01-31',
+    belop: 1000,
+    begrunnelse: 'Foreldet',
+    foreldelseVurderingType: {
+      kode: foreldelseVurderingType.FORELDET,
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+    foreldelsesfrist: '2020-04-01',
+  }, {
+    fom: '2019-02-01',
+    tom: '2019-02-28',
+    belop: 3000,
+    begrunnelse: 'Over foreldelsesfrist, med tillegsfrist brukes',
+    foreldelseVurderingType: {
+      kode: foreldelseVurderingType.TILLEGGSFRIST,
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+    foreldelsesfrist: '2020-04-01',
+    oppdagelsesDato: '2019-11-01',
+  }, {
+    fom: '2019-03-01',
+    tom: '2019-03-31',
+    belop: 3000,
+    begrunnelse: 'Ikke foreldet',
+    foreldelseVurderingType: {
+      kode: foreldelseVurderingType.IKKE_FORELDET,
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+  },
+  {
+    fom: '2019-04-01',
+    tom: '2019-04-30',
+    belop: 4000,
+    foreldelseVurderingType: {
+      kode: foreldelseVurderingType.UDEFINERT,
+      kodeverk: 'FORELDELSE_VURDERING',
+    },
+  }],
+} as FeilutbetalingPerioderWrapper;
 
 describe('<ForeldelseProsessStegInitPanel>', () => {
-  it('skal rendre komponent', () => {
-    requestTilbakekrevingApi.mock(TilbakekrevingBehandlingApiKeys.BEREGNE_BELØP.name);
-
-    const wrapper = shallow(<ForeldelseProsessStegInitPanel
-      valgtProsessSteg="default"
-      registrerProsessPanel={() => {}}
-      fagsakKjønn={{
-        kode: navBrukerKjonn.KVINNE,
+  const submitCallback = jest.fn();
+  jest.spyOn(Felles, 'default').mockImplementation(() => ({
+    behandling,
+    alleMerknaderFraBeslutter: {},
+    submitCallback,
+    status: vilkarUtfallType.IKKE_VURDERT,
+    alleKodeverk: kodeverk,
+    isReadOnly: false,
+    readOnlySubmitButton: false,
+    aksjonspunkter: [{
+      definisjon: {
+        kode: aksjonspunktCodesTilbakekreving.VURDER_FORELDELSE,
         kodeverk: '',
-      }}
-      fptilbakeKodeverk={{} as AlleKodeverkTilbakekreving}
-      behandling={{ versjon: 1 } as Behandling}
-    />);
+      },
+      erAktivt: true,
+      kanLoses: true,
+      status: {
+        kode: aksjonspunktStatus.OPPRETTET,
+        kodeverk: '',
+      },
+    }] as Aksjonspunkt[],
+    vilkar: [],
+    isAksjonspunktOpen: true,
+    setFormData: () => undefined,
+  }));
 
-    const panel = wrapper.find<Required<ProsessDefaultInitPanelProps<INIT_DATA, any>> & ProsessPanelInitProps>(ProsessDefaultInitPanel);
+  it('skal rendre komponent', async () => {
+    const data = [
+      { key: TilbakekrevingBehandlingApiKeys.AKSJONSPUNKTER.name, data: [] },
+      { key: TilbakekrevingBehandlingApiKeys.PERIODER_FORELDELSE.name, data: perioderForeldelse },
+      { key: TilbakekrevingBehandlingApiKeys.BEREGNE_BELØP.name, data: undefined },
+    ];
+    render(
+      <RestApiMock data={data} requestApi={requestTilbakekrevingApi}>
+        <ForeldelseProsessStegInitPanel
+          valgtProsessSteg="default"
+          registrerProsessPanel={() => {}}
+          fagsakKjønn={{
+            kode: navBrukerKjonn.KVINNE,
+            kodeverk: '',
+          }}
+          fptilbakeKodeverk={alleKodeverkTilbakekreving}
+          behandling={{ versjon: 1 } as Behandling}
+        />
+      </RestApiMock>,
+    );
 
-    expect(panel.props().skalPanelVisesIMeny({} as StandardProsessPanelProps, RestApiState.SUCCESS)).toBe(true);
-
-    const perioderForeldelse = {} as FeilutbetalingPerioderWrapper;
-
-    expect(panel.props().hentOverstyrtStatus({}, {} as StandardProsessPanelProps)).toEqual(vilkarUtfallType.IKKE_VURDERT);
-    // @ts-ignore
-    expect(panel.props().hentOverstyrtStatus({ perioderForeldelse })).toEqual(vilkarUtfallType.OPPFYLT);
-
-    expect(panel.props().renderPanel({}, { aksjonspunkter: [], perioderForeldelse: {} as FeilutbetalingPerioderWrapper }).type).toEqual(ForeldelseProsessIndex);
+    expect(await screen.findByText('Foreldelse')).toBeInTheDocument();
+    expect(screen.getByText('Perioden før 07.04.2019 kan være foreldet. Del opp perioden ved behov og fastsett foreldelse')).toBeInTheDocument();
   });
 });
