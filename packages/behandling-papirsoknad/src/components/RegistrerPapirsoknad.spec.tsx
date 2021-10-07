5e06344bdcd9259936e567d57b7419ef39c4d528
tor-nav <tor.roneid@nav.no>
2021-10-07 14:28:27 +0200

    div

--- packages/behandling-papirsoknad/src/components/RegistrerPapirsoknad.spec.tsx
+++ packages/behandling-papirsoknad/src/components/RegistrerPapirsoknad.spec.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import sinon from 'sinon';
-import { Dispatch } from 'redux';
+import { render, screen } from '@testing-library/react';
+import { IntlProvider } from 'react-intl';
 
-import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
-import aksjonspunktStatus from '@fpsak-frontend/kodeverk/src/aksjonspunktStatus';
+import RestApiMock from '@fpsak-frontend/utils-test/src/rest/RestApiMock';
 import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
-import foreldreType from '@fpsak-frontend/kodeverk/src/foreldreType';
-import familieHendelseType from '@fpsak-frontend/kodeverk/src/familieHendelseType';
 import behandlingType from '@fpsak-frontend/kodeverk/src/behandlingType';
 import behandlingStatus from '@fpsak-frontend/kodeverk/src/behandlingStatus';
 import fagsakStatus from '@fpsak-frontend/kodeverk/src/fagsakStatus';
-import { BehandlingPaVent } from '@fpsak-frontend/behandling-felles';
-import { SoknadData } from '@fpsak-frontend/papirsoknad-felles';
 import { AlleKodeverk, Behandling, Fagsak } from '@fpsak-frontend/types';
+import { alleKodeverk } from '@fpsak-frontend/storybook-utils';
 
 import RegistrerPapirsoknad from './RegistrerPapirsoknad';
-import SoknadRegistrertModal from './SoknadRegistrertModal';
-import RegistrerPapirsoknadPanel from './RegistrerPapirsoknadPanel';
 import { requestPapirsoknadApi, PapirsoknadApiKeys } from '../data/papirsoknadApi';
+
+import messages from '../../i18n/nb_NO.json';
 
 const fagsak = {
   saksnummer: '123456',
@@ -58,143 +54,88 @@ const rettigheter = {
   },
 };
 
+// @ts-ignore
+const kodeverk = alleKodeverk as AlleKodeverk;
+
 describe('<RegistrerPapirsoknad>', () => {
-  it('skal rendre komponenter', () => {
-    requestPapirsoknadApi.mock(PapirsoknadApiKeys.AKSJONSPUNKTER.name, []);
+  it('skal rendre komponenter', async () => {
+    const data = [
+      { key: PapirsoknadApiKeys.AKSJONSPUNKTER.name, data: [] },
+    ];
+    render(
+      <IntlProvider locale="nb-NO" messages={messages}>
+        <RestApiMock data={data} requestApi={requestPapirsoknadApi}>
+          <RegistrerPapirsoknad
+            fagsak={fagsak}
+            fagsakPersonnummer="12343541"
+            behandling={behandling}
+            kodeverk={kodeverk}
+            rettigheter={rettigheter}
+            hentBehandling={sinon.spy()}
+            lagreAksjonspunkt={sinon.spy()}
+          />
+        </RestApiMock>
+      </IntlProvider>,
+    );
 
-    const wrapper = shallow(<RegistrerPapirsoknad
-      fagsak={fagsak}
-      fagsakPersonnummer="12343541"
-      behandling={behandling}
-      kodeverk={{} as AlleKodeverk}
-      rettigheter={rettigheter}
-      hentBehandling={sinon.spy()}
-      lagreAksjonspunkt={sinon.spy()}
-    />);
-    expect(wrapper.find(BehandlingPaVent)).toHaveLength(1);
-    expect(wrapper.find(SoknadRegistrertModal)).toHaveLength(1);
-    const panel = wrapper.find(RegistrerPapirsoknadPanel);
-    expect(panel).toHaveLength(1);
-    expect(panel.prop('readOnly')).toBe(false);
+    expect(await screen.findByText('Registrere søknad')).toBeInTheDocument();
+    expect(screen.getByText('Registrer inn alle opplysninger fra papirsøknaden')).toBeInTheDocument();
   });
 
-  it('skal rendre komponenter som readonly når veileder', () => {
-    requestPapirsoknadApi.mock(PapirsoknadApiKeys.AKSJONSPUNKTER.name, []);
+  it('skal rendre komponenter som readonly når veileder', async () => {
+    const data = [
+      { key: PapirsoknadApiKeys.AKSJONSPUNKTER.name, data: [] },
+    ];
+    render(
+      <IntlProvider locale="nb-NO" messages={messages}>
+        <RestApiMock data={data} requestApi={requestPapirsoknadApi}>
+          <RegistrerPapirsoknad
+            fagsak={fagsak}
+            fagsakPersonnummer="12343541"
+            behandling={behandling}
+            kodeverk={kodeverk}
+            rettigheter={{
+              ...rettigheter,
+              writeAccess: {
+                isEnabled: false,
+                employeeHasAccess: false,
+              },
+            }}
+            hentBehandling={sinon.spy()}
+            lagreAksjonspunkt={sinon.spy()}
+          />
+        </RestApiMock>
+      </IntlProvider>,
+    );
 
-    const wrapper = shallow(<RegistrerPapirsoknad
-      fagsak={fagsak}
-      fagsakPersonnummer="12343541"
-      behandling={behandling}
-      kodeverk={{} as AlleKodeverk}
-      rettigheter={{
-        ...rettigheter,
-        writeAccess: {
-          isEnabled: false,
-          employeeHasAccess: false,
-        },
-      }}
-      hentBehandling={sinon.spy()}
-      lagreAksjonspunkt={sinon.spy()}
-    />);
-    const panel = wrapper.find(RegistrerPapirsoknadPanel);
-    expect(panel.prop('readOnly')).toBe(true);
+    expect(await screen.findByText('Registrere søknad')).toBeInTheDocument();
+    expect(screen.queryByText('Registrer inn alle opplysninger fra papirsøknaden')).not.toBeInTheDocument();
   });
 
-  it('skal rendre komponenter som readonly når behandling er satt på vent', () => {
-    requestPapirsoknadApi.mock(PapirsoknadApiKeys.AKSJONSPUNKTER.name, []);
+  it('skal rendre komponenter som readonly når behandling er satt på vent', async () => {
+    const data = [
+      { key: PapirsoknadApiKeys.AKSJONSPUNKTER.name, data: [] },
+    ];
+    render(
+      <IntlProvider locale="nb-NO" messages={messages}>
+        <RestApiMock data={data} requestApi={requestPapirsoknadApi}>
+          <RegistrerPapirsoknad
+            fagsak={fagsak}
+            fagsakPersonnummer="12343541"
+            behandling={{
+              ...behandling,
+              behandlingPaaVent: true,
+            } as Behandling}
+            kodeverk={kodeverk}
+            rettigheter={rettigheter}
+            hentBehandling={sinon.spy()}
+            lagreAksjonspunkt={sinon.spy()}
+          />
+        </RestApiMock>
+      </IntlProvider>,
+    );
 
-    const wrapper = shallow(<RegistrerPapirsoknad
-      fagsak={fagsak}
-      fagsakPersonnummer="12343541"
-      behandling={{
-        ...behandling,
-        behandlingPaaVent: true,
-      } as Behandling}
-      kodeverk={{} as AlleKodeverk}
-      rettigheter={rettigheter}
-      hentBehandling={sinon.spy()}
-      lagreAksjonspunkt={sinon.spy()}
-    />);
-    expect(wrapper.find(BehandlingPaVent)).toHaveLength(1);
-    expect(wrapper.find(SoknadRegistrertModal)).toHaveLength(1);
-    const panel = wrapper.find(RegistrerPapirsoknadPanel);
-    expect(panel).toHaveLength(1);
-    expect(panel.prop('readOnly')).toBe(true);
-  });
-
-  it('skal sette nye søknadsdata', () => {
-    requestPapirsoknadApi.mock(PapirsoknadApiKeys.AKSJONSPUNKTER.name, []);
-
-    const wrapper = shallow(<RegistrerPapirsoknad
-      fagsak={fagsak}
-      fagsakPersonnummer="12343541"
-      behandling={behandling}
-      kodeverk={{} as AlleKodeverk}
-      rettigheter={rettigheter}
-      hentBehandling={sinon.spy()}
-      lagreAksjonspunkt={sinon.spy()}
-    />);
-
-    const panel = wrapper.find(RegistrerPapirsoknadPanel);
-    expect(panel.prop('soknadData')).toBeUndefined();
-
-    const nyeSoknadsdata = new SoknadData(fagsakYtelseType.FORELDREPENGER, familieHendelseType.FODSEL, foreldreType.MOR);
-    panel.prop('setSoknadData')(nyeSoknadsdata);
-
-    const panelV2 = wrapper.find(RegistrerPapirsoknadPanel);
-    expect(panelV2.prop('soknadData')).toEqual(nyeSoknadsdata);
-  });
-
-  it('skal lagre aksjonspunkt', () => {
-    requestPapirsoknadApi.mock(PapirsoknadApiKeys.AKSJONSPUNKTER.name, [{
-      definisjon: {
-        kode: aksjonspunktCodes.REGISTRER_PAPIRSOKNAD_FORELDREPENGER,
-        kodeverk: 'DEF',
-      },
-      status: {
-        kode: aksjonspunktStatus.OPPRETTET,
-        kodeverk: 'STATUS',
-      },
-      kanLoses: true,
-      erAktivt: true,
-    }]);
-
-    const lagreAksjonspunkt = sinon.stub();
-    lagreAksjonspunkt.returns(Promise.resolve());
-
-    const wrapper = shallow(<RegistrerPapirsoknad
-      fagsak={fagsak}
-      fagsakPersonnummer="12343541"
-      behandling={behandling}
-      kodeverk={{} as AlleKodeverk}
-      rettigheter={rettigheter}
-      hentBehandling={sinon.spy()}
-      lagreAksjonspunkt={lagreAksjonspunkt}
-    />);
-
-    const panel = wrapper.find(RegistrerPapirsoknadPanel);
-
-    const nyeSoknadsdata = new SoknadData(fagsakYtelseType.FORELDREPENGER, familieHendelseType.FODSEL, foreldreType.MOR);
-    panel.prop('setSoknadData')(nyeSoknadsdata);
-
-    const panelV2 = wrapper.find(RegistrerPapirsoknadPanel);
-    panelV2.prop('lagreFullstendig')(undefined, {} as Dispatch, { valuesForRegisteredFieldsOnly: { data: 'test' } });
-
-    const calls = lagreAksjonspunkt.getCalls();
-    expect(calls).toHaveLength(1);
-    const { args } = calls[0];
-    expect(args).toHaveLength(1);
-    expect(args[0]).toEqual({
-      saksnummer: fagsak.saksnummer,
-      behandlingUuid: behandling.uuid,
-      behandlingVersjon: behandling.versjon,
-      bekreftedeAksjonspunktDtoer: [{
-        '@type': aksjonspunktCodes.REGISTRER_PAPIRSOKNAD_FORELDREPENGER,
-        soker: foreldreType.MOR,
-        soknadstype: fagsakYtelseType.FORELDREPENGER,
-        tema: familieHendelseType.FODSEL,
-        data: 'test',
-      }],
-    });
+    expect(await screen.findByText('Registrere søknad')).toBeInTheDocument();
+    expect(screen.queryByText('Registrer inn alle opplysninger fra papirsøknaden')).not.toBeInTheDocument();
   });
 });
