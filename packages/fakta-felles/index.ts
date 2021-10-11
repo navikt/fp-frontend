3aa43884cae7ae4beef28d64bd133cfad86a4f7b
tor-nav <tor.roneid@nav.no>
2021-10-11 15:35:29 +0200

    NOJIRA Endre form- og test-rammeverk for fakta-verge

--- packages/fakta-felles/index.ts
+++ packages/fakta-felles/index.ts
@@ -1,5 +1,7 @@
 export { default as MerkePanel } from './src/components/Merkepanel';
 export { default as FaktaSubmitButton } from './src/components/FaktaSubmitButton';
+export { default as FaktaBegrunnelseTextFieldNew } from './src/components/FaktaBegrunnelseTextFieldNew';
+export { default as FaktaSubmitButtonNew } from './src/components/FaktaSubmitButtonNew';
 export { default as FaktaBegrunnelseTextField } from './src/components/FaktaBegrunnelseTextField';
 export type { FormValues as FaktaBegrunnelseFormValues } from './src/components/FaktaBegrunnelseTextField';
 
