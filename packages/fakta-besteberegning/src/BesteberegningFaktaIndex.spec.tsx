51edc8092fd18a006f54ccc24500fc967dc2f049
Petter Ekern <43809623+pekern@users.noreply.github.com>
2021-11-10 14:48:58 +0100

    FIX: Nytt rammeverk for forms og tester besteberegning (#2104)
    * FIX: Nytt rammeverk for forms og tester besteberegning
    
    Former-commit-id: df12d5f90e1d8af87d73a26a446272b73f420a29

---
+++ packages/fakta-besteberegning/src/BesteberegningFaktaIndex.spec.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  render, screen, waitFor,
+} from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import userEvent from '@testing-library/user-event';
+import * as stories from './BesteberegningFaktaIndex.stories';
+
+const {
+  BesteberegningMedDagpengerOgArbeid,
+  BesteberegningMedDagpengerOgArbeidÅpentAksjonspunkt,
+} = composeStories(stories);
+
+describe('<BesteberegningFaktaIndex>', () => {
+  it('skal se at tabell renderes med korrekt antall måneder og at passende navn vises for aktivitetene', async () => {
+    render(<BesteberegningMedDagpengerOgArbeid />);
+    expect(await screen.findByText('Beregning etter §14-7, 1. ledd gir beste beregning.')).toBeInTheDocument();
+    expect(screen.getByText('Inntektsgrunnlag for besteberegning')).toBeInTheDocument();
+    expect(screen.queryByText('Beregning etter §14-7, 3. ledd gir beste beregning.')).not.toBeInTheDocument();
+    expect(screen.queryByText('Bekreft og fortsett')).not.toBeInTheDocument();
+    expect(screen.queryByText('Vurdering')).not.toBeInTheDocument();
+
+    expect(screen.getAllByText('Dagpenger')).toHaveLength(9);
+    expect(screen.getAllByText('BEDRIFT AS (910909088)')).toHaveLength(3);
+    expect(screen.getByText('Jan Bertheussen Johansen (03.03.1943)')).toBeInTheDocument();
+
+    expect(screen.getByText('Januar - 2020')).toBeInTheDocument();
+    expect(screen.getByText('Februar - 2020')).toBeInTheDocument();
+    expect(screen.getByText('Mars - 2020')).toBeInTheDocument();
+    expect(screen.getByText('April - 2020')).toBeInTheDocument();
+    expect(screen.getByText('Mai - 2020')).toBeInTheDocument();
+    expect(screen.getByText('Juni - 2020')).toBeInTheDocument();
+  });
+
+  it('skal bekrefte aksjonspunkt for vurder besteberegning', async () => {
+    const lagre = jest.fn(() => Promise.resolve());
+
+    const utils = render(<BesteberegningMedDagpengerOgArbeidÅpentAksjonspunkt submitCallback={lagre} />);
+
+    expect(await screen.findByText('Bekreft og fortsett')).toBeInTheDocument();
+    expect(await screen.getByText('Bekreft og fortsett')).toBeDisabled();
+
+    expect(screen.getByText('Saken er riktig beregnet, fortsett behandlingen')).toBeEnabled();
+    expect(screen.getByText('Saken er ikke riktig beregnet, sett saken på vent')).toBeEnabled();
+    userEvent.click(screen.getByLabelText('Saken er riktig beregnet, fortsett behandlingen'));
+    userEvent.type(utils.getByLabelText('Vurdering'), 'Min begrunnelse for vurdering av besteberegning');
+
+    expect(screen.getByText('Bekreft og fortsett')).toBeEnabled();
+    userEvent.click(screen.getByText('Bekreft og fortsett'));
+
+    await waitFor(() => expect(lagre).toHaveBeenCalledTimes(1));
+    expect(lagre).toHaveBeenNthCalledWith(1, {
+      kode: '5048',
+      begrunnelse: 'Min begrunnelse for vurdering av besteberegning',
+      besteberegningErKorrekt: true,
+    });
+  });
+});
---
+++ packages/fakta-besteberegning/src/scenario/BesteberegningScenario.ts
@@ -0,0 +1,163 @@
+import opptjeningAktivitetType from '@fpsak-frontend/kodeverk/src/opptjeningAktivitetType';
+import { Beregningsgrunnlag, BeregningsgrunnlagAndel } from '@fpsak-frontend/types';
+
+export const besteberegninggrunnlag = {
+  besteMåneder: [{
+    fom: '2020-01-01',
+    tom: '2020-01-31',
+    inntekter: [
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 5531,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.ARBEID, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        arbeidsgiverId: '910909088',
+        inntekt: 20000,
+      },
+    ],
+  },
+  {
+    fom: '2020-02-01',
+    tom: '2020-02-29',
+    inntekter: [
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 5531,
+      },
+    ],
+  },
+  {
+    fom: '2020-03-01',
+    tom: '2020-03-31',
+    inntekter: [
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 5531,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.ARBEID, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        arbeidsgiverId: '910909088',
+        inntekt: 4321,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 354,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.ARBEID, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        arbeidsgiverId: '6823424234232',
+        inntekt: 32894,
+      },
+    ],
+  },
+  {
+    fom: '2020-04-01',
+    tom: '2020-04-30',
+    inntekter: [
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 5531,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.ARBEID, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        arbeidsgiverId: '910909088',
+        inntekt: 25322,
+      },
+    ],
+  },
+  {
+    fom: '2020-05-01',
+    tom: '2020-05-31',
+    inntekter: [
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 354,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 5531,
+      },
+    ],
+  },
+  {
+    fom: '2020-06-01',
+    tom: '2020-06-30',
+    inntekter: [
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 5531,
+      },
+      {
+        opptjeningAktivitetType: { kode: opptjeningAktivitetType.DAGPENGER, kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        inntekt: 354,
+      },
+    ],
+  },
+  ],
+};
+
+export const beregningsgrunnlag = {
+  skjaeringstidspunktBeregning: '2020-01-13',
+  skjæringstidspunkt: '2020-01-13',
+  aktivitetStatus: [{ kode: 'AT', kodeverk: 'AKTIVITET_STATUS' }, { kode: 'DP', kodeverk: 'AKTIVITET_STATUS' }],
+  beregningsgrunnlagPeriode: [{
+    beregningsgrunnlagPeriodeFom: '2020-01-13',
+    beregnetPrAar: 560000.00,
+    bruttoPrAar: 750000.00,
+    bruttoInkludertBortfaltNaturalytelsePrAar: 0,
+    avkortetPrAar: 600000.00,
+    redusertPrAar: 600000.00,
+    periodeAarsaker: [],
+    beregningsgrunnlagPrStatusOgAndel: [{
+      aktivitetStatus: { kode: 'AT', kodeverk: 'AKTIVITET_STATUS' },
+      beregningsperiodeFom: '2019-10-01',
+      beregningsperiodeTom: '2019-12-31',
+      beregnetPrAar: 360000.00,
+      andelsnr: 1,
+      besteberegningPrAar: 400000.00,
+      inntektskategori: { kode: 'ARBEIDSTAKER', kodeverk: 'INNTEKTSKATEGORI' },
+      arbeidsforhold: {
+        arbeidsgiverNavn: 'BEDRIFT AS',
+        arbeidsgiverId: '910909088',
+        startdato: '2019-02-03',
+        opphoersdato: '2020-02-03',
+        arbeidsforholdType: { kode: 'ARBEID', kodeverk: 'OPPTJENING_AKTIVITET_TYPE' },
+        belopFraInntektsmeldingPrMnd: 30000.00,
+        organisasjonstype: { kode: 'VIRKSOMHET', kodeverk: 'ORGANISASJONSTYPE' },
+      },
+      fastsattAvSaksbehandler: false,
+      lagtTilAvSaksbehandler: false,
+      belopPrMndEtterAOrdningen: 10000.0000000000,
+      belopPrAarEtterAOrdningen: 120000.000000000000,
+      erTilkommetAndel: false,
+      skalFastsetteGrunnlag: false,
+    },
+    {
+      beregningsperiodeFom: '2019-10-01',
+      beregningsperiodeTom: '2019-12-31',
+      aktivitetStatus: { kode: 'DP', kodeverk: 'AKTIVITET_STATUS' },
+      beregnetPrAar: 200000.00,
+      andelsnr: 2,
+      besteberegningPrAar: 350000.00,
+      inntektskategori: { kode: 'DAGPENGER', kodeverk: 'INNTEKTSKATEGORI' },
+      fastsattAvSaksbehandler: false,
+      lagtTilAvSaksbehandler: false,
+      belopPrMndEtterAOrdningen: 10000.0000000000,
+      belopPrAarEtterAOrdningen: 120000.000000000000,
+      erTilkommetAndel: false,
+      skalFastsetteGrunnlag: false,
+    } as BeregningsgrunnlagAndel],
+  }],
+  ytelsesspesifiktGrunnlag: {
+    besteberegninggrunnlag,
+    ytelsetype: 'FP',
+  },
+  sammenligningsgrunnlagPrStatus: [],
+  halvG: 49929.0,
+  grunnbeløp: 99858,
+  hjemmel: { kode: '-', kodeverk: 'BG_HJEMMEL' },
+  årsinntektVisningstall: 0,
+  dekningsgrad: 100,
+  erOverstyrtInntekt: false,
+} as Beregningsgrunnlag;
