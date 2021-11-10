6389cab98f8e8f17790fd602deacbaefbbd91d5f
tor-nav <tor.roneid@nav.no>
2021-11-10 10:38:44 +0100

    div
    Former-commit-id: 80c12cfab7cdc58e01bc980a90401827cac8c5e9

--- packages/sak-app/src/index.ts
+++ packages/sak-app/src/index.ts
@@ -1,1 +1,5 @@
+// @ts-ignore
+window.app2Url = () => (process.env.NODE_ENV === 'development' ? 'http://localhost:9005' : 'test');
+
+// Use dynamic import here to allow webpack to interface with module federation code
 import('./bootstrap');
