f89760021d98ba7ef27485814793baf3b78634ba
Tor Røneid <tor.roneid@nav.no>
2021-12-16 11:31:14 +0100

    NOJIRA Oppdatert dep (#2309)

--- packages/sak-infosider/src/NotFoundPage.spec.tsx
+++ packages/sak-infosider/src/NotFoundPage.spec.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './InfosiderSakIndex.stories';
 
+// @ts-ignore Fjern når fiksa
 const { SideIkkeFunnet } = composeStories(stories);
 
 describe('<NotFoundPage>', () => {
