8267597f6916c6a722fa470477f0cc92e9d92235
tor-nav <tor.roneid@nav.no>
2022-01-04 11:13:32 +0100

    div

--- packages/utils/src/dateUtils.ts
+++ packages/utils/src/dateUtils.ts
@@ -113,14 +113,17 @@ export const addDaysToDate = (dateString: string, nrOfDays: number): string => (
   : moment(dateString, ISO_DATE_FORMAT).add(nrOfDays, 'days').format(ISO_DATE_FORMAT));
 
 export const findDifferenceInMonthsAndDays = (fomDate: string, tomDate: string): { months: number; days: number; } | undefined => {
-  const fDate = dayjs(fomDate, ISO_DATE_FORMAT, true).startOf('day');
-  const tDate = dayjs(tomDate, ISO_DATE_FORMAT, true).startOf('day').add(1, 'days');
+  const fDate = dayjs(fomDate, ISO_DATE_FORMAT, true);
+  const tDate = dayjs(tomDate, ISO_DATE_FORMAT, true).add(1, 'days');
   if (!fDate.isValid() || !tDate.isValid() || fDate.isAfter(tDate)) {
     return undefined;
   }
 
   const diff = tDate.diff(fDate);
   const diffDuration = dayjs.duration(diff);
+
+  console.log(fDate);
+  console.log(tDate);
 
   return {
     months: diffDuration.months(),
