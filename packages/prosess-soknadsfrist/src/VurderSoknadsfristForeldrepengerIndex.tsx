ac14bf66484f054e0e1ad7b1543be4a1452bdd05
tor-nav <tor.roneid@nav.no>
2022-01-17 13:57:47 +0100

    div

--- packages/prosess-soknadsfrist/src/VurderSoknadsfristForeldrepengerIndex.tsx
+++ packages/prosess-soknadsfrist/src/VurderSoknadsfristForeldrepengerIndex.tsx
@@ -3,7 +3,6 @@ import { RawIntlProvider } from 'react-intl';
 
 import { Soknad, UttakPeriodeGrense, StandardProsessPanelProps } from '@fpsak-frontend/types';
 import { createIntl } from '@fpsak-frontend/utils';
-import { ReduxWrapper } from '@fpsak-frontend/form';
 
 import VurderSoknadsfristForeldrepengerForm from './components/VurderSoknadsfristForeldrepengerForm';
 import messages from '../i18n/nb_NO.json';
@@ -27,17 +26,17 @@ const VurderSoknadsfristForeldrepengerIndex: FunctionComponent<OwnProps & Standa
   setFormData,
 }) => (
   <RawIntlProvider value={intl}>
-    <ReduxWrapper formName="VurderSoknadsfristForeldrepengerIndex" formData={formData} setFormData={setFormData}>
-      <VurderSoknadsfristForeldrepengerForm
-        uttakPeriodeGrense={uttakPeriodeGrense}
-        mottattDato={soknad.mottattDato}
-        aksjonspunkter={aksjonspunkter}
-        submitCallback={submitCallback}
-        readOnly={isReadOnly}
-        readOnlySubmitButton={readOnlySubmitButton}
-        isApOpen={isAksjonspunktOpen}
-      />
-    </ReduxWrapper>
+    <VurderSoknadsfristForeldrepengerForm
+      uttakPeriodeGrense={uttakPeriodeGrense}
+      mottattDato={soknad.mottattDato}
+      aksjonspunkter={aksjonspunkter}
+      submitCallback={submitCallback}
+      readOnly={isReadOnly}
+      readOnlySubmitButton={readOnlySubmitButton}
+      isApOpen={isAksjonspunktOpen}
+      formData={formData}
+      setFormData={setFormData}
+    />
   </RawIntlProvider>
 );
 
