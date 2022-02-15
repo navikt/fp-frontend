407d75955b4a0506a5598b063b581090f96eeaa7
Tor Røneid <tor.roneid@nav.no>
2022-02-15 14:28:12 +0100

    FIX Endre variablar i bekreft-dto for permisjon (#2608)
    * FIX Endre variabel i bekreft-dto for permisjon
    
    Former-commit-id: 9a21424bea190d46fc50060661e01d57eaa6c4f3

--- packages/fakta-permisjon/src/PermisjonFaktaIndex.spec.tsx
+++ packages/fakta-permisjon/src/PermisjonFaktaIndex.spec.tsx
@@ -39,6 +39,7 @@ describe('<PermisjonFaktaIndex>', () => {
       begrunnelse: 'Dette er en begrunnelse',
       arbeidsforhold: [{
         internArbeidsforholdId: 'bc9a409c-a15f-4416-856b-5b1ee42eb75c',
+        arbeidsgiverIdent: '910909088',
         permisjonStatus: 'IKKE_BRUK_PERMISJON',
       }],
     });
@@ -70,9 +71,11 @@ describe('<PermisjonFaktaIndex>', () => {
       begrunnelse: 'Dette er en begrunnelse',
       arbeidsforhold: [{
         internArbeidsforholdId: 'bc9a409c-a15f-4416-856b-5b1ee42eb75d',
+        arbeidsgiverIdent: '910909090',
         permisjonStatus: 'IKKE_BRUK_PERMISJON',
       }, {
         internArbeidsforholdId: 'bc9a409c-a15f-4416-856b-5b1ee42eb75c',
+        arbeidsgiverIdent: '910909088',
         permisjonStatus: 'BRUK_PERMISJON',
       }],
     });
