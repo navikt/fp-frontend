9237ac3ee7c1ea74eca4bbe53140c50172febe91
tor-nav <tor.roneid@nav.no>
2022-03-04 12:58:37 +0100

    FIX i arbeid-og-inntekt
    Former-commit-id: cef028f554fbdf6304f74e83055f63990184f57a

--- packages/fakta-arbeid-og-inntekt/src/components/ArbeidsforholdInformasjonPanel.tsx
+++ packages/fakta-arbeid-og-inntekt/src/components/ArbeidsforholdInformasjonPanel.tsx
@@ -1,44 +1,29 @@
 import React, {
-  FunctionComponent, useState, useMemo, Fragment,
+  FunctionComponent, useState, useMemo,
 } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import dayjs from 'dayjs';
 import Lenke from 'nav-frontend-lenker';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import {
-  Image, FlexColumn, FlexContainer, FlexRow, Tooltip,
+  Image, FlexColumn, FlexContainer, FlexRow,
 } from '@navikt/fp-react-components';
 
-import advarselIkonUrl from '@fpsak-frontend/assets/images/advarsel2.svg';
-import { hentDokumentLenke } from '@fpsak-frontend/konstanter';
-import dokumentSvg from '@fpsak-frontend/assets/images/dokument_filled.svg';
 import pilOppIkonUrl from '@fpsak-frontend/assets/images/pil_opp.svg';
 import pilNedIkonUrl from '@fpsak-frontend/assets/images/pil_ned.svg';
-import { formatCurrencyNoKr, ISO_DATE_FORMAT, TIDENES_ENDE } from '@fpsak-frontend/utils';
+import { formatCurrencyNoKr, ISO_DATE_FORMAT } from '@fpsak-frontend/utils';
 import { AoIArbeidsforhold, Inntektsmelding, Inntektspost } from '@fpsak-frontend/types';
-import {
-  VerticalSpacer, FloatRight, PeriodLabel, AvsnittSkiller, DateTimeLabel,
-} from '@fpsak-frontend/shared-components';
+import { VerticalSpacer, FloatRight } from '@fpsak-frontend/shared-components';
 
-import { Column, Row } from 'nav-frontend-grid';
 import styles from './arbeidsforholdInformasjonPanel.less';
+import InntektsmeldingerTest from './InntektsmeldingerTest';
 
 type ForenkletInntektspost = {
   beløp: number;
   fom: string;
 }
 
-const erMatch = (
-  arbeidsforhold: AoIArbeidsforhold,
-  inntektsmelding: Inntektsmelding,
-): boolean => inntektsmelding.arbeidsgiverIdent === arbeidsforhold.arbeidsgiverIdent
-  && (!inntektsmelding.internArbeidsforholdId || inntektsmelding.internArbeidsforholdId === arbeidsforhold.internArbeidsforholdId);
-
-const delOppAId = (eksternArbeidsforholdId: string) => {
-  const lengde = Math.ceil(eksternArbeidsforholdId.length / 25);
-  const oppdeltId = Array.from({ length: lengde }, (_x, i) => eksternArbeidsforholdId.slice(i * 25, (i * 25) + 25));
-  return <p>{oppdeltId.join('-')}</p>;
-};
+const EMPTY_ARRAY = [];
 
 const behandleInntektsposter = (
   skjæringspunktDato: string,
@@ -66,7 +51,7 @@ interface OwnProps {
   skjæringspunktDato: string;
   inntektsposter?: Inntektspost[];
   arbeidsforholdForRad: AoIArbeidsforhold[];
-  inntektsmeldingerForRad: Inntektsmelding[];
+  inntektsmeldingerForRad?: Inntektsmelding[];
 }
 
 const ArbeidsforholdInformasjonPanel: FunctionComponent<OwnProps> = ({
@@ -74,9 +59,8 @@ const ArbeidsforholdInformasjonPanel: FunctionComponent<OwnProps> = ({
   skjæringspunktDato,
   inntektsposter = [],
   arbeidsforholdForRad,
-  inntektsmeldingerForRad,
+  inntektsmeldingerForRad = EMPTY_ARRAY,
 }) => {
-  const intl = useIntl();
   const [visAlleMåneder, toggleMånedvisning] = useState(false);
 
   const sorterteInntektsposter = useMemo(() => behandleInntektsposter(skjæringspunktDato, inntektsposter), [inntektsposter]);
@@ -85,107 +69,11 @@ const ArbeidsforholdInformasjonPanel: FunctionComponent<OwnProps> = ({
 
   return (
     <>
-      {!harEttArbeidsforhold && (
-        <>
-          <VerticalSpacer eightPx />
-          <AvsnittSkiller dividerParagraf />
-          <VerticalSpacer sixteenPx />
-          {arbeidsforholdForRad.map((a) => {
-            const inntektsmelding = inntektsmeldingerForRad.find((i) => erMatch(a, i));
-            return (
-              <React.Fragment key={`${a.arbeidsgiverIdent}${a.internArbeidsforholdId}`}>
-                <Row>
-                  <Column xs="8">
-                    <FlexRow>
-                      <FlexColumn>
-                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.ArbeidsforholdId" /></Element>
-                      </FlexColumn>
-                      <FlexColumn>
-                        {a.eksternArbeidsforholdId.length < 50 && (
-                          <Normaltekst>{a.eksternArbeidsforholdId}</Normaltekst>
-                        )}
-                        {a.eksternArbeidsforholdId.length >= 50 && (
-                          <Tooltip
-                            content={delOppAId(a.eksternArbeidsforholdId)}
-                            alignTop
-                          >
-                            <Normaltekst>{`${a.eksternArbeidsforholdId.substring(0, 50)}...`}</Normaltekst>
-                          </Tooltip>
-                        )}
-                      </FlexColumn>
-                    </FlexRow>
-                    <VerticalSpacer fourPx />
-                    <FlexRow>
-                      <FlexColumn>
-                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.Periode" /></Element>
-                      </FlexColumn>
-                      <FlexColumn>
-                        <Normaltekst>
-                          <PeriodLabel dateStringFom={a.fom} dateStringTom={a.tom !== TIDENES_ENDE ? a.tom : undefined} />
-                        </Normaltekst>
-                      </FlexColumn>
-                    </FlexRow>
-                    <VerticalSpacer fourPx />
-                    <FlexRow>
-                      <FlexColumn>
-                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.Stillingsprosent" /></Element>
-                      </FlexColumn>
-                      <FlexColumn>
-                        <Normaltekst>{`${a.stillingsprosent}%`}</Normaltekst>
-                      </FlexColumn>
-                    </FlexRow>
-                  </Column>
-                  {inntektsmelding && (
-                    <Column xs="4">
-                      <FloatRight>
-                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.ImMottatt" /></Element>
-                      </FloatRight>
-                      <FloatRight>
-                        <Normaltekst><DateTimeLabel dateTimeString={inntektsmelding.motattDato} useNewFormat /></Normaltekst>
-                      </FloatRight>
-                      <FloatRight>
-                        <VerticalSpacer eightPx />
-                        <Lenke href={hentDokumentLenke(saksnummer, inntektsmelding.journalpostId, inntektsmelding.dokumentId)} target="_blank">
-                          <span>
-                            <Normaltekst className={styles.inline}>
-                              <FormattedMessage id="ArbeidsforholdInformasjonPanel.ÅpneInntektsmelding" />
-                            </Normaltekst>
-                          </span>
-                          <Image src={dokumentSvg} />
-                        </Lenke>
-                      </FloatRight>
-                    </Column>
-                  )}
-                  {!inntektsmelding && (
-                    <Column xs="4">
-                      <FloatRight>
-                        <Image className={styles.aksjonpunktImage} alt={intl.formatMessage({ id: 'ArbeidsforholdRad.Aksjonspunkt' })} src={advarselIkonUrl} />
-                        <div className={styles.ikkeMottatt}>
-                          <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.ImIkkeMottatt" /></Element>
-                        </div>
-                      </FloatRight>
-                    </Column>
-                  )}
-                </Row>
-                <VerticalSpacer sixteenPx />
-                <AvsnittSkiller dividerParagraf />
-                <VerticalSpacer sixteenPx />
-              </React.Fragment>
-            );
-          })}
-        </>
-      )}
-      <VerticalSpacer eightPx />
-      {harEttArbeidsforhold && (
-        <FlexRow>
-          <FlexColumn>
-            <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.Stillingsprosent" /></Element>
-          </FlexColumn>
-          <FlexColumn>
-            <Normaltekst>{`${arbeidsforholdForRad[0].stillingsprosent}%`}</Normaltekst>
-          </FlexColumn>
-        </FlexRow>
-      )}
+      <InntektsmeldingerTest
+        saksnummer={saksnummer}
+        arbeidsforholdForRad={arbeidsforholdForRad}
+        inntektsmeldingerForRad={inntektsmeldingerForRad}
+      />
       <VerticalSpacer thirtyTwoPx />
       {inntektsposter.length > 0 && (
         <>
---
+++ packages/fakta-arbeid-og-inntekt/src/components/InntektsmeldingerTest.tsx
@@ -0,0 +1,204 @@
+import React, {
+  FunctionComponent, useState,
+} from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import Lenke from 'nav-frontend-lenker';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
+import {
+  Image, FlexColumn, FlexRow, Tooltip,
+} from '@navikt/fp-react-components';
+
+import pilOppIkonUrl from '@fpsak-frontend/assets/images/pil_opp.svg';
+import pilNedIkonUrl from '@fpsak-frontend/assets/images/pil_ned.svg';
+import advarselIkonUrl from '@fpsak-frontend/assets/images/advarsel2.svg';
+import { TIDENES_ENDE } from '@fpsak-frontend/utils';
+import { AoIArbeidsforhold, Inntektsmelding } from '@fpsak-frontend/types';
+import {
+  VerticalSpacer, FloatRight, PeriodLabel, AvsnittSkiller, DateTimeLabel,
+} from '@fpsak-frontend/shared-components';
+
+import { Column, Row } from 'nav-frontend-grid';
+import styles from './inntektsmeldingerTest.less';
+import InntektsmeldingOpplysningerPanel from './InntektsmeldingOpplysningerPanel';
+
+const erMatch = (
+  arbeidsforhold: AoIArbeidsforhold,
+  inntektsmelding: Inntektsmelding,
+): boolean => inntektsmelding.arbeidsgiverIdent === arbeidsforhold.arbeidsgiverIdent
+  && (!inntektsmelding.internArbeidsforholdId || inntektsmelding.internArbeidsforholdId === arbeidsforhold.internArbeidsforholdId);
+
+const finnInntektsmelding = (
+  inntektsmeldinger: Inntektsmelding[],
+  internArbeidsforholdId?: string,
+) => {
+  const harImMedId = inntektsmeldinger.some((i) => i.internArbeidsforholdId);
+  const harImUtenId = inntektsmeldinger.some((i) => !i.internArbeidsforholdId);
+  if (harImMedId && harImUtenId) {
+    throw Error('Har inntektsmelding både med og uten id');
+  }
+
+  return inntektsmeldinger.find((i) => !i.internArbeidsforholdId || i.internArbeidsforholdId === internArbeidsforholdId);
+};
+
+const delOppAId = (eksternArbeidsforholdId: string) => {
+  const lengde = Math.ceil(eksternArbeidsforholdId.length / 25);
+  const oppdeltId = Array.from({ length: lengde }, (_x, i) => eksternArbeidsforholdId.slice(i * 25, (i * 25) + 25));
+  return <p>{oppdeltId.join('-')}</p>;
+};
+
+interface OwnProps {
+  saksnummer: string;
+  arbeidsforholdForRad: AoIArbeidsforhold[];
+  inntektsmeldingerForRad: Inntektsmelding[];
+}
+
+const InntektsmeldingerTest: FunctionComponent<OwnProps> = ({
+  saksnummer,
+  arbeidsforholdForRad,
+  inntektsmeldingerForRad,
+}) => {
+  const intl = useIntl();
+  const [visInfoOmIm, toggleInfoOmIm] = useState({});
+
+  const harEttArbeidsforhold = arbeidsforholdForRad.length === 1;
+
+  return (
+    <>
+      {!harEttArbeidsforhold && (
+        <>
+          <VerticalSpacer eightPx />
+          <AvsnittSkiller dividerParagraf />
+          <VerticalSpacer sixteenPx />
+          {arbeidsforholdForRad.map((a) => {
+            const inntektsmelding = inntektsmeldingerForRad.find((i) => erMatch(a, i));
+            return (
+              <React.Fragment key={`${a.arbeidsgiverIdent}${a.internArbeidsforholdId}`}>
+                <Row>
+                  <Column xs="8">
+                    <FlexRow>
+                      <FlexColumn>
+                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.ArbeidsforholdId" /></Element>
+                      </FlexColumn>
+                      <FlexColumn>
+                        {a.eksternArbeidsforholdId.length < 50 && (
+                          <Normaltekst>{a.eksternArbeidsforholdId}</Normaltekst>
+                        )}
+                        {a.eksternArbeidsforholdId.length >= 50 && (
+                          <Tooltip
+                            content={delOppAId(a.eksternArbeidsforholdId)}
+                            alignTop
+                          >
+                            <Normaltekst>{`${a.eksternArbeidsforholdId.substring(0, 50)}...`}</Normaltekst>
+                          </Tooltip>
+                        )}
+                      </FlexColumn>
+                    </FlexRow>
+                    <VerticalSpacer fourPx />
+                    <FlexRow>
+                      <FlexColumn>
+                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.Periode" /></Element>
+                      </FlexColumn>
+                      <FlexColumn>
+                        <Normaltekst>
+                          <PeriodLabel dateStringFom={a.fom} dateStringTom={a.tom !== TIDENES_ENDE ? a.tom : undefined} />
+                        </Normaltekst>
+                      </FlexColumn>
+                    </FlexRow>
+                    <VerticalSpacer fourPx />
+                    <FlexRow>
+                      <FlexColumn>
+                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.Stillingsprosent" /></Element>
+                      </FlexColumn>
+                      <FlexColumn>
+                        <Normaltekst>{`${a.stillingsprosent}%`}</Normaltekst>
+                      </FlexColumn>
+                    </FlexRow>
+                    {inntektsmelding && (
+                      <>
+                        {visInfoOmIm[inntektsmelding.internArbeidsforholdId] && (
+                          <InntektsmeldingOpplysningerPanel
+                            saksnummer={saksnummer}
+                            inntektsmelding={inntektsmelding}
+                            skalViseArbeidsforholdId={false}
+                          />
+                        )}
+                        <VerticalSpacer fourPx />
+                        <Lenke
+                          onClick={(e) => {
+                            e.preventDefault();
+                            toggleInfoOmIm((info) => {
+                              const status = info[inntektsmelding.internArbeidsforholdId];
+                              return {
+                                ...info,
+                                [inntektsmelding.internArbeidsforholdId]: status === undefined || status === false,
+                              };
+                            });
+                          }}
+                          href=""
+                        >
+                          <span>
+                            <Normaltekst className={styles.inline}>
+                              <FormattedMessage
+                                id={!visInfoOmIm[inntektsmelding.internArbeidsforholdId]
+                                  ? 'ArbeidsforholdInformasjonPanel.ApneImInfo' : 'ArbeidsforholdInformasjonPanel.LukkeImInfo'}
+                              />
+                            </Normaltekst>
+                          </span>
+                          <Image className={styles.arrow} src={visInfoOmIm[inntektsmelding.internArbeidsforholdId] ? pilOppIkonUrl : pilNedIkonUrl} />
+                        </Lenke>
+                      </>
+                    )}
+                  </Column>
+                  {inntektsmelding && (
+                    <Column xs="4">
+                      <FloatRight>
+                        <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.ImMottatt" /></Element>
+                      </FloatRight>
+                      <FloatRight>
+                        <Normaltekst><DateTimeLabel dateTimeString={inntektsmelding.motattDato} useNewFormat /></Normaltekst>
+                      </FloatRight>
+                    </Column>
+                  )}
+                  {!inntektsmelding && (
+                    <Column xs="4">
+                      <FloatRight>
+                        <Image className={styles.aksjonpunktImage} alt={intl.formatMessage({ id: 'ArbeidsforholdRad.Aksjonspunkt' })} src={advarselIkonUrl} />
+                        <div className={styles.ikkeMottatt}>
+                          <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.ImIkkeMottatt" /></Element>
+                        </div>
+                      </FloatRight>
+                    </Column>
+                  )}
+                </Row>
+                <VerticalSpacer sixteenPx />
+                <AvsnittSkiller dividerParagraf />
+                <VerticalSpacer sixteenPx />
+              </React.Fragment>
+            );
+          })}
+        </>
+      )}
+      <VerticalSpacer eightPx />
+      {harEttArbeidsforhold && inntektsmeldingerForRad.length > 0 && (
+        <InntektsmeldingOpplysningerPanel
+          saksnummer={saksnummer}
+          stillingsprosent={arbeidsforholdForRad[0].stillingsprosent}
+          inntektsmelding={finnInntektsmelding(inntektsmeldingerForRad, arbeidsforholdForRad[0].internArbeidsforholdId)}
+          skalViseArbeidsforholdId={inntektsmeldingerForRad.length > 1}
+        />
+      )}
+      {harEttArbeidsforhold && inntektsmeldingerForRad.length === 0 && (
+        <FlexRow>
+          <FlexColumn>
+            <Element><FormattedMessage id="ArbeidsforholdInformasjonPanel.Stillingsprosent" /></Element>
+          </FlexColumn>
+          <FlexColumn>
+            <Normaltekst>{`${arbeidsforholdForRad[0].stillingsprosent}%`}</Normaltekst>
+          </FlexColumn>
+        </FlexRow>
+      )}
+    </>
+  );
+};
+
+export default InntektsmeldingerTest;
