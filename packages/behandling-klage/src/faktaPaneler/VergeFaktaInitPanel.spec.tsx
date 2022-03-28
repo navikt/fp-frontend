4333f7b54bcde9529d651a7974e5c31dd9a3d758
Tor Røneid <tor.roneid@nav.no>
2022-03-28 11:11:33 +0200

    Legg til opprett/fjern verge for anke og klage (#2771)
    * Legg til opprett/fjern verge for anke og klage

---
+++ packages/behandling-klage/src/faktaPaneler/VergeFaktaInitPanel.spec.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
+import VergeFaktaIndex from '@fpsak-frontend/fakta-verge';
+import { FaktaDefaultInitPanel, FaktaDefaultInitPanelProps, FaktaPanelInitProps } from '@fpsak-frontend/behandling-felles';
+import { Aksjonspunkt } from '@fpsak-frontend/types';
+
+import VergeFaktaInitPanel from './VergeFaktaInitPanel';
+
+type INIT_DATA = {
+  aksjonspunkter: Aksjonspunkt[];
+}
+
+describe('<VergeFaktaInitPanel>', () => {
+  it('skal rendre komponent', () => {
+    const wrapper = shallow(<VergeFaktaInitPanel
+      valgtFaktaSteg="default"
+      registrerFaktaPanel={() => {}}
+    />);
+
+    const panel = wrapper.find<FaktaDefaultInitPanelProps<INIT_DATA, any> & FaktaPanelInitProps>(FaktaDefaultInitPanel);
+
+    const aksjonspunkter = [{
+      definisjon: aksjonspunktCodes.AVKLAR_VERGE,
+    }] as Aksjonspunkt[];
+
+    expect(panel.props().skalPanelVisesIMeny({ aksjonspunkter })).toBe(true);
+    expect(panel.props().skalPanelVisesIMeny({})).toBe(false);
+    expect(panel.props().renderPanel({}).type).toEqual(VergeFaktaIndex);
+  });
+});
