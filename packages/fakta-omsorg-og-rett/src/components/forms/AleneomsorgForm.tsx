52fe86bd70587aff28cf7e916091a5af9bf3bfe7
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-05-27 09:12:07 +0200

    Vent med EØS til august

--- packages/fakta-omsorg-og-rett/src/components/forms/AleneomsorgForm.tsx
+++ packages/fakta-omsorg-og-rett/src/components/forms/AleneomsorgForm.tsx
@@ -58,7 +58,7 @@ const AleneomsorgForm: FunctionComponent<OwnProps> = ({
 
   const harAleneomsorg = formMethods.watch('harAleneomsorg');
   const skalAvklareUforetrygd = true;
-  const skalAvklareStonadEOS = true;
+  const skalAvklareStonadEOS = false; // TODO (WLB) enable i august 2022
 
   const transformerFeltverdier = useCallback((feltVerdier: FormValues) => lagreCallback({
     kode: AksjonspunktCode.MANUELL_KONTROLL_AV_OM_BRUKER_HAR_ALENEOMSORG,
