d0933407984dda83f94d716d623285883d139599
tor-nav <tor.roneid@nav.no>
2022-07-01 10:55:19 +0200

    div

--- packages/fakta-medlemskap/src/MedlemskapFaktaIndex.spec.tsx
+++ packages/fakta-medlemskap/src/MedlemskapFaktaIndex.spec.tsx
@@ -7,7 +7,7 @@ import AksjonspunktCode from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import * as stories from './MedlemskapFaktaIndex.stories';
 
 const {
-  AvklarOmBrukerErBosatt, AvklarOmBrukerHarGyldigPeriode, AvklarOmBrukerHarOppholdsrett, AvklarOmBrukerHarLovligOpphold,
+  AvklarOmBrukerErBosatt, AvklarOmBrukerHarGyldigPeriode, AvklarOmBrukerHarOppholdsrett, AvklarOmBrukerHarLovligOpphold, AvklarFortsattMedlemskap,
 } = composeStories(stories);
 
 describe('<MedlemskapFaktaIndex>', () => {
@@ -192,6 +192,146 @@ describe('<MedlemskapFaktaIndex>', () => {
         årsaker: ['SKJÆRINGSTIDSPUNKT'],
       }],
       kode: AksjonspunktCode.AVKLAR_LOVLIG_OPPHOLD,
+    }]);
+  });
+
+  it('skal løse aksjonspunkt for flere perioder', async () => {
+    const lagreVurdering = jest.fn(() => Promise.resolve());
+
+    render(<AvklarFortsattMedlemskap submitCallback={lagreVurdering} />);
+
+    expect(await screen.findByText('Fakta om medlemskap')).toBeInTheDocument();
+    expect(screen.getByText('Vurder om søker fortsatt har gyldig medlemskap i perioden')).toBeInTheDocument();
+    expect(screen.getByText('Vurder om søker er bosatt i Norge')).toBeInTheDocument();
+    expect(screen.getByText('Avklar om søker har lovlig opphold')).toBeInTheDocument();
+
+    expect(screen.getAllByText('SKJÆRINGSTIDSPUNKT')).toHaveLength(4);
+
+    expect(screen.getByText('Periode - 05.10.2017')).toBeInTheDocument();
+
+    expect(screen.getByText('Bekreft og fortsett')).toBeDisabled();
+    expect(screen.getByText('Oppdater')).toBeDisabled();
+    expect(screen.getByText('Avbryt')).toBeEnabled();
+
+    userEvent.click(screen.getAllByText('Søker har lovlig opphold')[0]);
+
+    userEvent.type(screen.getByText('Begrunn endringene'), 'Dette er en begrunnelse');
+
+    expect(screen.getByText('Bekreft og fortsett')).toBeDisabled();
+
+    userEvent.click(screen.getByText('Oppdater'));
+
+    expect(await screen.findByText('Periode - 07.11.2019')).toBeInTheDocument();
+
+    expect(screen.getByText('Bekreft og fortsett')).toBeDisabled();
+    expect(screen.getByText('Oppdater')).toBeDisabled();
+    expect(screen.getByText('Avbryt')).toBeEnabled();
+
+    userEvent.click(screen.getAllByText('Søker er bosatt i Norge')[0]);
+
+    userEvent.type(screen.getByText('Begrunn endringene'), 'Dette er en begrunnelse');
+
+    expect(screen.getByText('Bekreft og fortsett')).toBeDisabled();
+
+    userEvent.click(screen.getByText('Oppdater'));
+
+    expect(await screen.findByText('Bekreft og fortsett')).toBeEnabled();
+
+    expect(screen.queryByText('Opplysninger oppgitt i søknaden')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByText('Bekreft og fortsett'));
+
+    await waitFor(() => expect(lagreVurdering).toHaveBeenCalledTimes(1));
+    expect(lagreVurdering).toHaveBeenNthCalledWith(1, [{
+      begrunnelse: '',
+      bekreftedePerioder: [{
+        aksjonspunkter: [AksjonspunktCode.AVKLAR_OM_BRUKER_ER_BOSATT],
+        begrunnelse: 'Dette er en begrunnelse',
+        bosattVurdering: true,
+        medlemskapManuellVurderingType: undefined,
+        personopplysningBruker: {
+          adresser: [{
+            adresseType: 'BOSTEDSADRESSE',
+            adresselinje1: 'Skogvegen 3',
+            land: 'NOR',
+            postNummer: '4353',
+            poststed: 'Klepp Stasjon',
+          }],
+          personstatus: 'BOSA',
+          region: 'NORDEN',
+        },
+        vurderingsdato: '2019-11-07',
+        årsaker: ['SKJÆRINGSTIDSPUNKT'],
+      }],
+      kode: AksjonspunktCode.AVKLAR_OM_BRUKER_ER_BOSATT,
+    }, {
+      begrunnelse: '',
+      bekreftedePerioder: [{
+        aksjonspunkter: [AksjonspunktCode.AVKLAR_LOVLIG_OPPHOLD],
+        begrunnelse: 'Dette er en begrunnelse',
+        bosattVurdering: undefined,
+        erEosBorger: false,
+        lovligOppholdVurdering: true,
+        medlemskapManuellVurderingType: undefined,
+        oppholdsrettVurdering: undefined,
+        personopplysningBruker: {
+          adresser: [{
+            adresseType: 'BOSTEDSADRESSE',
+            adresselinje1: 'Skogvegen 3',
+            land: 'NOR',
+            postNummer: '4353',
+            poststed: 'Klepp Stasjon',
+          }],
+          personstatus: 'BOSA',
+          region: 'NORDEN',
+        },
+        vurderingsdato: '2017-10-05',
+        årsaker: ['SKJÆRINGSTIDSPUNKT'],
+      }],
+      kode: AksjonspunktCode.AVKLAR_LOVLIG_OPPHOLD,
+    }, {
+      begrunnelse: '',
+      bekreftedePerioder: [{
+        aksjonspunkter: [AksjonspunktCode.AVKLAR_LOVLIG_OPPHOLD],
+        begrunnelse: 'Dette er en begrunnelse',
+        bosattVurdering: undefined,
+        erEosBorger: false,
+        lovligOppholdVurdering: true,
+        medlemskapManuellVurderingType: undefined,
+        oppholdsrettVurdering: undefined,
+        personopplysningBruker: {
+          adresser: [{
+            adresseType: 'BOSTEDSADRESSE',
+            adresselinje1: 'Skogvegen 3',
+            land: 'NOR',
+            postNummer: '4353',
+            poststed: 'Klepp Stasjon',
+          }],
+          personstatus: 'BOSA',
+          region: 'NORDEN',
+        },
+        vurderingsdato: '2017-10-05',
+        årsaker: ['SKJÆRINGSTIDSPUNKT'],
+      }, {
+        aksjonspunkter: [AksjonspunktCode.AVKLAR_OM_BRUKER_ER_BOSATT],
+        begrunnelse: 'Dette er en begrunnelse',
+        bosattVurdering: true,
+        medlemskapManuellVurderingType: undefined,
+        personopplysningBruker: {
+          adresser: [{
+            adresseType: 'BOSTEDSADRESSE',
+            adresselinje1: 'Skogvegen 3',
+            land: 'NOR',
+            postNummer: '4353',
+            poststed: 'Klepp Stasjon',
+          }],
+          personstatus: 'BOSA',
+          region: 'NORDEN',
+        },
+        vurderingsdato: '2019-11-07',
+        årsaker: ['SKJÆRINGSTIDSPUNKT'],
+      }],
+      kode: AksjonspunktCode.AVKLAR_FORTSATT_MEDLEMSKAP,
     }]);
   });
 });
