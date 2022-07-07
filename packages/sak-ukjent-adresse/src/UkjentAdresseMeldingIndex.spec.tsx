fb717b332c394b1cf7e99d10b19e8240f9290fd2
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-07-07 14:36:33 +0200

    TFP-4518: Up-front advarsel til sbh at brukeren har ikke en registrert adresse. (#3300)
    Former-commit-id: bb6f19470f8f8788952ea87a5fd13ef95572b485

---
+++ packages/sak-ukjent-adresse/src/UkjentAdresseMeldingIndex.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import * as stories from './UkjentAdresseMeldingIndex.stories';
+
+const { IngenAdresse } = composeStories(stories);
+
+describe('<IngenAdresse>', () => {
+  it('skal vise panelet om brukeren mangler adresse', async () => {
+    render(<IngenAdresse />);
+    expect(await screen.findByText('Brukeren har ikke en registrert adresse. Utsendelse av brev kan feile om brukeren ikke er digital.')).toBeInTheDocument();
+  });
+});
