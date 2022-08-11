7a5af059238645569d36bf16783438f5255df910
Petter Ekern <petter.ekern@nav.no>
2022-08-11 09:30:41 +0200

    FIX: Støtte for flere bg i fordelpanel

--- packages/behandling-fp/src/faktaPaneler/FordelingFaktaInitPanel.tsx
+++ packages/behandling-fp/src/faktaPaneler/FordelingFaktaInitPanel.tsx
@@ -1,14 +1,15 @@
 import React, {
   FunctionComponent,
 } from 'react';
-import { createIntl } from '@navikt/ft-utils';
+import { createIntl, TIDENES_ENDE } from '@navikt/ft-utils';
 
 import { FaktaFordelBeregningAksjonspunktCode } from '@navikt/ft-fakta-fordel-beregningsgrunnlag';
-import { Beregningsgrunnlag } from '@navikt/ft-types';
+import { Beregningsgrunnlag, Vilkar, Vilkarperiode } from '@navikt/ft-types';
 import { FaktaPanelCode } from '@fpsak-frontend/konstanter';
-import { Aksjonspunkt, ArbeidsgiverOpplysningerPerId } from '@fpsak-frontend/types';
+import { Aksjonspunkt, ArbeidsgiverOpplysningerPerId, Vilkar as FpVilkar } from '@fpsak-frontend/types';
 import { FaktaPanelInitProps, FaktaDefaultInitPanel, DynamicLoader } from '@fpsak-frontend/behandling-felles';
 
+import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
 import messages from '../../i18n/nb_NO.json';
 import { FpBehandlingApiKeys, requestFpApi } from '../data/fpBehandlingApi';
 
@@ -16,6 +17,54 @@ import { FpBehandlingApiKeys, requestFpApi } from '../data/fpBehandlingApi';
 import '@navikt/ft-fakta-fordel-beregningsgrunnlag/dist/style.css';
 
 const ProsessFordeling = React.lazy(() => import('@navikt/ft-fakta-fordel-beregningsgrunnlag'));
+
+const lagModifisertCallback = (
+  submitCallback: (params: any, keepData?: boolean) => Promise<any>,
+) => (aksjonspunkterSomSkalLagres: any | any[]) => {
+  const apListe = Array.isArray(aksjonspunkterSomSkalLagres) ? aksjonspunkterSomSkalLagres : [aksjonspunkterSomSkalLagres];
+  const transformerteData = apListe.map((apData) => ({
+    kode: apData.kode,
+    ...apData.grunnlag[0],
+  }));
+  return submitCallback(transformerteData);
+};
+
+const lagStandardPeriode = (beregningsgrunnlag: Beregningsgrunnlag, bgVilkar: FpVilkar): Vilkarperiode => ({
+  avslagKode: bgVilkar.avslagKode,
+  vurderesIBehandlingen: true,
+  merknadParametere: {},
+  periode: {
+    fom: beregningsgrunnlag ? beregningsgrunnlag.skjaeringstidspunktBeregning : '',
+    tom: TIDENES_ENDE,
+  },
+  vilkarStatus: bgVilkar.vilkarStatus,
+});
+
+const lagBGVilkar = (vilkar: FpVilkar[], beregningsgrunnlag: Beregningsgrunnlag): Vilkar | null => {
+  if (!vilkar) {
+    return null;
+  }
+  const bgVilkar = vilkar.find((v) => v.vilkarType && v.vilkarType === vilkarType.BEREGNINGSGRUNNLAGVILKARET);
+  if (!bgVilkar || !beregningsgrunnlag) {
+    return null;
+  }
+  const nyVK = {
+    ...bgVilkar,
+    perioder: [lagStandardPeriode(beregningsgrunnlag, bgVilkar)],
+  };
+  return nyVK;
+};
+
+const lagFormatertBG = (beregningsgrunnlag: Beregningsgrunnlag): Beregningsgrunnlag[] => {
+  if (!beregningsgrunnlag) {
+    return [];
+  }
+  const nyttBG = {
+    vilkårsperiodeFom: beregningsgrunnlag.skjaeringstidspunktBeregning,
+    ...beregningsgrunnlag,
+  };
+  return [nyttBG];
+};
 
 const ProsessFordelingMF = process.env.NODE_ENV !== 'development' ? undefined
   // eslint-disable-next-line import/no-unresolved
@@ -25,9 +74,10 @@ const intl = createIntl(messages);
 
 const AKSJONSPUNKT_KODER = [FaktaFordelBeregningAksjonspunktCode.FORDEL_BEREGNINGSGRUNNLAG, FaktaFordelBeregningAksjonspunktCode.VURDER_REFUSJON_BERGRUNN];
 
-const ENDEPUNKTER_INIT_DATA = [FpBehandlingApiKeys.AKSJONSPUNKTER];
+const ENDEPUNKTER_INIT_DATA = [FpBehandlingApiKeys.AKSJONSPUNKTER, FpBehandlingApiKeys.VILKAR];
 type EndepunktInitData = {
   aksjonspunkter: Aksjonspunkt[];
+  vilkar: Vilkar[];
 }
 
 const ENDEPUNKTER_PANEL_DATA = [FpBehandlingApiKeys.BEREGNINGSGRUNNLAG];
@@ -60,8 +110,11 @@ const FordelingFaktaInitPanel: FunctionComponent<OwnProps & FaktaPanelInitProps>
       <DynamicLoader<React.ComponentProps<typeof ProsessFordeling>>
         packageCompFn={() => import('@navikt/ft-fakta-fordel-beregningsgrunnlag')}
         federatedCompFn={ProsessFordelingMF}
-        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
         {...data}
+        beregningsgrunnlagVilkår={lagBGVilkar(data.vilkar, data.beregningsgrunnlag)}
+        beregningsgrunnlagListe={lagFormatertBG(data.beregningsgrunnlag)}
+        submitCallback={lagModifisertCallback(data.submitCallback)}
+        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
       />
     )}
   />
