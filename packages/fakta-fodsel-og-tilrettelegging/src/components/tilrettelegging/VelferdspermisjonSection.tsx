cfa71d5d4d1b50b1ce9aa21d7a7753e75dc3a131
Tor Røneid <tor.roneid@nav.no>
2022-08-29 08:18:58 +0200

    Feature/fakta fødsel og tilrettelegging (#3471)
    * Ta i bruk nytt form- og test-rammeverk for fakta-fødsel-og-tilrettelegging
    
    Former-commit-id: 7646e253ae16f18368de250b106121eb32e1107a

--- packages/fakta-fodsel-og-tilrettelegging/src/components/tilrettelegging/VelferdspermisjonSection.tsx
+++ packages/fakta-fodsel-og-tilrettelegging/src/components/tilrettelegging/VelferdspermisjonSection.tsx
@@ -1,36 +1,42 @@
 import React, { FunctionComponent } from 'react';
-import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Normaltekst } from 'nav-frontend-typografi';
 import moment from 'moment';
 import { DDMMYYYY_DATE_FORMAT } from '@navikt/ft-utils';
 import { required } from '@navikt/ft-form-validators';
-import { RadioGroupField, RadioOption } from '@fpsak-frontend/form';
-
+import { RadioGroupField, RadioOption, formHooks } from '@navikt/ft-form-hooks';
 import { VerticalSpacer } from '@navikt/ft-ui-komponenter';
+
 import { Permisjon } from '@fpsak-frontend/types';
 
-import { formValueSelector } from 'redux-form';
 import styles from './tilretteleggingArbeidsforholdSection.less';
 
-interface PureOwnProps {
-  formName: string;
+export const finnSkalTaHensynTilPermisjon = (
+  tilretteleggingBehovFom: string,
+  permisjon: Permisjon,
+): boolean => !moment(permisjon.permisjonFom)
+  .isAfter(tilretteleggingBehovFom) && (permisjon.permisjonTom == null || !moment(permisjon.permisjonTom).isBefore(tilretteleggingBehovFom));
+
+interface OwnProps {
   readOnly: boolean;
   permisjon: Permisjon;
   formSectionName: string;
 }
 
-interface MappedOwnProps {
-  skalTaHensynTilPermisjon: boolean;
-}
-
-export const finnPermisjonFieldName = (permisjon: Permisjon): string => `permisjon${permisjon.permisjonFom}`;
-
-const VelferdspermisjonSection: FunctionComponent<PureOwnProps & MappedOwnProps> = ({
+const VelferdspermisjonSection: FunctionComponent<OwnProps> = ({
   readOnly,
   permisjon,
-  skalTaHensynTilPermisjon,
+  formSectionName,
 }) => {
+  const intl = useIntl();
+
+  const {
+    watch,
+  } = formHooks.useFormContext();
+
+  const tilretteleggingBehovFom = watch(`${formSectionName}.tilretteleggingBehovFom`);
+  const skalTaHensynTilPermisjon = finnSkalTaHensynTilPermisjon(tilretteleggingBehovFom, permisjon);
+
   if (!skalTaHensynTilPermisjon) {
     return null;
   }
@@ -59,24 +65,17 @@ const VelferdspermisjonSection: FunctionComponent<PureOwnProps & MappedOwnProps>
       </Normaltekst>
       <VerticalSpacer sixteenPx />
       <RadioGroupField
-        name={finnPermisjonFieldName(permisjon)}
+        name={`${formSectionName}.permisjon${permisjon.permisjonFom}`}
         validate={[required]}
         bredde="XL"
         readOnly={readOnly}
+        parse={(value: string) => value === 'true'}
       >
-        <RadioOption value label={{ id: 'VelferdspermisjonSection.ErGyldig' }} />
-        <RadioOption value={false} label={{ id: 'VelferdspermisjonSection.ErIkkeGyldig' }} />
+        <RadioOption value="true" label={intl.formatMessage({ id: 'VelferdspermisjonSection.ErGyldig' })} />
+        <RadioOption value="false" label={intl.formatMessage({ id: 'VelferdspermisjonSection.ErIkkeGyldig' })} />
       </RadioGroupField>
     </>
   );
 };
 
-export const finnSkalTaHensynTilPermisjon = (tilretteleggingBehovFom: string, permisjon: Permisjon): boolean => !moment(permisjon.permisjonFom)
-  .isAfter(tilretteleggingBehovFom) && (permisjon.permisjonTom == null || !moment(permisjon.permisjonTom).isBefore(tilretteleggingBehovFom));
-
-const mapStateToProps = (state, ownProps: PureOwnProps): MappedOwnProps => ({
-  skalTaHensynTilPermisjon: finnSkalTaHensynTilPermisjon(formValueSelector(ownProps.formName)(state,
-    `${ownProps.formSectionName}.tilretteleggingBehovFom`), ownProps.permisjon),
-});
-
-export default connect(mapStateToProps)(VelferdspermisjonSection);
+export default VelferdspermisjonSection;
