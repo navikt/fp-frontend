397be210b858f2b844d88e451d22c8beff6c0525
tor-nav <tor.roneid@nav.no>
2022-10-05 13:42:42 +0200

    div

--- packages/utils-test/src/setup-test-env.ts
+++ packages/utils-test/src/setup-test-env.ts
@@ -7,3 +7,13 @@ configureEnzyme({ adapter: new Adapter() });
 
 // TODO Testane bør ikkje vera så treige!
 jest.setTimeout(15000);
+
+// Skjul warning fra echarts
+const originalWarn = console.warn.bind(console.warn);
+beforeAll(() => {
+  console.warn = (msg) => !msg.toString().includes('Can\'t get DOM width or height.') && originalWarn(msg);
+});
+afterAll(() => {
+  console.warn = originalWarn;
+});
+
