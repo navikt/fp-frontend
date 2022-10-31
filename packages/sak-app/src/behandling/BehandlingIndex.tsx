0df0eab28bfac18a5fc263cfd41b4dc8a738799e
Tor Røneid <tor.roneid@nav.no>
2022-10-31 12:53:33 +0100

    Test bygging etter at Espen endra oppsett (#3761)

--- packages/sak-app/src/behandling/BehandlingIndex.tsx
+++ packages/sak-app/src/behandling/BehandlingIndex.tsx
@@ -85,6 +85,7 @@ const BehandlingIndex: FunctionComponent<OwnProps> = ({
   });
 
   const fagsak = fagsakData.getFagsak();
+
   const alleBehandlinger = fagsakData.getAlleBehandlinger();
   const behandling = alleBehandlinger.find((b) => b.uuid === behandlingUuid);
 
