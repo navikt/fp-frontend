c4d7d5b5b8964aa7f800044a2e1cd4b383b9d051
Petter Ekern <petter.ekern@nav.no>
2022-11-03 15:37:16 +0100

    Revert "Tar i bruk fakta om beregning som pakke (#3773)"
    This reverts commit 7e8f1ef5602354082a77fee3d1b12d0644e746b0.

--- packages/sak-app/src/behandling/forstegangsoknadOgRevurdering/paneler/fakta/BeregningFaktaInitPanel.tsx
+++ packages/sak-app/src/behandling/forstegangsoknadOgRevurdering/paneler/fakta/BeregningFaktaInitPanel.tsx
@@ -4,77 +4,15 @@ import React, {
 import { useIntl } from 'react-intl';
 
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
+import BeregningFaktaIndex from '@fpsak-frontend/fakta-beregning';
 import { FaktaPanelCode } from '@fpsak-frontend/konstanter';
 import {
-  AksessRettigheter, Aksjonspunkt, ArbeidsgiverOpplysningerPerId, Vilkar as FpVilkar,
+  AksessRettigheter, Aksjonspunkt, ArbeidsgiverOpplysningerPerId, Beregningsgrunnlag,
 } from '@fpsak-frontend/types';
 
-import {
-  Vilkar, Vilkarperiode, Beregningsgrunnlag,
-} from '@navikt/ft-types';
-import { TIDENES_ENDE } from '@navikt/ft-utils';
-import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
-import DynamicLoader from '../../../felles/DynamicLoader';
 import FaktaPanelInitProps from '../../../felles/typer/faktaPanelInitProps';
 import { BehandlingFellesApiKeys } from '../../../felles/data/behandlingFellesApi';
 import FaktaDefaultInitPanel from '../../../felles/fakta/FaktaDefaultInitPanel';
-
-// TODO Denne burde ligga sånn til at den kun blir importert når denne pakka dynamisk blir importert
-import '@navikt/ft-fakta-beregning/dist/style.css';
-
-const ProsessFaktaBeregning = React.lazy(() => import('@navikt/ft-fakta-beregning'));
-
-const ProsessFaktaBeregningMF = process.env.NODE_ENV !== 'development' ? undefined
-  // eslint-disable-next-line import/no-unresolved
-  : () => import('ft_fakta_beregning/FaktaBeregning');
-
-const lagModifisertCallback = (
-  submitCallback: (params: any, keepData?: boolean) => Promise<any>,
-) => (aksjonspunkterSomSkalLagres: any | any[]) => {
-  const apListe = Array.isArray(aksjonspunkterSomSkalLagres) ? aksjonspunkterSomSkalLagres : [aksjonspunkterSomSkalLagres];
-  const transformerteData = apListe.map((apData) => ({
-    kode: apData.kode,
-    ...apData.grunnlag[0],
-  }));
-  return submitCallback(transformerteData);
-};
-
-const lagStandardPeriode = (beregningsgrunnlag: Beregningsgrunnlag, bgVilkar: FpVilkar): Vilkarperiode => ({
-  avslagKode: bgVilkar.avslagKode,
-  vurderesIBehandlingen: true,
-  merknadParametere: {},
-  periode: {
-    fom: beregningsgrunnlag ? beregningsgrunnlag.skjaeringstidspunktBeregning : '',
-    tom: TIDENES_ENDE,
-  },
-  vilkarStatus: bgVilkar.vilkarStatus,
-});
-
-const lagBGVilkar = (vilkar: FpVilkar[], beregningsgrunnlag: Beregningsgrunnlag): Vilkar | null => {
-  if (!vilkar) {
-    return null;
-  }
-  const bgVilkar = vilkar.find((v) => v.vilkarType && v.vilkarType === vilkarType.BEREGNINGSGRUNNLAGVILKARET);
-  if (!bgVilkar || !beregningsgrunnlag) {
-    return null;
-  }
-  const nyVK = {
-    ...bgVilkar,
-    perioder: [lagStandardPeriode(beregningsgrunnlag, bgVilkar)],
-  };
-  return nyVK;
-};
-
-const lagFormatertBG = (beregningsgrunnlag: Beregningsgrunnlag): Beregningsgrunnlag[] => {
-  if (!beregningsgrunnlag) {
-    return [];
-  }
-  const nyttBG = {
-    vilkårsperiodeFom: beregningsgrunnlag.skjaeringstidspunktBeregning,
-    ...beregningsgrunnlag,
-  };
-  return [nyttBG];
-};
 
 const AKSJONSPUNKT_KODER = [
   aksjonspunktCodes.VURDER_FAKTA_FOR_ATFL_SN,
@@ -85,12 +23,10 @@ const AKSJONSPUNKT_KODER = [
 
 const OVERSTYRING_AP_CODES = [aksjonspunktCodes.OVERSTYRING_AV_BEREGNINGSAKTIVITETER, aksjonspunktCodes.OVERSTYRING_AV_BEREGNINGSGRUNNLAG];
 
-const ENDEPUNKTER_INIT_DATA = [BehandlingFellesApiKeys.AKSJONSPUNKTER, BehandlingFellesApiKeys.BEREGNINGSGRUNNLAG, BehandlingFellesApiKeys.VILKAR];
-
+const ENDEPUNKTER_INIT_DATA = [BehandlingFellesApiKeys.AKSJONSPUNKTER, BehandlingFellesApiKeys.BEREGNINGSGRUNNLAG];
 type EndepunktInitData = {
   aksjonspunkter: Aksjonspunkt[];
   beregningsgrunnlag: Beregningsgrunnlag;
-  vilkar: FpVilkar[];
 }
 
 interface OwnProps {
@@ -115,17 +51,11 @@ const BeregningFaktaInitPanel: FunctionComponent<OwnProps & FaktaPanelInitProps>
     faktaPanelMenyTekst={useIntl().formatMessage({ id: 'BeregningInfoPanel.Title' })}
     skalPanelVisesIMeny={(initData) => !!initData?.beregningsgrunnlag}
     renderPanel={(data) => (
-      <DynamicLoader<React.ComponentProps<typeof ProsessFaktaBeregning>>
-        packageCompFn={() => import('@navikt/ft-fakta-beregning')}
-        federatedCompFn={ProsessFaktaBeregningMF}
-        // @ts-ignore
-        {...data}
-        vilkar={lagBGVilkar(data.vilkar, data.beregningsgrunnlag)}
-        beregningsgrunnlag={lagFormatertBG(data.beregningsgrunnlag)}
-        submitCallback={lagModifisertCallback(data.submitCallback)}
-        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+      <BeregningFaktaIndex
         erOverstyrer={rettigheter.kanOverstyreAccess.isEnabled}
-        skalKunneOverstyreAktiviteter
+        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+        // @ts-ignore Eg trur denne feilar grunna feil i typescript-pakka. Sjekk på eit seinare tidspunkt om denne er retta
+        {...data}
       />
     )}
   />
