1fc3c61e74629f775a2daf4fb84fc15790b4e6ca
tor-nav <tor.roneid@nav.no>
2022-11-21 13:17:17 +0100

    div

--- .storybook/main.js
+++ .storybook/main.js
@@ -145,17 +145,17 @@ module.exports = {
     
     config.resolve.extensions.push('.ts', '.tsx', '.less');
 
-    config.resolve.fallback = {
-      "fs": false,
-      "tls": false,
-      "net": false,
-      "path": false,
-      "zlib": false,
-      "http": false,
-      "https": false,
-      "stream": false,
-      "crypto": false,
-    }
+    const fallback = config.resolve.fallback || {}; 
+		Object.assign(fallback, { 
+    	"crypto": require.resolve("crypto-browserify"), 
+      "stream": require.resolve("stream-browserify"), 
+      "assert": require.resolve("assert"), 
+      "http": require.resolve("stream-http"), 
+      "https": require.resolve("https-browserify"), 
+      "os": require.resolve("os-browserify"), 
+      "url": require.resolve("url") 
+      }) 
+   config.resolve.fallback = fallback; 
 
     // Return the altered config
     return config;
