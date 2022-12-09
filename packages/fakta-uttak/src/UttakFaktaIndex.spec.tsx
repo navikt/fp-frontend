b306ae199085132f8a62f2c47b6aaf48927348df
tor-nav <tor.roneid@nav.no>
2022-12-09 10:57:45 +0100

    Fleire testar på nytt uttakspanel

--- packages/fakta-uttak/src/UttakFaktaIndex.spec.tsx
+++ packages/fakta-uttak/src/UttakFaktaIndex.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  fireEvent,
   render, screen, waitFor,
 } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
@@ -10,6 +11,11 @@ import * as stories from './UttakFaktaIndex.stories';
 const {
   VisUttaksperiodeUtenAksjonspunkt,
   VisUttaksperiodeUtenAksjonspunktKanOverstyre,
+  VisUttaksperiodeMedAksjonspunkt,
+  VisUtsettelseperiodeMedAksjonspunkt,
+  VisAksjonspunktDerIngenPerioderFinnes,
+  VisAksjonspunktDerArbeidsfoholdErUkjentVedGradering,
+  VisPanelDerAksjonspunktErLøstOgBehandlingAvsluttet,
 } = composeStories(stories);
 
 describe('<UttakFaktaIndex>', () => {
@@ -58,11 +64,237 @@ describe('<UttakFaktaIndex>', () => {
         periodeKilde: 'SØKNAD',
         samtidigUttaksprosent: '10',
         uttakPeriodeType: 'MØDREKVOTE',
+        aksjonspunktType: undefined,
         arbeidsforhold: undefined,
         arbeidstidsprosent: undefined,
         flerbarnsdager: false,
         morsAktivitet: undefined,
       }],
     }]);
+  });
+
+  it('skal få aksjonspunkt der en må justere fom dato til avklart startdato', async () => {
+    const lagre = jest.fn(() => Promise.resolve());
+
+    const utils = render(<VisUttaksperiodeMedAksjonspunkt submitCallback={lagre} />);
+
+    expect(await screen.findByText('Fakta om uttak')).toBeInTheDocument();
+
+    expect(screen.getByText('Første periode starter ikke på avklart startdato 31.01.2022. Legg inn periode fra startdato')).toBeInTheDocument();
+
+    await userEvent.click(screen.getAllByAltText('Åpne rad')[0]);
+
+    expect(await screen.findByText('Periode til')).toBeInTheDocument();
+
+    const periodeFra = utils.getByLabelText('Periode fra');
+    await userEvent.clear(periodeFra);
+    await userEvent.type(periodeFra, '31.01.2022');
+    fireEvent.blur(periodeFra);
+
+    await userEvent.click(screen.getByText('Oppdater'));
+
+    await userEvent.type(utils.getByLabelText('Begrunn endringene'), 'Dette er en begrunnelse');
+
+    await userEvent.click(screen.getByText('Bekreft og fortsett'));
+
+    await waitFor(() => expect(lagre).toHaveBeenCalledTimes(1));
+    expect(lagre).toHaveBeenNthCalledWith(1, [{
+      kode: '5065',
+      begrunnelse: 'Dette er en begrunnelse',
+      perioder: [{
+        fom: '2022-01-31',
+        tom: '2022-12-01',
+        originalFom: '2022-11-12',
+        periodeKilde: 'SØKNAD',
+        samtidigUttaksprosent: 80,
+        uttakPeriodeType: 'MØDREKVOTE',
+        arbeidsforhold: {
+          arbeidType: 'ORDINÆRT_ARBEID',
+          arbeidsgiverReferanse: '910909088',
+        },
+        arbeidstidsprosent: 10,
+        flerbarnsdager: true,
+        aksjonspunktType: undefined,
+        morsAktivitet: undefined,
+      }, {
+        fom: '2022-12-02',
+        tom: '2022-12-10',
+        originalFom: '2022-12-02',
+        periodeKilde: 'SØKNAD',
+        uttakPeriodeType: 'MØDREKVOTE',
+        arbeidsforhold: {
+          arbeidType: 'ORDINÆRT_ARBEID',
+          arbeidsgiverReferanse: '910909088',
+        },
+        arbeidstidsprosent: 50,
+        flerbarnsdager: false,
+      }, {
+        fom: '2022-12-11',
+        tom: '2022-12-20',
+        originalFom: '2022-12-11',
+        periodeKilde: 'SØKNAD',
+        uttakPeriodeType: 'MØDREKVOTE',
+        arbeidsforhold: {
+          arbeidType: 'ORDINÆRT_ARBEID',
+          arbeidsgiverReferanse: '910909088',
+        },
+        arbeidstidsprosent: 50,
+        flerbarnsdager: false,
+      }],
+    }]);
+  });
+
+  it('skal vise aksjonspunkt når det ikke finnes perioder og så legge til en periode', async () => {
+    const lagre = jest.fn(() => Promise.resolve());
+
+    const utils = render(<VisAksjonspunktDerIngenPerioderFinnes submitCallback={lagre} />);
+
+    expect(await screen.findByText('Fakta om uttak')).toBeInTheDocument();
+
+    expect(screen.getByText('Ingen perioder å vurdere. Vurder om behandlingen er feilopprettet og kan henlegges')).toBeInTheDocument();
+
+    expect(screen.queryByAltText('Åpne rad')).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByText('Legg til periode'));
+
+    const periodeFra = utils.getByLabelText('Periode fra');
+    await userEvent.type(periodeFra, '12.12.2022');
+    fireEvent.blur(periodeFra);
+
+    const periodeTil = utils.getByLabelText('Periode til');
+    await userEvent.type(periodeTil, '14.12.2022');
+    fireEvent.blur(periodeTil);
+
+    await userEvent.click(screen.getByText('Opphold'));
+
+    await userEvent.selectOptions(utils.getByLabelText('Årsak'), 'UTTAK_FEDREKVOTE_ANNEN_FORELDER');
+
+    await userEvent.click(screen.getByText('Oppdater'));
+
+    await userEvent.type(utils.getByLabelText('Begrunn endringene'), 'Dette er en begrunnelse');
+
+    await userEvent.click(screen.getByText('Bekreft og fortsett'));
+
+    await waitFor(() => expect(lagre).toHaveBeenCalledTimes(1));
+    expect(lagre).toHaveBeenNthCalledWith(1, [{
+      kode: '5064',
+      begrunnelse: 'Dette er en begrunnelse',
+      perioder: [{
+        fom: '2022-12-12',
+        tom: '2022-12-14',
+        periodeKilde: 'SØKNAD',
+        aksjonspunktType: undefined,
+        arbeidsforhold: undefined,
+        morsAktivitet: undefined,
+        oppholdÅrsak: 'UTTAK_FEDREKVOTE_ANNEN_FORELDER',
+      }],
+    }]);
+  });
+
+  it('skal få aksjonspunkt der arbeidsforholdet i periode er ukjent', async () => {
+    const lagre = jest.fn(() => Promise.resolve());
+
+    const utils = render(<VisAksjonspunktDerArbeidsfoholdErUkjentVedGradering submitCallback={lagre} />);
+
+    expect(await screen.findByText('Fakta om uttak')).toBeInTheDocument();
+
+    expect(screen.getByText('Gradering av ukjent arbeidsforhold. Vurder gradering')).toBeInTheDocument();
+
+    await userEvent.click(screen.getByAltText('Åpne rad'));
+
+    expect(await screen.findByText('Arbeidsgiver oppgitt for perioden er ukjent. Referanse: 91090823')).toBeInTheDocument();
+
+    await userEvent.selectOptions(utils.getByLabelText('Arbeidsgiver'), '910909088-ORDINÆRT_ARBEID');
+
+    await userEvent.click(screen.getByText('Oppdater'));
+
+    await userEvent.type(utils.getByLabelText('Begrunn endringene'), 'Dette er en begrunnelse');
+
+    await userEvent.click(screen.getByText('Bekreft og fortsett'));
+
+    await waitFor(() => expect(lagre).toHaveBeenCalledTimes(1));
+    expect(lagre).toHaveBeenNthCalledWith(1, [{
+      kode: '5063',
+      begrunnelse: 'Dette er en begrunnelse',
+      perioder: [{
+        fom: '2022-11-12',
+        tom: '2022-12-01',
+        originalFom: '2022-11-12',
+        periodeKilde: 'SØKNAD',
+        samtidigUttaksprosent: undefined,
+        uttakPeriodeType: 'MØDREKVOTE',
+        arbeidsforhold: {
+          arbeidType: 'ORDINÆRT_ARBEID',
+          arbeidsgiverReferanse: '910909088',
+        },
+        arbeidstidsprosent: 50,
+        flerbarnsdager: false,
+        aksjonspunktType: undefined,
+        morsAktivitet: undefined,
+      }],
+    }]);
+  });
+
+  it('skal vise ulike felter for ulike periodetyper', async () => {
+    const utils = render(<VisUtsettelseperiodeMedAksjonspunkt />);
+
+    expect(await screen.findByText('Fakta om uttak')).toBeInTheDocument();
+
+    await userEvent.click(screen.getByAltText('Åpne rad'));
+
+    expect(await screen.findByText('Periodetype')).toBeInTheDocument();
+
+    // For periodetype = Utsettelse
+    expect(utils.getByLabelText('Årsak')).toBeInTheDocument();
+    expect(utils.getByLabelText('Mors aktivitet')).toBeInTheDocument();
+    expect(utils.queryByLabelText('Stønadskonto')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Gradering %')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Arbeidsgiver')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Samtidig uttaksprosent')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Flerbarnsdager')).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByText('Overføring'));
+
+    expect(await utils.findByLabelText('Stønadskonto')).toBeInTheDocument();
+    expect(utils.getByLabelText('Årsak')).toBeInTheDocument();
+    expect(utils.getByLabelText('Mors aktivitet')).toBeInTheDocument();
+    expect(utils.queryByLabelText('Gradering %')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Arbeidsgiver')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Samtidig uttaksprosent')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Flerbarnsdager')).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByText('Uttak'));
+
+    expect(await utils.findByLabelText('Arbeidsgiver')).toBeInTheDocument();
+    expect(utils.getByLabelText('Stønadskonto')).toBeInTheDocument();
+    expect(utils.getByLabelText('Gradering %')).toBeInTheDocument();
+    expect(utils.getByLabelText('Samtidig uttaksprosent')).toBeInTheDocument();
+    expect(utils.getByLabelText('Mors aktivitet')).toBeInTheDocument();
+    expect(utils.getByLabelText('Flerbarnsdager')).toBeInTheDocument();
+    expect(utils.queryByLabelText('Årsak')).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByText('Opphold'));
+
+    expect(await utils.getByLabelText('Årsak')).toBeInTheDocument();
+    expect(utils.queryByLabelText('Mors aktivitet')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Stønadskonto')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Gradering %')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Arbeidsgiver')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Samtidig uttaksprosent')).not.toBeInTheDocument();
+    expect(utils.queryByLabelText('Flerbarnsdager')).not.toBeInTheDocument();
+  });
+
+  it('skal vise periode der aksjonspunkt er løst og behandlingen er avsluttet', async () => {
+    render(<VisPanelDerAksjonspunktErLøstOgBehandlingAvsluttet />);
+
+    expect(await screen.findByText('Fakta om uttak')).toBeInTheDocument();
+
+    expect(screen.queryByText('Bekreft og fortsett')).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByAltText('Åpne rad'));
+
+    expect(await screen.findByText('Periodetype')).toBeInTheDocument();
+    expect(screen.queryByRole('input')).not.toBeInTheDocument();
+    expect(screen.queryByText('Oppdater')).not.toBeInTheDocument();
   });
 });
