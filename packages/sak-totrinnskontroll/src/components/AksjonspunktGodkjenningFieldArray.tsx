9b19d5aa177f70472f6d25fbd540d94111c52023
tor-nav <tor.roneid@nav.no>
2022-12-12 13:05:20 +0100

    Fix diverse sonar warnings

--- packages/sak-totrinnskontroll/src/components/AksjonspunktGodkjenningFieldArray.tsx
+++ packages/sak-totrinnskontroll/src/components/AksjonspunktGodkjenningFieldArray.tsx
@@ -1,35 +1,18 @@
 import React, { FunctionComponent } from 'react';
-import { FormattedMessage } from 'react-intl';
 import { NavLink } from 'react-router-dom';
-import { UseFormGetValues } from 'react-hook-form';
-import { ErrorMessage, BodyShort, Label } from '@navikt/ds-react';
 import { Location } from 'history';
-import {
-  ArrowBox, FlexColumn, FlexRow, VerticalSpacer,
-} from '@navikt/ft-ui-komponenter';
-import { BehandlingType } from '@navikt/ft-kodeverk';
-import {
-  CheckboxField, TextAreaField, RadioGroupPanel, formHooks, useCustomValidation,
-} from '@navikt/ft-form-hooks';
-import {
-  hasValidText, maxLength, minLength, required, isRequiredMessage,
-} from '@navikt/ft-form-validators';
+import { BodyShort } from '@navikt/ds-react';
+import { VerticalSpacer } from '@navikt/ft-ui-komponenter';
+import { formHooks } from '@navikt/ft-form-hooks';
 
-import {
-  KodeverkMedNavn, TotrinnskontrollSkjermlenkeContext, BehandlingAppKontekst,
-} from '@fpsak-frontend/types';
+import { KodeverkMedNavn, TotrinnskontrollSkjermlenkeContext, BehandlingAppKontekst } from '@fpsak-frontend/types';
 
 import getAksjonspunkttekst from './aksjonspunktTekster/aksjonspunktTekstUtleder';
+import GodkjenningPanel from './GodkjenningPanel';
 
 import styles from './aksjonspunktGodkjenningFieldArray.less';
 
-const minLength3 = minLength(3);
-const maxLength2000 = maxLength(2000);
-
 const FIELD_ARRAY_NAME = 'aksjonspunktGodkjenning';
-
-const harIkkeValgtMinstEnFakta = (getValues: UseFormGetValues<any>, fieldIndex: string) => !getValues(`${fieldIndex}.feilFakta`)
-  && !getValues(`${fieldIndex}.feilLov`) && !getValues(`${fieldIndex}.feilRegel`) && !getValues(`${fieldIndex}.annet`);
 
 export type AksjonspunktGodkjenningData = {
   aksjonspunktKode: string;
@@ -67,7 +50,7 @@ const AksjonspunktGodkjenningFieldArray: FunctionComponent<OwnProps> = ({
   lagLenke,
 }) => {
   const {
-    control, watch, getValues,
+    control, watch,
   } = formHooks.useFormContext();
   const { fields } = formHooks.useFieldArray({
     control,
@@ -79,7 +62,7 @@ const AksjonspunktGodkjenningFieldArray: FunctionComponent<OwnProps> = ({
   return (
     <>
       {fields.map((field, index) => {
-        const { aksjonspunktKode, totrinnskontrollGodkjent } = aksjonspunktGodkjenning[index];
+        const { aksjonspunktKode } = aksjonspunktGodkjenning[index];
         const context = totrinnskontrollSkjermlenkeContext.find((c) => c.totrinnskontrollAksjonspunkter
           .some((ta) => ta.aksjonspunktKode === aksjonspunktKode));
         const totrinnskontrollAksjonspunkt = context?.totrinnskontrollAksjonspunkter
@@ -88,10 +71,6 @@ const AksjonspunktGodkjenningFieldArray: FunctionComponent<OwnProps> = ({
         if (!context || !totrinnskontrollAksjonspunkt) {
           return null;
         }
-
-        const erKlage = behandling.type === BehandlingType.KLAGE && totrinnskontrollGodkjent;
-        const visKunBegrunnelse = erKlage ? totrinnskontrollGodkjent : showBegrunnelse;
-        const visArsaker = erKlage || totrinnskontrollGodkjent === false;
 
         const aksjonspunktText = getAksjonspunkttekst(
           erForeldrepengerFagsak,
@@ -107,11 +86,6 @@ const AksjonspunktGodkjenningFieldArray: FunctionComponent<OwnProps> = ({
 
         const lenke = lagLenke(context.skjermlenkeType);
 
-        const fieldIndex = `${FIELD_ARRAY_NAME}.${index}`;
-
-        const feilmelding = !totrinnskontrollGodkjent && harIkkeValgtMinstEnFakta(getValues, fieldIndex) ? isRequiredMessage() : undefined;
-        const errorMessage = useCustomValidation(`${fieldIndex}.faktagruppe`, feilmelding);
-
         return (
           <div key={field.id}>
             {lenke && skjermlenkeTypeKodeverk && (
@@ -119,6 +93,13 @@ const AksjonspunktGodkjenningFieldArray: FunctionComponent<OwnProps> = ({
                 {skjermlenkeTypeKodeverk.navn}
               </NavLink>
             )}
+            <GodkjenningPanel
+              index={index}
+              behandling={behandling}
+              totrinnskontrollSkjermlenkeContext={totrinnskontrollSkjermlenkeContext}
+              readOnly={readOnly}
+              showBegrunnelse={showBegrunnelse}
+            />
             <div className={styles.approvalItemContainer}>
               {aksjonspunktText.map((formattedMessage, i) => (
                 <div key={aksjonspunktKode.concat('_'.concat(i.toString()))} className={styles.aksjonspunktTextContainer}>
@@ -127,67 +108,6 @@ const AksjonspunktGodkjenningFieldArray: FunctionComponent<OwnProps> = ({
                   </BodyShort>
                 </div>
               ))}
-              <RadioGroupPanel
-                name={`${fieldIndex}.totrinnskontrollGodkjent`}
-                isReadOnly={readOnly}
-                isHorizontal
-                isTrueOrFalseSelection
-                hideLegend
-                radios={[{
-                  value: 'true',
-                  label: <FormattedMessage id="ApprovalField.Godkjent" />,
-                }, {
-                  value: 'false',
-                  label: <FormattedMessage id="ApprovalField.Vurder" />,
-                },
-                ]}
-              />
-              {visArsaker && (
-                <>
-                  <VerticalSpacer sixteenPx />
-                  <ArrowBox alignOffset={totrinnskontrollGodkjent ? 1 : 110}>
-                    {!visKunBegrunnelse && (
-                      <>
-                        <Label size="small"><FormattedMessage id="AksjonspunktGodkjenningArsakPanel.Arsak" /></Label>
-                        <FlexRow>
-                          <FlexColumn className={styles.halfColumn}>
-                            <CheckboxField
-                              name={`${fieldIndex}.feilFakta`}
-                              label={<FormattedMessage id="AksjonspunktGodkjenningArsakPanel.FeilFakta" />}
-                              readOnly={readOnly}
-                            />
-                            <CheckboxField
-                              name={`${fieldIndex}.feilRegel`}
-                              label={<FormattedMessage id="AksjonspunktGodkjenningArsakPanel.FeilRegelForstaelse" />}
-                              readOnly={readOnly}
-                            />
-                          </FlexColumn>
-                          <FlexColumn className={styles.halfColumn}>
-                            <CheckboxField
-                              name={`${fieldIndex}.feilLov`}
-                              label={<FormattedMessage id="AksjonspunktGodkjenningArsakPanel.FeilLovanvendelse" />}
-                              readOnly={readOnly}
-                            />
-                            <CheckboxField
-                              name={`${fieldIndex}.annet`}
-                              label={<FormattedMessage id="AksjonspunktGodkjenningArsakPanel.Annet" />}
-                              readOnly={readOnly}
-                            />
-                          </FlexColumn>
-                        </FlexRow>
-                        {errorMessage && <ErrorMessage size="small">{errorMessage}</ErrorMessage>}
-                      </>
-                    )}
-                    <VerticalSpacer sixteenPx />
-                    <TextAreaField
-                      name={`${fieldIndex}.besluttersBegrunnelse`}
-                      label={<FormattedMessage id="AksjonspunktGodkjenningArsakPanel.Begrunnelse" />}
-                      validate={[required, minLength3, maxLength2000, hasValidText]}
-                      readOnly={readOnly}
-                    />
-                  </ArrowBox>
-                </>
-              )}
               <VerticalSpacer sixteenPx />
             </div>
           </div>
