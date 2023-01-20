a687abfbc9414c7229678fc6a80a2ab8b66b58e8
tor-nav <tor.roneid@nav.no>
2023-01-20 12:45:54 +0100

    FIX export av type
    Former-commit-id: 4c29f8c9a15f16a9c17d55293affa579bba53b7f

--- packages/fakta-felles/index.ts
+++ packages/fakta-felles/index.ts
@@ -1,6 +1,7 @@
 export { default as MerkePanel } from './src/components/Merkepanel';
-export { default as FaktaBegrunnelseTextFieldNew, FormValues as FaktaBegrunnelseFormValues } from './src/components/FaktaBegrunnelseTextFieldNew';
+export { default as FaktaBegrunnelseTextFieldNew } from './src/components/FaktaBegrunnelseTextFieldNew';
 export { default as FaktaSubmitButtonNew } from './src/components/FaktaSubmitButtonNew';
 
 export { default as isFieldEdited } from './src/utils/isFieldEdited';
 export type { FieldEditedInfo } from './src/utils/isFieldEdited';
+export type { FormValues as FaktaBegrunnelseFormValues } from './src/components/FaktaBegrunnelseTextFieldNew';
