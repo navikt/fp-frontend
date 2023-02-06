63a1bd902e5d18b13318df18be0b9120f00899d8
Petter Ekern <petter.ekern@nav.no>
2023-02-06 16:05:35 +0100

    Lint

--- packages/journalforing/src/JournalforingIndex.stories.tsx
+++ packages/journalforing/src/JournalforingIndex.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Story } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
-import { action } from '@storybook/addon-actions';
 
 import RestApiMock from '@navikt/fp-utils-test/src/rest/RestApiMock';
 import { withRouter } from '@navikt/fp-storybook-utils';
