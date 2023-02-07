3d5e896972d3181b6bad7bef005c711521005e4c
Petter Ekern <petter.ekern@nav.no>
2023-02-07 14:33:58 +0100

    Gjør restkall kun når vi skal vise siden

--- packages/journalforing/src/JournalforingIndex.tsx
+++ packages/journalforing/src/JournalforingIndex.tsx
@@ -34,7 +34,7 @@ interface OwnProps {
 const JournalforingIndex: FunctionComponent<OwnProps> = ({
   navAnsatt,
 }) => {
-  const alleJournalføringsoppgaverKall = restApiHooks.useRestApi(RestApiPathsKeys.ALLE_JOURNAL_OPPGAVER, { avdelingEnhet: undefined });
+  const alleJournalføringsoppgaverKall = restApiHooks.useRestApi(RestApiPathsKeys.ALLE_JOURNAL_OPPGAVER, { avdelingEnhet: undefined }, { suspendRequest: !harTilgangTilÅBrukeJournalføring(navAnsatt) });
   const { addErrorMessage } = useRestApiErrorDispatcher();
   requestApi.setAddErrorMessageHandler(addErrorMessage);
   if (alleJournalføringsoppgaverKall.state === RestApiState.NOT_STARTED || alleJournalføringsoppgaverKall.state === RestApiState.LOADING) {
