407bea8d5fd6fbe8db9937a81dc7a64992d1a669
Ekern <petter.ekern@nav.no>
2023-02-09 11:27:44 +0100

    Verified 2?

--- packages/journalforing/src/JournalforingIndex.stories.tsx
+++ packages/journalforing/src/JournalforingIndex.stories.tsx
@@ -50,7 +50,7 @@ const detaljertJournalpostMal = {
   ],
   fagsaker: [
     {
-      saksnummer: '2981467164',
+      saksnummer: '1249124123',
       ytelseType: fagsakYtelseType.FORELDREPENGER,
       datoOpprettet: '2022-01-02',
       sistEndret: '2022-06-03',
