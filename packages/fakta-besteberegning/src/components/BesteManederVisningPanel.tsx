c3b2cf402f34e5a63d0331903ca033dfd7cda227
Petter Ekern <petter.ekern@nav.no>
2023-02-21 09:47:35 +0100

    Eslint

--- packages/fakta-besteberegning/src/components/BesteManederVisningPanel.tsx
+++ packages/fakta-besteberegning/src/components/BesteManederVisningPanel.tsx
@@ -70,6 +70,7 @@ const lagVisningsNavn = (inntekt: BesteberegningInntekt, arbeidsgiverOpplysninge
 const lagInntektRader = (inntekter: BesteberegningInntekt[], arbeidsgiverOpplysninger: ArbeidsgiverOpplysningerPerId,
   getKodeverkNavn: (kodeverk: string, kodeverkType: KodeverkType) => string): ReactElement[] => (
   inntekter.map((inntekt: BesteberegningInntekt, index) => (
+    // eslint-disable-next-line react/no-array-index-key
     <TableRow key={index} className={styles.månedRad}>
       <TableColumn className={styles.månedAktivitet}>
         <BodyShort size="small">
