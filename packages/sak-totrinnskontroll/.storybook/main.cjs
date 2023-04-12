e10bfa81c9f8c1f09ac9bad9334013c78cc26b89
tor-nav <tor.roneid@nav.no>
2023-04-12 10:29:31 +0200

    Storybook 7

--- packages/sak-totrinnskontroll/.storybook/main.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    stories: ['../src/**/*.stories.@(ts|tsx)'],
-    core: {
-      builder: "@storybook/builder-vite",
-    },
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-};
