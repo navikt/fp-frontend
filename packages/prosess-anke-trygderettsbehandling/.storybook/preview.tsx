c941c8cb6109145a6db40c5d43a4e4e97543a642
Tor Røneid <tor.roneid@nav.no>
2023-04-26 09:42:02 +0200

    Rydd i storybook-config + Ikkje køyr dts-plugin ved bygging av storybook (#4618)

--- packages/prosess-anke-trygderettsbehandling/.storybook/preview.tsx
+++ packages/prosess-anke-trygderettsbehandling/.storybook/preview.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
 import '@navikt/ds-css';
 import '@navikt/ft-ui-komponenter/dist/style.css';
-
-import '../../utils-test/src/globalStylesTest.module.css';
-
-export const decorators = [(Story) => <div style={{ margin: '40px'}}><Story/></div>];
+export { decorators } from '../../../.storybook/preview-storybook';
--- packages/prosess-vilkar-fodsel/.storybook/preview.tsx
+++ packages/prosess-vilkar-fodsel/.storybook/preview.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
 import '@navikt/ds-css';
 import '@navikt/ft-ui-komponenter/dist/style.css';
 import '@navikt/ft-form-hooks/dist/style.css';
-
-import '../../utils-test/src/globalStylesTest.module.css';
-
-export const decorators = [(Story) => <div style={{ margin: '40px'}}><Story/></div>];
+export { decorators } from '../../../.storybook/preview-storybook';
