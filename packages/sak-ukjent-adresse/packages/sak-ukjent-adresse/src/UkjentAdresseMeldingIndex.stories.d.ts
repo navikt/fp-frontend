02ee826a95a1e08f8f21ade19113679e5aad78fe
tor-nav <tor.roneid@nav.no>
2023-06-07 14:41:49 +0200

    div

--- packages/sak-ukjent-adresse/packages/sak-ukjent-adresse/src/UkjentAdresseMeldingIndex.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-declare const _default: {
-    title: string;
-    component: React.FunctionComponent<{}>;
-    decorators: ((story: any) => JSX.Element)[];
-};
-export default _default;
-export declare const IngenAdresse: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, import("@storybook/types").Args>;
