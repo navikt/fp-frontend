ea4203638a3a237c6ffc3988a6c0a44ed6478771
tor-nav <tor.roneid@nav.no>
2023-06-08 11:16:34 +0200

    Førsøk nr 2 på å fiksa opplasting av source maps
    Former-commit-id: 6f5d7851a56c4150795bdcb0fda67cdfe3f49b95

--- scripts/sentry-release.js
+++ scripts/sentry-release.js
@@ -22,7 +22,7 @@ async function opprettReleaseTilSentry() {
     console.log('Laster opp source maps');
     await cli.releases.uploadSourceMaps(release, {
       include: ['dist/assets'],
-      urlPrefix: '~/public/',
+      urlPrefix: '~/assets/',
       rewrite: false,
     });
 
