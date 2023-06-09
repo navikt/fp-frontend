87ab82861c71c46be55f7348c27c91ecb3ea8d0c
tor-nav <tor.roneid@nav.no>
2023-06-09 11:11:04 +0200

    Endring av app-storybook

--- packages/storybook-utils/index.ts
+++ packages/storybook-utils/index.ts
@@ -3,3 +3,4 @@ export { default as WithReactHookForm } from './decorators/withReactHookForm';
 export { default as getIntlDecorator } from './decorators/withIntl';
 export { default as alleKodeverk } from './mocks/alleKodeverk.json';
 export { default as alleKodeverkLos } from './mocks/alleKodeverkLos.json';
+export { default as alleKodeverkTilbakekreving } from './mocks/alleKodeverkTilbakekreving.json';
