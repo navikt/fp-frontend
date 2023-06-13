8fd54ad2aed159c0b9dff67daca44126f0aab79f
tor-nav <tor.roneid@nav.no>
2023-06-13 14:36:28 +0200

    FIX Legg til opptjening-inngangsvilkår for SVP
    Former-commit-id: f5f5ac685c85f2c2bfc59309ee74cff462c2ce9b

--- apps/fp-frontend-app/src/behandling/svangerskapspenger/prosessPaneler/inngangsvilkarPaneler/OpptjeningInngangsvilkarInitPanel.tsx
+++ apps/fp-frontend-app/src/behandling/svangerskapspenger/prosessPaneler/inngangsvilkarPaneler/OpptjeningInngangsvilkarInitPanel.tsx
@@ -1,16 +1,24 @@
 import React, { FunctionComponent } from 'react';
 
 import { VilkarType, AksjonspunktCode } from '@navikt/fp-kodeverk';
-import { AksessRettigheter } from '@navikt/fp-types';
+import { AksessRettigheter, Opptjening } from '@navikt/fp-types';
 import { VerticalSpacer } from '@navikt/ft-ui-komponenter';
+import { OpptjeningVilkarProsessIndex } from '@navikt/fp-prosess-vilkar-opptjening';
 
+import { useIntl } from 'react-intl';
 import InngangsvilkarPanelInitProps from '../../../felles/typer/inngangsvilkarPanelInitProps';
 import InngangsvilkarDefaultInitPanel from '../../../felles/prosess/InngangsvilkarDefaultInitPanel';
 import OverstyringPanelDef from '../../../felles/prosess/OverstyringPanelDef';
+import { BehandlingApiKeys } from '../../../../data/behandlingContextApi';
 
-const AKSJONSPUNKT_KODER = [AksjonspunktCode.OVERSTYRING_AV_OPPTJENINGSVILKARET];
+const AKSJONSPUNKT_KODER = [AksjonspunktCode.VURDER_OPPTJENINGSVILKARET];
 
 const VILKAR_KODER = [VilkarType.OPPTJENINGSPERIODE, VilkarType.OPPTJENINGSVILKARET];
+
+const ENDEPUNKTER_PANEL_DATA = [BehandlingApiKeys.OPPTJENING];
+type EndepunktPanelData = {
+  opptjening: Opptjening;
+};
 
 interface OwnProps {
   behandlingVersjon: number;
@@ -21,34 +29,53 @@ const OpptjeningInngangsvilkarInitPanel: FunctionComponent<OwnProps & Inngangsvi
   behandlingVersjon,
   rettigheter,
   ...props
-}) => (
-  <InngangsvilkarDefaultInitPanel
-    {...props}
-    behandlingVersjon={behandlingVersjon}
-    aksjonspunktKoder={AKSJONSPUNKT_KODER}
-    vilkarKoder={VILKAR_KODER}
-    inngangsvilkarPanelKode="OPPTJENINGSVILKARET"
-    hentInngangsvilkarPanelTekst={() => ''}
-    renderPanel={(data, erOverstyrt, toggleOverstyring) => (
-      <>
-        <OverstyringPanelDef
-          aksjonspunkter={data.aksjonspunkter}
-          aksjonspunktKode={AksjonspunktCode.OVERSTYRING_AV_OPPTJENINGSVILKARET}
-          vilkar={data.vilkar}
-          vilkarKoder={VILKAR_KODER}
-          panelTekstKode="Inngangsvilkar.Opptjeningsvilkaret"
-          erMedlemskapsPanel={false}
-          toggleOverstyring={toggleOverstyring}
-          erOverstyrt={erOverstyrt}
-          overrideReadOnly={
-            data.isReadOnly || (props.harInngangsvilkarApentAksjonspunkt && !(data.isAksjonspunktOpen || erOverstyrt))
-          }
-          kanOverstyreAccess={rettigheter.kanOverstyreAccess}
-        />
-        <VerticalSpacer thirtyTwoPx />
-      </>
-    )}
-  />
-);
+}) => {
+  const intl = useIntl();
+  return (
+    <InngangsvilkarDefaultInitPanel<EndepunktPanelData>
+      {...props}
+      behandlingVersjon={behandlingVersjon}
+      panelEndepunkter={ENDEPUNKTER_PANEL_DATA}
+      aksjonspunktKoder={AKSJONSPUNKT_KODER}
+      vilkarKoder={VILKAR_KODER}
+      inngangsvilkarPanelKode="OPPTJENINGSVILKARET"
+      hentInngangsvilkarPanelTekst={() => intl.formatMessage({ id: 'OpptjeningVilkarView.VurderOmSøkerHarRett' })}
+      renderPanel={(data, erOverstyrt, toggleOverstyring) => (
+        <>
+          {data.aksjonspunkter.length === 0 && (
+            <>
+              <OverstyringPanelDef
+                aksjonspunkter={data.aksjonspunkter}
+                aksjonspunktKode={AksjonspunktCode.OVERSTYRING_AV_OPPTJENINGSVILKARET}
+                vilkar={data.vilkar}
+                vilkarKoder={VILKAR_KODER}
+                panelTekstKode="Inngangsvilkar.Opptjeningsvilkaret"
+                erMedlemskapsPanel={false}
+                toggleOverstyring={toggleOverstyring}
+                erOverstyrt={erOverstyrt}
+                overrideReadOnly={
+                  data.isReadOnly ||
+                  (props.harInngangsvilkarApentAksjonspunkt && !(data.isAksjonspunktOpen || erOverstyrt))
+                }
+                kanOverstyreAccess={rettigheter.kanOverstyreAccess}
+              />
+              <VerticalSpacer thirtyTwoPx />
+            </>
+          )}
+          {data.aksjonspunkter.length > 0 && (
+            <>
+              <OpptjeningVilkarProsessIndex
+                lovReferanse={data.vilkar[0].lovReferanse}
+                // @ts-ignore fiks
+                {...data}
+              />
+              <VerticalSpacer thirtyTwoPx />
+            </>
+          )}
+        </>
+      )}
+    />
+  );
+};
 
 export default OpptjeningInngangsvilkarInitPanel;
