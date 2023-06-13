d71d4056ebd91e23570668eefdea9e224d052408
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-06-13 14:13:40 +0200

    Åpner opp journalføring (uten lenke foreløpig) (#4809)

--- packages/journalforing/src/OppgaveJournalføringIndex.tsx
+++ packages/journalforing/src/OppgaveJournalføringIndex.tsx
@@ -1,15 +1,15 @@
-import React, { FunctionComponent, useEffect, useState, useCallback } from 'react';
-import { FormattedMessage, RawIntlProvider } from 'react-intl';
-import { createIntl } from '@navikt/ft-utils';
-import { Heading, Link } from '@navikt/ds-react';
-import { ChevronLeftIcon } from '@navikt/aksel-icons';
-import { NavAnsatt } from '@navikt/fp-types';
-import { FlexColumn, FlexContainer, FlexRow, LoadingPanel, VerticalSpacer } from '@navikt/ft-ui-komponenter';
-import { RestApiState, useRestApiErrorDispatcher } from '@navikt/fp-rest-api-hooks';
+import React, {FunctionComponent, useCallback, useEffect, useState} from 'react';
+import {FormattedMessage, RawIntlProvider} from 'react-intl';
+import {createIntl} from '@navikt/ft-utils';
+import {Heading, Link} from '@navikt/ds-react';
+import {ChevronLeftIcon} from '@navikt/aksel-icons';
+import {NavAnsatt} from '@navikt/fp-types';
+import {FlexColumn, FlexContainer, FlexRow, LoadingPanel, VerticalSpacer} from '@navikt/ft-ui-komponenter';
+import {RestApiState, useRestApiErrorDispatcher} from '@navikt/fp-rest-api-hooks';
 import messages from '../i18n/nb_NO.json';
 import JournalforingPanel from './components/JournalforingPanel';
 import JournalføringIndex from './components/JournalføringIndex';
-import { RestApiPathsKeys, restApiHooks, requestApi } from './data/fpfordelRestApi';
+import {requestApi, restApiHooks, RestApiPathsKeys} from './data/fpfordelRestApi';
 import OppgaveOversikt from './typer/oppgaveOversiktTsType';
 import styles from './oppgaveJournalføringIndex.module.css';
 import JournalførSubmitValue from './typer/ferdigstillJournalføringSubmit';
@@ -17,18 +17,6 @@ import JournalførtSubmitModal from './components/journalpost/modal/Journalført
 
 const intl = createIntl(messages);
 const TOM_ARRAY: OppgaveOversikt[] = [];
-
-const tilatteBrukere = ['H137440', 'A138225', 'J116396', 'W119202', 'S148979', 'R107802', 'V149165', 'D151669'];
-
-// Mildertidig intill tilganger er avklart og testing er ferdig
-const harTilgangTilÅBrukeJournalføring = (navAnsatt?: NavAnsatt): boolean => {
-  const url = window.location.href;
-  const erProd = url.includes('fpsak.intern.nav.no');
-  if (!erProd) {
-    return true;
-  }
-  return navAnsatt ? tilatteBrukere.includes(navAnsatt.brukernavn) : false;
-};
 
 interface OwnProps {
   navAnsatt?: NavAnsatt;
@@ -79,15 +67,15 @@ const JournalforingIndex: FunctionComponent<OwnProps> = ({ navAnsatt }) => {
   );
 
   useEffect(() => {
-    if (navAnsatt && harTilgangTilÅBrukeJournalføring(navAnsatt)) {
+    if (navAnsatt) {
       innhentAlleOppgaver({ ident: navAnsatt.brukernavn });
     }
   }, [navAnsatt]);
 
-  if (status === RestApiState.NOT_STARTED) {
+  if (status === RestApiState.NOT_STARTED || status === RestApiState.LOADING) {
     return <LoadingPanel />;
   }
-  if (!navAnsatt || !harTilgangTilÅBrukeJournalføring(navAnsatt)) {
+  if (!navAnsatt) {
     return (
       <Heading size="medium">
         <FormattedMessage id="Journalforing.Tilgang" />
