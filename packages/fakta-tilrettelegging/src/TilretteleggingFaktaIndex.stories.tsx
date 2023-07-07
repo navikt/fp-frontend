51e753f5be0afe841c1e1df3ac6f19d65699d37c
Tor Røneid <tor.roneid@nav.no>
2023-07-07 11:40:50 +0200

    Feature/fodseltilrettelegging (#4839)
    * Redisign av fakta-fodsel-og-tilrettelegging
    
    Former-commit-id: 077dc1d3be558ae8f449c24fe65c70836c60f711

---
+++ packages/fakta-tilrettelegging/src/TilretteleggingFaktaIndex.stories.tsx
@@ -0,0 +1,309 @@
+import React from 'react';
+import { StoryFn } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { action } from '@storybook/addon-actions';
+import { Aksjonspunkt } from '@navikt/ft-types';
+
+import { aksjonspunktStatus, AksjonspunktCode, tilretteleggingType } from '@navikt/fp-kodeverk';
+import {
+  ArbeidsforholdFodselOgTilrettelegging,
+  Behandling,
+  FodselOgTilrettelegging,
+  ArbeidOgInntektsmelding,
+} from '@navikt/fp-types';
+import { alleKodeverk } from '@navikt/fp-storybook-utils';
+import { FaktaAksjonspunkt } from '@navikt/fp-types-avklar-aksjonspunkter';
+
+import TilretteleggingFaktaIndex from './TilretteleggingFaktaIndex';
+
+import '@navikt/ds-css';
+import '@navikt/ft-ui-komponenter/dist/style.css';
+import '@navikt/ft-form-hooks/dist/style.css';
+
+const behandling = {
+  uuid: '1',
+  versjon: 1,
+} as Behandling;
+
+const tilretteleggingPermisjon = {
+  termindato: '2020-11-06',
+  arbeidsforholdListe: [
+    {
+      tilretteleggingId: 1116961,
+      tilretteleggingBehovFom: '2020-03-17',
+      tilretteleggingDatoer: [
+        {
+          fom: '2020-03-17',
+          type: 'INGEN_TILRETTELEGGING',
+        },
+        {
+          fom: '2020-10-15',
+          type: 'HEL_TILRETTELEGGING',
+        },
+      ],
+      arbeidsgiverReferanse: '999999999',
+      uttakArbeidType: 'FRILANS',
+      kopiertFraTidligereBehandling: true,
+      mottattTidspunkt: '2020-03-11T16:21:48.532298',
+      skalBrukes: true,
+      kanTilrettelegges: true,
+      velferdspermisjoner: [
+        {
+          permisjonFom: '2020-02-17',
+          permisjonTom: '2020-07-12',
+          permisjonsprosent: 50.0,
+          type: 'VELFERDSPERMISJON',
+          erGyldig: true,
+        },
+        {
+          permisjonFom: '2019-08-06',
+          permisjonTom: '2019-08-06',
+          permisjonsprosent: 50.0,
+          type: 'VELFERDSPERMISJON',
+          erGyldig: true,
+        },
+        {
+          permisjonFom: '2019-10-03',
+          permisjonTom: '2019-10-03',
+          permisjonsprosent: 50.0,
+          type: 'VELFERDSPERMISJON',
+          erGyldig: true,
+        },
+      ],
+      avklarteOppholdPerioder: [],
+    },
+  ],
+  saksbehandlet: true,
+};
+
+const svangerskapspengerTilretteleggingForFrilanser = {
+  termindato: '2020-02-27',
+  saksbehandlet: false,
+  // @ts-ignore fixme
+  arbeidsforholdListe: [
+    {
+      tilretteleggingId: 1008653,
+      tilretteleggingBehovFom: '2019-10-01',
+      tilretteleggingDatoer: [
+        {
+          fom: '2019-10-01',
+          type: tilretteleggingType.INGEN_TILRETTELEGGING,
+        },
+      ],
+      arbeidsgiverReferanse: '1',
+      skalBrukes: true,
+      velferdspermisjoner: [],
+      avklarteOppholdPerioder: [],
+    },
+    {
+      tilretteleggingId: 1008654,
+      tilretteleggingBehovFom: '2019-10-01',
+      tilretteleggingDatoer: [
+        {
+          fom: '2019-10-01',
+          type: tilretteleggingType.INGEN_TILRETTELEGGING,
+        },
+      ],
+      arbeidsgiverReferanse: '2',
+      uttakArbeidType: 'ORDINÆRT_ARBEID',
+      skalBrukes: true,
+      velferdspermisjoner: [],
+      avklarteOppholdPerioder: [],
+    },
+  ] as ArbeidsforholdFodselOgTilrettelegging[],
+};
+
+const defaultArbeidOgInntekt = {
+  arbeidsforhold: [
+    {
+      arbeidsgiverIdent: '555864629',
+      internArbeidsforholdId: '555864629-null',
+    },
+  ],
+} as ArbeidOgInntektsmelding;
+
+const spesiellArbeidOgInntekt = {
+  inntektsmeldinger: [
+    {
+      arbeidsgiverIdent: '999999999',
+      motattDato: '2020-03-20',
+      refusjonPrMnd: 5170.0,
+    },
+  ],
+  arbeidsforhold: [
+    {
+      internArbeidsforholdId: '999999999-null',
+      arbeidsgiverIdent: '999999999',
+      fom: '2019-04-01',
+      stillingsprosent: 100.0,
+    },
+  ],
+} as ArbeidOgInntektsmelding;
+
+const arbeidsgiverOpplysningerPerId = {
+  1: {
+    erPrivatPerson: false,
+    identifikator: '973861778',
+    navn: 'Frilanser, samlet aktivitet',
+  },
+  2: {
+    erPrivatPerson: true,
+    identifikator: '973861779',
+    navn: 'Selvstendig næringsdrivende',
+  },
+  3: {
+    erPrivatPerson: false,
+    identifikator: '973861779',
+    navn: 'Test',
+  },
+  555864629: {
+    erPrivatPerson: false,
+    identifikator: '555864629',
+    navn: 'WWW.EIENDOMSDRIFT.CC SA',
+  },
+  999999999: {
+    erPrivatPerson: false,
+    identifikator: '999999999',
+    navn: 'Arbeidsgiveren AS',
+  },
+  1111111: {
+    erPrivatPerson: false,
+    identifikator: '1111111',
+    navn: 'STATOIL',
+  },
+  342352362: {
+    referanse: '342352362',
+    identifikator: '342352362',
+    navn: 'Lagt til av saksbehandler',
+    fødselsdato: null,
+    erPrivatPerson: false,
+  },
+};
+
+export default {
+  title: 'fakta/fakta-tilrettelegging',
+  component: TilretteleggingFaktaIndex,
+};
+
+const Template: StoryFn<{
+  aksjonspunkter: Aksjonspunkt[];
+  submitCallback: (aksjonspunktData: FaktaAksjonspunkt | FaktaAksjonspunkt[]) => Promise<void>;
+  svangerskapspengerTilrettelegging: FodselOgTilrettelegging;
+  arbeidOgInntekt: ArbeidOgInntektsmelding;
+  erOverstyrer?: boolean;
+}> = ({ aksjonspunkter, submitCallback, svangerskapspengerTilrettelegging, arbeidOgInntekt, erOverstyrer = false }) => (
+  <TilretteleggingFaktaIndex
+    submitCallback={submitCallback}
+    readOnly={false}
+    harApneAksjonspunkter
+    submittable
+    alleMerknaderFraBeslutter={{}}
+    setFormData={() => undefined}
+    behandling={behandling}
+    svangerskapspengerTilrettelegging={svangerskapspengerTilrettelegging}
+    aksjonspunkter={aksjonspunkter}
+    erOverstyrer={erOverstyrer}
+    // @ts-ignore fixme
+    arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+    alleKodeverk={alleKodeverk as any}
+    arbeidOgInntekt={arbeidOgInntekt}
+  />
+);
+
+export const TilretteleggingMedVelferdspermisjon = Template.bind({});
+TilretteleggingMedVelferdspermisjon.args = {
+  submitCallback: action('button-click') as (data: any) => Promise<any>,
+  aksjonspunkter: [
+    {
+      definisjon: AksjonspunktCode.FODSELTILRETTELEGGING,
+      status: aksjonspunktStatus.OPPRETTET,
+      begrunnelse: undefined,
+      kanLoses: true,
+    },
+  ],
+  svangerskapspengerTilrettelegging: tilretteleggingPermisjon,
+  arbeidOgInntekt: spesiellArbeidOgInntekt,
+};
+
+export const AksjonspunktForFødselstilretteleggingForFrilanserOgSelvstendigNæringsdrivende = Template.bind({});
+AksjonspunktForFødselstilretteleggingForFrilanserOgSelvstendigNæringsdrivende.args = {
+  submitCallback: action('button-click') as (data: any) => Promise<any>,
+  aksjonspunkter: [
+    {
+      definisjon: AksjonspunktCode.FODSELTILRETTELEGGING,
+      status: aksjonspunktStatus.OPPRETTET,
+      begrunnelse: undefined,
+      kanLoses: true,
+    },
+  ],
+  svangerskapspengerTilrettelegging: svangerskapspengerTilretteleggingForFrilanser,
+  arbeidOgInntekt: defaultArbeidOgInntekt,
+};
+
+export const ErOverstyrer = Template.bind({});
+ErOverstyrer.args = {
+  submitCallback: action('button-click') as (data: any) => Promise<any>,
+  aksjonspunkter: [
+    {
+      definisjon: AksjonspunktCode.FODSELTILRETTELEGGING,
+      status: aksjonspunktStatus.OPPRETTET,
+      begrunnelse: undefined,
+      kanLoses: true,
+    },
+  ],
+  svangerskapspengerTilrettelegging: tilretteleggingPermisjon,
+  arbeidOgInntekt: spesiellArbeidOgInntekt,
+  erOverstyrer: true,
+};
+
+export const HarOpphold = Template.bind({});
+HarOpphold.args = {
+  submitCallback: action('button-click') as (data: any) => Promise<any>,
+  aksjonspunkter: [
+    {
+      definisjon: AksjonspunktCode.FODSELTILRETTELEGGING,
+      status: aksjonspunktStatus.OPPRETTET,
+      begrunnelse: undefined,
+      kanLoses: true,
+    },
+  ],
+  svangerskapspengerTilrettelegging: {
+    ...tilretteleggingPermisjon,
+    arbeidsforholdListe: [
+      {
+        tilretteleggingId: 1116961,
+        tilretteleggingBehovFom: '2020-03-17',
+        tilretteleggingDatoer: [
+          {
+            fom: '2020-03-17',
+            type: 'INGEN_TILRETTELEGGING',
+          },
+          {
+            fom: '2020-10-15',
+            type: 'HEL_TILRETTELEGGING',
+          },
+        ],
+        arbeidsgiverReferanse: '999999999',
+        uttakArbeidType: 'FRILANS',
+        skalBrukes: true,
+        kanTilrettelegges: true,
+        velferdspermisjoner: [],
+        avklarteOppholdPerioder: [
+          {
+            fom: '2020-09-15',
+            tom: '2020-09-20',
+            oppholdÅrsak: 'SYKEPENGER',
+            forVisning: false,
+          },
+          {
+            fom: '2020-09-25',
+            tom: '2020-09-30',
+            oppholdÅrsak: 'FERIE',
+            forVisning: true,
+          },
+        ],
+      },
+    ],
+  },
+  arbeidOgInntekt: spesiellArbeidOgInntekt,
+  erOverstyrer: true,
+};
---
+++ packages/fakta-tilrettelegging/src/components/TilretteleggingFaktaForm.tsx
@@ -0,0 +1,163 @@
+import React, { useMemo, FunctionComponent } from 'react';
+import { useForm } from 'react-hook-form';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Datepicker, Form, TextAreaField } from '@navikt/ft-form-hooks';
+import {
+  AksjonspunktHelpTextHTML,
+  FlexColumn,
+  FlexContainer,
+  FlexRow,
+  VerticalSpacer,
+} from '@navikt/ft-ui-komponenter';
+import { hasValidDate, hasValidText, maxLength, required } from '@navikt/ft-form-validators';
+
+import {
+  Aksjonspunkt,
+  ArbeidsgiverOpplysningerPerId,
+  FodselOgTilrettelegging,
+  ArbeidsforholdFodselOgTilrettelegging,
+} from '@navikt/fp-types';
+import { BekreftSvangerskapspengerAp } from '@navikt/fp-types-avklar-aksjonspunkter';
+import { AksjonspunktCode } from '@navikt/fp-kodeverk';
+
+import { FaktaSubmitButtonNew } from '@navikt/fp-fakta-felles';
+import ArbeidsgiverFieldArray from './arbeidsgiver/ArbeidsgiverFieldArray';
+
+const maxLength1500 = maxLength(1500);
+
+type FormValues = {
+  arbeidsforhold: ArbeidsforholdFodselOgTilrettelegging[];
+  termindato: string;
+  fødselsdato: string;
+  begrunnelse: string;
+};
+
+const sorterArbeidsforhold = (
+  alleArbeidsforhold: ArbeidsforholdFodselOgTilrettelegging[],
+  arbeidsgiverOpplysningerPerId: ArbeidsgiverOpplysningerPerId,
+): ArbeidsforholdFodselOgTilrettelegging[] =>
+  [...(alleArbeidsforhold || [])].sort((a, b) => {
+    const arbeidsgiverOpplysningerA1 = arbeidsgiverOpplysningerPerId[a.arbeidsgiverReferanse];
+    const arbeidsgiverOpplysningerA2 = arbeidsgiverOpplysningerPerId[b.arbeidsgiverReferanse];
+    return arbeidsgiverOpplysningerA1 && arbeidsgiverOpplysningerA2
+      ? arbeidsgiverOpplysningerA1.navn.localeCompare(arbeidsgiverOpplysningerA2.navn)
+      : 0;
+  });
+
+const getAksjonspunktBegrunnelse = (aksjonspunkter: Aksjonspunkt[]): string | undefined => {
+  const aksjonpunkt = aksjonspunkter.find(ap => ap.definisjon === AksjonspunktCode.FODSELTILRETTELEGGING);
+  return aksjonpunkt ? aksjonpunkt.begrunnelse : undefined;
+};
+
+const getIsBegrunnelseRequired = (isDirty: boolean) => (value?: string) => value !== undefined || isDirty;
+
+interface OwnProps {
+  behandlingVersjon: number;
+  readOnly: boolean;
+  hasOpenAksjonspunkter: boolean;
+  arbeidsgiverOpplysningerPerId: ArbeidsgiverOpplysningerPerId;
+  svangerskapspengerTilrettelegging: FodselOgTilrettelegging;
+  aksjonspunkter: Aksjonspunkt[];
+  submitCallback: (data: BekreftSvangerskapspengerAp) => Promise<void>;
+  formData: FormValues;
+  setFormData: (data: FormValues) => void;
+  submittable: boolean;
+}
+
+/**
+ * TilretteleggingFaktaForm
+ *
+ * Viser tillrettlegging før svangerskapspenger
+ */
+const TilretteleggingFaktaForm: FunctionComponent<OwnProps> = ({
+  behandlingVersjon,
+  readOnly,
+  hasOpenAksjonspunkter,
+  arbeidsgiverOpplysningerPerId,
+  aksjonspunkter,
+  svangerskapspengerTilrettelegging,
+  submitCallback,
+  formData,
+  setFormData,
+  submittable,
+}) => {
+  const intl = useIntl();
+
+  const sorterteArbeidsforhold = useMemo(
+    () => sorterArbeidsforhold(svangerskapspengerTilrettelegging.arbeidsforholdListe, arbeidsgiverOpplysningerPerId),
+    [behandlingVersjon],
+  );
+
+  const formMethods = useForm<FormValues>({
+    defaultValues: formData || {
+      arbeidsforhold: sorterteArbeidsforhold,
+      termindato: svangerskapspengerTilrettelegging ? svangerskapspengerTilrettelegging.termindato : '',
+      fødselsdato: svangerskapspengerTilrettelegging ? svangerskapspengerTilrettelegging.fødselsdato : '',
+      begrunnelse: getAksjonspunktBegrunnelse(aksjonspunkter),
+    },
+  });
+
+  const fødselsdato = svangerskapspengerTilrettelegging?.fødselsdato || '';
+
+  const isRequiredFn = getIsBegrunnelseRequired(formMethods.formState.isDirty);
+
+  return (
+    // @ts-ignore fixme
+    <Form formMethods={formMethods} setDataOnUnmount={setFormData} onSubmit={values => submitCallback(values)}>
+      {hasOpenAksjonspunkter && (
+        <>
+          <AksjonspunktHelpTextHTML>
+            {[<FormattedMessage id="TilretteleggingFaktaForm.Aksjonspunkt" key="svangerskapspengerAp" />]}
+          </AksjonspunktHelpTextHTML>
+          <VerticalSpacer thirtyTwoPx />
+        </>
+      )}
+      <FlexContainer wrap>
+        <FlexRow>
+          <FlexColumn>
+            <Datepicker
+              name="termindato"
+              label={intl.formatMessage({ id: 'TilretteleggingFaktaForm.Termindato' })}
+              validate={[required, hasValidDate]}
+              isReadOnly={readOnly}
+            />
+          </FlexColumn>
+          {fødselsdato && (
+            <FlexColumn>
+              <Datepicker
+                name="fødselsdato"
+                label={intl.formatMessage({ id: 'TilretteleggingFaktaForm.Fodselsdato' })}
+                validate={[required, hasValidDate]}
+                isReadOnly={readOnly}
+              />
+            </FlexColumn>
+          )}
+        </FlexRow>
+      </FlexContainer>
+      <VerticalSpacer fourtyPx />
+      <ArbeidsgiverFieldArray
+        sorterteArbeidsforhold={sorterteArbeidsforhold}
+        arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+        readOnly={readOnly}
+      />
+      <VerticalSpacer fourtyPx />
+      <TextAreaField
+        name="begrunnelse"
+        label={intl.formatMessage({ id: 'TilretteleggingFaktaForm.BegrunnEndringene' })}
+        validate={[isRequiredFn, maxLength1500, hasValidText]}
+        maxLength={1500}
+        readOnly={readOnly}
+      />
+      <VerticalSpacer fourtyPx />
+      <FaktaSubmitButtonNew
+        isSubmittable={submittable}
+        isReadOnly={readOnly}
+        isSubmitting={formMethods.formState.isSubmitting}
+        isDirty={formMethods.formState.isDirty}
+      />
+      <VerticalSpacer fourtyPx />
+    </Form>
+  );
+};
+
+export default TilretteleggingFaktaForm;
