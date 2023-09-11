1a1991fa4248b5063038e1e1e6fd9923f95a8e8e
tor-nav <tor.roneid@nav.no>
2023-09-11 12:56:50 +0200

    div

--- packages/sak-historikk/src/components/maler/felles/snakkeboble.tsx
+++ packages/sak-historikk/src/components/maler/felles/snakkeboble.tsx
@@ -1,45 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import { Chat } from '@navikt/ds-react';
-import { Image } from '@navikt/ft-ui-komponenter';
+import {
+  FigureInwardFillIcon,
+  FigureOutwardFillIcon,
+  PersonCheckmarkFillIcon,
+  PersonPencilFillIcon,
+  PersonSuitFillIcon,
+  RobotSmileIcon,
+} from '@navikt/aksel-icons';
 
 import { historikkAktor as HistorikkAktor, navBrukerKjonn } from '@navikt/fp-kodeverk';
-import kvinneImg from '../../../images/kvinne.svg';
-import maskinImg from '../../../images/maskin.svg';
-import arbeidsgiverImg from '../../../images/arbeidsgiver.svg';
-import mannImg from '../../../images/mann.svg';
-import beslutterImg from '../../../images/beslutter.svg';
-import navAnsattHistorikkImg from '../../../images/nav_ansatt_historikk.svg';
 
+import { IntlShape, useIntl } from 'react-intl';
 import styles from './snakkeboble.module.css';
 
 const formatDate = (date: string): string =>
   `${date.substring(8, 10)}.${date.substring(5, 7)}.${date.substring(0, 4)} - ${date.substring(11, 16)}`;
-
-const utledIkon = (aktoer: string, kjoenn?: string) => {
-  if (aktoer === HistorikkAktor.SAKSBEHANDLER) {
-    return navAnsattHistorikkImg;
-  }
-  if (aktoer === HistorikkAktor.SOKER && kjoenn === navBrukerKjonn.MANN) {
-    return mannImg;
-  }
-  if (aktoer === HistorikkAktor.SOKER && kjoenn === navBrukerKjonn.KVINNE) {
-    return kvinneImg;
-  }
-  if (aktoer === HistorikkAktor.BESLUTTER) {
-    return beslutterImg;
-  }
-  if (aktoer === HistorikkAktor.VEDTAKSLOSNINGEN) {
-    return maskinImg;
-  }
-  return arbeidsgiverImg;
-};
-
-const utledPlassering = (aktoer: string): 'right' | 'left' =>
-  aktoer === HistorikkAktor.SAKSBEHANDLER ||
-  aktoer === HistorikkAktor.VEDTAKSLOSNINGEN ||
-  aktoer === HistorikkAktor.BESLUTTER
-    ? 'right'
-    : 'left';
 
 const BAKGRUNNSFARGER = {
   [HistorikkAktor.SAKSBEHANDLER]: styles.bubbleSaksbehandler,
@@ -48,6 +24,56 @@ const BAKGRUNNSFARGER = {
   [HistorikkAktor.SOKER]: styles.bubbleSoker,
   [HistorikkAktor.ARBEIDSGIVER]: styles.bubbleArbeidsgiver,
 } as Record<string, string>;
+
+const utledIkon = (intl: IntlShape, aktoer: string, kjoenn?: string) => {
+  if (aktoer === HistorikkAktor.SAKSBEHANDLER) {
+    return (
+      <PersonPencilFillIcon
+        className={styles.imageSaksbehandler}
+        title={intl.formatMessage({ id: 'Snakkeboble.Saksbehandler' })}
+      />
+    );
+  }
+  if (aktoer === HistorikkAktor.SOKER && kjoenn === navBrukerKjonn.MANN) {
+    return (
+      <FigureInwardFillIcon className={styles.imageMann} title={intl.formatMessage({ id: 'Snakkeboble.Soker' })} />
+    );
+  }
+  if (aktoer === HistorikkAktor.SOKER && kjoenn === navBrukerKjonn.KVINNE) {
+    return (
+      <FigureOutwardFillIcon className={styles.imageKvinne} title={intl.formatMessage({ id: 'Snakkeboble.Soker' })} />
+    );
+  }
+  if (aktoer === HistorikkAktor.BESLUTTER) {
+    return (
+      <PersonCheckmarkFillIcon
+        className={styles.imageBeslutter}
+        title={intl.formatMessage({ id: 'Snakkeboble.Beslutter' })}
+      />
+    );
+  }
+  if (aktoer === HistorikkAktor.VEDTAKSLOSNINGEN) {
+    return (
+      <RobotSmileIcon
+        className={styles.imageVedtak}
+        title={intl.formatMessage({ id: 'Snakkeboble.Vedtakslosninger' })}
+      />
+    );
+  }
+  return (
+    <PersonSuitFillIcon
+      className={styles.imageArbeidsgiver}
+      title={intl.formatMessage({ id: 'Snakkeboble.Arbeidsgiver' })}
+    />
+  );
+};
+
+const utledPlassering = (aktoer: string): 'right' | 'left' =>
+  aktoer === HistorikkAktor.SAKSBEHANDLER ||
+  aktoer === HistorikkAktor.VEDTAKSLOSNINGEN ||
+  aktoer === HistorikkAktor.BESLUTTER
+    ? 'right'
+    : 'left';
 
 interface OwnProps {
   dato: string;
@@ -67,17 +93,20 @@ const Snakkeboble: FunctionComponent<OwnProps> = ({
   opprettetAv,
   children,
   erFørsteBoble,
-}) => (
-  <div className={erFørsteBoble ? styles.marginForste : styles.margin} data-testid={`snakkeboble-${dato}`}>
-    <Chat
-      avatar={<Image className={styles.image} src={utledIkon(aktoer, kjoenn)} />}
-      timestamp={`${formatDate(dato)} // ${rolleNavn} ${opprettetAv || ''}`}
-      position={utledPlassering(aktoer)}
-      toptextPosition="left"
-    >
-      <Chat.Bubble className={BAKGRUNNSFARGER[aktoer]}>{children}</Chat.Bubble>
-    </Chat>
-  </div>
-);
+}) => {
+  const intl = useIntl();
+  return (
+    <div className={erFørsteBoble ? styles.marginForste : styles.margin} data-testid={`snakkeboble-${dato}`}>
+      <Chat
+        avatar={utledIkon(intl, aktoer, kjoenn)}
+        timestamp={`${formatDate(dato)} // ${rolleNavn} ${opprettetAv || ''}`}
+        position={utledPlassering(aktoer)}
+        toptextPosition="left"
+      >
+        <Chat.Bubble className={BAKGRUNNSFARGER[aktoer]}>{children}</Chat.Bubble>
+      </Chat>
+    </div>
+  );
+};
 
 export default Snakkeboble;
