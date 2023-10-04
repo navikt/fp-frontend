d6d05888798390f458e00bd57b7d77dff34b62c9
Petter Ekern <petter.ekern@nav.no>
2023-10-04 08:29:23 +0200

    Ny test for generell journalføring

--- packages/journalforing/src/components/journalpost/JournalpostDetaljer.spec.tsx
+++ packages/journalforing/src/components/journalpost/JournalpostDetaljer.spec.tsx
@@ -3,6 +3,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { composeStories } from '@storybook/react';
 import userEvent from '@testing-library/user-event';
 import * as stories from './JournalpostDetaljer.stories';
+import Sakstype from '../../kodeverk/sakstype';
 
 const { VisOppgaveForSubmitReservertAvMeg } = composeStories(stories);
 
@@ -98,6 +99,55 @@ describe('<JournalforingIndex>', () => {
       opprettSak: {
         aktørId: '98594685464858',
         ytelseType: 'FP',
+        sakstype: Sakstype.FAGSAK,
+      },
+    });
+  });
+
+  it('skal kunne journalføre på ny sak', async () => {
+    const journalfør = vi.fn();
+
+    render(<VisOppgaveForSubmitReservertAvMeg submitJournalføring={journalfør} />);
+    expect(await screen.findByText('Søker Søkersen')).toBeInTheDocument();
+
+    expect(screen.getByText('12048714373')).toBeInTheDocument();
+    expect(screen.getByText('Svingen sag og høvleri')).toBeInTheDocument();
+    expect(screen.getByText('999999999')).toBeInTheDocument();
+    expect(screen.getAllByText('Bekreftelse fra arbeidsgiver')).toHaveLength(2);
+    expect(screen.getByText('Søknad om foreldrepenger ved fødsel')).toBeInTheDocument();
+    expect(screen.getByText('Annet')).toBeInTheDocument();
+
+    expect(screen.getByLabelText('125416597 Foreldrepenger')).toBeInTheDocument();
+    expect(screen.getByLabelText('155462542 Svangerskapspenger')).toBeInTheDocument();
+    expect(screen.getByLabelText('175419131 Foreldrepenger')).toBeInTheDocument();
+    expect(screen.getByLabelText('Opprett ny sak')).toBeInTheDocument();
+
+    expect(screen.getByText('Journalfør').closest('button')).toBeDisabled();
+    expect(screen.getByText('Avbryt').closest('button')).toBeEnabled();
+
+    expect(screen.getAllByRole('radio')).toHaveLength(5);
+    expect(screen.getAllByRole('radio')[0]).not.toBeChecked();
+    expect(screen.getAllByRole('radio')[1]).not.toBeChecked();
+    expect(screen.getAllByRole('radio')[2]).not.toBeChecked();
+    expect(screen.getAllByRole('radio')[3]).not.toBeChecked();
+    expect(screen.getAllByRole('radio')[4]).not.toBeChecked();
+    expect(screen.queryByLabelText('Ytelse')).not.toBeInTheDocument;
+    await userEvent.click(screen.getAllByRole('radio')[0]);
+    expect(screen.getByText('Journalfør').closest('button')).toBeEnabled();
+    expect(screen.queryByLabelText('Ytelse')).not.toBeInTheDocument;
+    await userEvent.click(screen.getAllByRole('radio')[4]);
+    expect(screen.queryByLabelText('Ytelse')).not.toBeInTheDocument;
+    await userEvent.click(screen.getByText('Journalfør'));
+    await waitFor(() => expect(journalfør).toHaveBeenCalledTimes(1));
+
+    expect(journalfør).toHaveBeenNthCalledWith(1, {
+      enhetId: '4108',
+      oppgaveId: 600,
+      oppdaterTitlerDto: undefined,
+      journalpostId: '986547336994',
+      opprettSak: {
+        aktørId: '98594685464858',
+        sakstype: Sakstype.GENERELL,
       },
     });
   });
