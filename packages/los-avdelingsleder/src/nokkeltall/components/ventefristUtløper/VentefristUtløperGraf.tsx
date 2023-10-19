45648dbe3d36d0f180fb85d53b2271b7276d43b9
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-10-19 23:29:48 +0200

    TFP-5589 erstatning for ventegraf - alle førstegang på vent fordelt på frist

---
+++ packages/los-avdelingsleder/src/nokkeltall/components/ventefristUtløper/VentefristUtløperGraf.tsx
@@ -0,0 +1,106 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import { Panel } from '@navikt/ds-react';
+import { DDMMYYYY_DATE_FORMAT } from '@navikt/ft-utils';
+import { ReactECharts } from '@navikt/fp-los-felles';
+
+import OppgaverManueltPaVent from '../../../typer/oppgaverManueltPaVentTsType';
+
+dayjs.extend(isSameOrBefore);
+
+interface Koordinat {
+  x: number;
+  y: number;
+}
+
+const lagKoordinater = (oppgaverManueltPaVent: OppgaverManueltPaVent[]): Koordinat[] =>
+  oppgaverManueltPaVent.map(o => ({
+    x: dayjs(o.behandlingFrist).startOf('day').toDate().getTime(),
+    y: o.antall,
+  }));
+
+const lagDatastruktur = (koordinater: Koordinat[]): (number | Date)[][] => {
+  const nyeKoordinater = [];
+  const periodeStart = koordinater
+    .map(koordinat => dayjs(koordinat.x))
+    .reduce(
+      (tidligesteDato, dato) => (tidligesteDato.isSameOrBefore(dato) ? tidligesteDato : dato),
+      dayjs().startOf('day'),
+    )
+    .toDate();
+  const periodeSlutt = koordinater
+    .map(koordinat => dayjs(koordinat.x))
+    .reduce((senesteDato, dato) => (senesteDato.isSameOrAfter(dato) ? senesteDato : dato), dayjs().startOf('day'))
+    .toDate();
+
+  for (let dato = dayjs(periodeStart); dato.isSameOrBefore(periodeSlutt); dato = dato.add(1, 'days')) {
+    const funnetKoordinat = koordinater.find(k => dayjs(k.x).isSame(dato));
+    nyeKoordinater.push([dato.toDate(), funnetKoordinat ? funnetKoordinat.y : 0]);
+  }
+
+  return nyeKoordinater;
+};
+
+interface OwnProps {
+  height: number;
+  behandlingerPaVent: OppgaverManueltPaVent[];
+}
+
+/**
+ * VentefristUtløperGraf.
+ */
+const VentefristUtløperGraf: FunctionComponent<OwnProps> = ({ height, behandlingerPaVent }) => {
+  const koordinater = useMemo(() => lagKoordinater(behandlingerPaVent), [behandlingerPaVent]);
+  const data = useMemo(() => lagDatastruktur(koordinater), [koordinater]);
+  return (
+    <Panel>
+      <ReactECharts
+        height={height}
+        option={{
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              snap: true,
+              label: {
+                formatter: params => {
+                  if (params.axisDimension === 'y') {
+                    return parseInt(params.value as string, 10).toString();
+                  }
+                  return dayjs(params.value).format(DDMMYYYY_DATE_FORMAT);
+                },
+              },
+            },
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {
+                title: 'Lagre ',
+                name: 'Antall_forstegangsbehandlinger_der_frist_utloper',
+              },
+            },
+          },
+          xAxis: {
+            type: 'time',
+            axisLabel: {
+              formatter: '{dd}.{MM}.{yyyy}',
+            },
+          },
+          yAxis: {
+            type: 'value',
+          },
+          series: [
+            {
+              data,
+              type: 'line',
+              areaStyle: {},
+            },
+          ],
+          color: ['#337c9b'],
+        }}
+      />
+    </Panel>
+  );
+};
+
+export default VentefristUtløperGraf;
