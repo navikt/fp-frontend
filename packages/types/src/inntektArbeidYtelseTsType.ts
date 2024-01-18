10c1098c2a8d7a5671068aede9d1f00521a74f5a
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-01-18 12:18:10 +0100

    Fjerne IAY

--- packages/types/src/inntektArbeidYtelseTsType.ts
+++ packages/types/src/inntektArbeidYtelseTsType.ts
@@ -8,11 +8,6 @@ export type RelatertTilgrensedYtelse = Readonly<{
   }[];
 }>;
 
-export type IAYInntektsmelding = Readonly<{
-  arbeidsgiverReferanse: string;
-  arbeidsgiverStartdato?: string;
-}>;
-
 type InntektArbeidYtelse = Readonly<{
   relatertTilgrensendeYtelserForSoker: RelatertTilgrensedYtelse[];
   relatertTilgrensendeYtelserForAnnenForelder: RelatertTilgrensedYtelse[];
