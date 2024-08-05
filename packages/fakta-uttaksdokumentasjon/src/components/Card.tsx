9a756b0848f6010fc6a410078e7510030e4b8776
Siri Mykland <siri.mykland@nav.no>
2024-08-05 10:45:22 +0200

    fikse warnings i card component

--- packages/fakta-uttaksdokumentasjon/src/components/Card.tsx
+++ packages/fakta-uttaksdokumentasjon/src/components/Card.tsx
@@ -1,25 +1,27 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { Box, HStack, VStack } from '@navikt/ds-react';
 
-const Card: FC<PropsWithChildren> & {
-  Header: FC<CardHeaderProps>;
-  Content: FC<CardContentProps>;
-} = ({ children }) => <div>{children}</div>;
+const CardWrapper: FC<PropsWithChildren> = ({ children }: PropsWithChildren) => <div>{children}</div>;
 
-Card.Header = ({ children }: PropsWithChildren) => (
+const CardHeader: FC<PropsWithChildren> = ({ children }) => (
   <Box background="surface-neutral-moderate" paddingInline="4" paddingBlock="2">
     <HStack gap="6">{children}</HStack>
   </Box>
 );
 
-Card.Header.displayName = 'Card.Header';
-
-Card.Content = ({ children }: PropsWithChildren) => (
+const CardContent: FC<PropsWithChildren> = ({ children }) => (
   <Box background="surface-neutral-subtle" paddingInline="4" paddingBlock="2">
     <VStack gap="6">{children}</VStack>
   </Box>
 );
 
-Card.Content.displayName = 'Card.Content';
+type CardProps = typeof CardWrapper & {
+  Header: typeof CardHeader;
+  Content: typeof CardContent;
+};
+
+const Card = CardWrapper as CardProps;
+Card.Header = CardHeader;
+Card.Content = CardContent;
 
 export default Card;
