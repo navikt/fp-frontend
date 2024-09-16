6f4894a5729372626ebdfdea6844bbd6ee46e72a
johannbm <johannes.moskvil@gmail.com>
2024-09-16 15:52:06 +0200

    brukt felt for aktive istedenfor bortfalte

--- packages/fakta-inntektsmelding/src/InntektsmeldingInnhold.tsx
+++ packages/fakta-inntektsmelding/src/InntektsmeldingInnhold.tsx
@@ -170,7 +170,7 @@ const BortfalteNaturalYtelser = ({ inntektsmelding }: { inntektsmelding: Inntekt
     <InntektsmeldingInfoBlokk
       tittel={intl.formatMessage({ id: 'InntektsmeldingFaktaPanel.bortfalteNaturalytelser.heading' })}
     >
-      {inntektsmelding.bortfalteNaturalytelser.length === 0 ? (
+      {inntektsmelding.aktiveNaturalytelser.length === 0 ? (
         <span>
           <FormattedMessage id="InntektsmeldingFaktaPanel.bortfalteNaturalytelser.ingen" />
         </span>
@@ -212,7 +212,7 @@ const BortfalteNaturalYtelser = ({ inntektsmelding }: { inntektsmelding: Inntekt
  * bortfalt periode: {fomDato: '2024-09-05', tomDato: '2024-09-26'}
  */
 const konverterAktivePerioderTilBortfaltePerioder = (inntektsmelding: Inntektsmelding) => {
-  const aktiveNaturalytelser = inntektsmelding.bortfalteNaturalytelser;
+  const aktiveNaturalytelser = inntektsmelding.aktiveNaturalytelser;
 
   const gruppertPåType = aktiveNaturalytelser.reduce((prev, value) => {
     const type = value.type;
