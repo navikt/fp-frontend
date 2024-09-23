27f821b4f42d0e77a07463fffbe35594ed729de5
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2024-09-23 09:03:29 +0200

    fjerne kodeverk i for land i medlemskapsfaktapanel (#5893)

--- packages/fakta-medlemskap/src/v3/utils/landUtils.ts
+++
@@ -1,8 +0,0 @@
-import { AlleKodeverk } from '@navikt/fp-types';
-import { KodeverkType } from '@navikt/fp-kodeverk';
-import { toTitleCapitalization } from './stringUtils';
-
-export const getLandnavnFraLandkode = (landKode: string | null, alleKodeverk: AlleKodeverk) => {
-  const landnavn = alleKodeverk[KodeverkType.LANDKODER].find(kv => kv.kode == landKode)?.navn;
-  return landnavn ? toTitleCapitalization(landnavn) : undefined;
-};
