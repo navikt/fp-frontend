415b37d77c6d48c8d9bb6f79c06e434282b1789b
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2024-09-25 10:58:17 +0200

    endre sisteBostedLand til å gi I utland/I Norge (#5906)

--- packages/fakta-medlemskap/src/v3/components/situasjon/situasjonUtils.ts
+++ packages/fakta-medlemskap/src/v3/components/situasjon/situasjonUtils.ts
@@ -1,13 +1,9 @@
 import { AlleKodeverk, MedlemskapV3, UtlandsoppholdPeriode } from '@navikt/fp-types';
-import { getKodeverknavnFn, KodeverkType, opplysningAdresseType } from '@navikt/fp-kodeverk';
-import kodeverkTyper from '@navikt/fp-kodeverk/src/kodeverkTyper';
+import { getKodeverknavnFn, KodeverkType, landkoder, opplysningAdresseType } from '@navikt/fp-kodeverk';
 import { IntlShape } from 'react-intl';
-import { toTitleCapitalization } from '../../utils/stringUtils';
-import { sorterPerioder } from '../../utils/periodeUtils';
 
-const formaterLand = (landNavn: string, intl: IntlShape) => {
-  return intl.formatMessage({ id: 'Situasjon.ILand' }, { land: toTitleCapitalization(landNavn) });
-};
+import { sorterPerioder } from '../../utils/periodeUtils';
+import { toTitleCapitalization } from '../../utils/stringUtils';
 
 export const getSisteRegion = (medlemskap: MedlemskapV3, alleKodeverk: AlleKodeverk): string | null => {
   const alleRegioner = alleKodeverk[KodeverkType.REGION];
@@ -23,18 +19,15 @@ export const getSistePersonstatus = (medlemskap: MedlemskapV3, alleKodeverk: All
   return null;
 };
 
-export const getSisteBostedsLand = (
-  medlemskap: MedlemskapV3,
-  alleKodeverk: AlleKodeverk,
-  intl: IntlShape,
-): string | null => {
+export const getSisteBostedsLand = (medlemskap: MedlemskapV3, intl: IntlShape): string | null => {
   const nyeste = medlemskap.adresser
     .filter(adresse => adresse.adresse.adresseType === opplysningAdresseType.BOSTEDSADRESSE)
     .sort(sorterPerioder)[0]?.adresse.land;
-  if (nyeste) {
-    return formaterLand(nyeste, intl);
+  if (!nyeste) return null;
+  if ([landkoder.NORGE, 'Norge'].includes(nyeste)) {
+    return intl.formatMessage({ id: 'Situasjon.INorge' });
   }
-  return nyeste ?? null;
+  return intl.formatMessage({ id: 'Situasjon.IUtlandet' });
 };
 
 export const formaterUtenlandsopphold = (
@@ -42,11 +35,13 @@ export const formaterUtenlandsopphold = (
   alleKodeverk: AlleKodeverk,
   intl: IntlShape,
 ): string => {
-  if (utenlandsopphold.length == 1) {
-    return formaterLand(utenlandsopphold[0].landNavn, intl);
-  } else if (utenlandsopphold.length > 1) {
+  if (utenlandsopphold.length > 1) {
     return intl.formatMessage({ id: 'Situasjon.FlereLand' });
   }
-  const landNavn = getKodeverknavnFn(alleKodeverk)('NOR', kodeverkTyper.LANDKODER);
-  return formaterLand(landNavn, intl);
+  const landNavn =
+    utenlandsopphold.length === 1
+      ? utenlandsopphold[0].landNavn
+      : getKodeverknavnFn(alleKodeverk)(landkoder.NORGE, KodeverkType.LANDKODER);
+
+  return intl.formatMessage({ id: 'Situasjon.ILand' }, { land: toTitleCapitalization(landNavn) });
 };
