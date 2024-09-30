9ffa9f5eb47cfb4523e57dce19d4a4bdb4a2f6c9
Siri Mykland <siri.mykland@nav.no>
2024-09-30 14:11:38 +0200

    fjerne isVilkårOk from Overstyring typer

--- packages/types-avklar-aksjonspunkter/src/prosess/OverstyringMedlemskapsvilkaretAp.ts
+++ packages/types-avklar-aksjonspunkter/src/prosess/OverstyringMedlemskapsvilkaretAp.ts
@@ -2,7 +2,6 @@ import AksjonspunktTilBekreftelse from '../AksjonspunktTilBekreftelse';
 import { AksjonspunktCode } from '@navikt/fp-kodeverk';
 
 type OverstyringMedlemskapsvilkaretAp = {
-  erVilkarOk?: boolean;
   avslagskode?: string;
   opphørFom?: string;
 } & AksjonspunktTilBekreftelse<AksjonspunktCode.OVERSTYR_MEDLEMSKAPSVILKAR>;
--- packages/types-avklar-aksjonspunkter/src/prosess/OverstyringMedlemskapvilkaretForutgaendeAp.ts
+++ packages/types-avklar-aksjonspunkter/src/prosess/OverstyringMedlemskapvilkaretForutgaendeAp.ts
@@ -2,7 +2,6 @@ import AksjonspunktTilBekreftelse from '../AksjonspunktTilBekreftelse';
 import { AksjonspunktCode } from '@navikt/fp-kodeverk';
 
 type OverstyringMedlemskapsvilkaretForutgaendeAp = {
-  erVilkarOk?: boolean;
   avslagskode?: string;
   medlemFom?: string;
 } & AksjonspunktTilBekreftelse<AksjonspunktCode.OVERSTYR_MEDLEMSKAPSVILKAR_FORUTGAENDE>;
