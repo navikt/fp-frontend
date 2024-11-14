91c00657f3cba9bc3293f11554fa99fa5a634e37
johannbm <johannes.moskvil@gmail.com>
2024-11-14 11:18:23 +0100

    ts headers

--- server/src/headers.ts
+++ server/src/headers.ts
@@ -1,12 +1,17 @@
-export const setup = app => {
-  app.disable('x-powered-by');
-  app.use((req, res, next) => {
-    res.header('X-Content-Type-Options', 'nosniff');
-    res.header('X-Xss-Protection', '1; mode=block');
-    res.header('X-Frame-Options', 'DENY');
-    res.header('Referrer-Policy', 'origin');
-    res.header('Strict-Transport-Security', 'max-age=31536000');
-    res.header('Feature-Policy', "geolocation 'none'; microphone 'none'; camera 'none'");
+import { Express } from "express";
+
+export const addHeaders = (app: Express) => {
+  app.disable("x-powered-by");
+  app.use((_, response, next) => {
+    response.header("X-Content-Type-Options", "nosniff");
+    response.header("X-Xss-Protection", "1; mode=block");
+    response.header("X-Frame-Options", "DENY");
+    response.header("Referrer-Policy", "origin");
+    response.header("Strict-Transport-Security", "max-age=31536000");
+    response.header(
+      "Feature-Policy",
+      "geolocation 'none'; microphone 'none'; camera 'none'",
+    );
     next();
   });
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,16 @@
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import timeout from 'connect-timeout';
-import * as headers from './headers.js';
-import logger from './logger.js';
-import { serveViteMode } from '@navikt/vite-mode';
+import { serveViteMode } from "@navikt/vite-mode";
+import timeout from "connect-timeout";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
 
 // for debugging during development
-import config from './config.js';
-import { getUserGroups, getUserInfoFromGraphApi } from './msgraph.js';
-import reverseProxy from './reverse-proxy.js';
-import { verifyToken } from './tokenValidation.js';
+import config from "./config.js";
+import { addHeaders } from "./headers.js";
+import logger from "./logger.js";
+import { getUserGroups, getUserInfoFromGraphApi } from "./msgraph.js";
+import reverseProxy from "./reverse-proxy.js";
+import { verifyToken } from "./tokenValidation.js";
 
 const server = express();
 const { port } = config.server;
@@ -22,32 +22,36 @@ const globalErrorHandler = (err, req, res) => {
 
 async function startApp() {
   try {
-    server.use(timeout('10m'));
-    headers.setup(server);
+    server.use(timeout("10m"));
+    addHeaders(server);
 
     // Logging i json format
     server.use(logger.morganMiddleware);
 
-    server.set('trust proxy', 1);
+    server.set("trust proxy", 1);
 
     server.use(
       helmet({
         contentSecurityPolicy: {
           useDefaults: false,
           directives: {
-            'default-src': ["'self'"],
-            'base-uri': ["'self'"],
-            'connect-src': ["'self'", 'https://sentry.gc.nav.no', 'https://graph.microsoft.com'],
-            'font-src': ["'self'", 'https://cdn.nav.no', 'data:'],
-            'img-src': ["'self'", 'data:'],
-            'style-src': ["'self'", "'unsafe-inline'"],
-            'frame-src': ["'self'"],
-            'child-src': ["'self'"],
-            'media-src': ["'none'"],
-            'object-src': ["'none'"],
+            "default-src": ["'self'"],
+            "base-uri": ["'self'"],
+            "connect-src": [
+              "'self'",
+              "https://sentry.gc.nav.no",
+              "https://graph.microsoft.com",
+            ],
+            "font-src": ["'self'", "https://cdn.nav.no", "data:"],
+            "img-src": ["'self'", "data:"],
+            "style-src": ["'self'", "'unsafe-inline'"],
+            "frame-src": ["'self'"],
+            "child-src": ["'self'"],
+            "media-src": ["'none'"],
+            "object-src": ["'none'"],
           },
         },
-        referrerPolicy: { policy: 'origin' },
+        referrerPolicy: { policy: "origin" },
         hidePoweredBy: true,
         noSniff: true,
       }),
@@ -64,29 +68,31 @@ async function startApp() {
     );
 
     // Liveness and readiness probes for Kubernetes / nais
-    server.get(['/health/isAlive', '/health/isReady'], (req, res) => {
-      res.status(200).send('Alive');
+    server.get(["/health/isAlive", "/health/isReady"], (req, res) => {
+      res.status(200).send("Alive");
     });
 
-    server.get(['/oauth2/login'], async (req, res) => {
+    server.get(["/oauth2/login"], async (req, res) => {
       res.status(502).send({
-        message: 'Wonderwall must handle /oauth2/login',
+        message: "Wonderwall must handle /oauth2/login",
       });
     });
 
     // The routes below require the user to be authenticated
     server.use(verifyToken);
 
-    server.get(['/logout'], async (req, res) => {
+    server.get(["/logout"], async (req, res) => {
       if (req.headers.authorization) {
-        res.redirect('/oauth2/logout');
+        res.redirect("/oauth2/logout");
       }
     });
 
     // return user info fetched from the Microsoft Graph API
-    server.get('/me', async (req, res, next) => {
+    server.get("/me", async (req, res, next) => {
       try {
-        const userInfo = await getUserInfoFromGraphApi(req.headers.authorization);
+        const userInfo = await getUserInfoFromGraphApi(
+          req.headers.authorization,
+        );
         return res.json(userInfo);
       } catch (error) {
         return next(error);
@@ -94,9 +100,11 @@ async function startApp() {
     });
 
     // return groups that the user is a member of from the Microsoft Graph API
-    server.get('/me/memberOf', async (req, res, next) => {
+    server.get("/me/memberOf", async (req, res, next) => {
       try {
-        const userInfo = await getUserInfoFromGraphApi(req.headers.authorization);
+        const userInfo = await getUserInfoFromGraphApi(
+          req.headers.authorization,
+        );
         return res.json(userInfo);
       } catch (error) {
         return next(error);
@@ -105,21 +113,21 @@ async function startApp() {
 
     reverseProxy.setup(server);
 
-    serveViteMode(server, { port: '9010' });
+    serveViteMode(server, { port: "9010" });
 
     // serve static files
-    const rootDir = './dist';
+    const rootDir = "./dist";
     server.use(express.static(rootDir));
     server.use(/^\/(?!.*dist).*$/, (req, res) => {
-      res.sendFile('index.html', { root: rootDir });
+      res.sendFile("index.html", { root: rootDir });
     });
 
     server.use(globalErrorHandler);
 
     server.listen(port, () => logger.info(`Listening on port ${port}`));
   } catch (error) {
-    logger.error('Error during start-up: ', error);
+    logger.error("Error during start-up: ", error);
   }
 }
 
-startApp().catch(err => logger.error(err));
+startApp().catch((error) => logger.error(error));
