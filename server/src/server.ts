c1042ac1904ae636b3cb776901a1deba41da1028
johannbm <johannes.moskvil@gmail.com>
2024-11-14 13:27:43 +0100

    Fjern redundant try/catch

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,112 +16,108 @@ const server = express();
 const { port } = config.server;
 
 function startApp() {
-  try {
-    server.use(timeout("10m"));
-    addHeaders(server);
+  server.use(timeout("10m"));
+  addHeaders(server);
 
-    // Logging i json format
-    server.use(logger.morganMiddleware);
+  // Logging i json format
+  server.use(logger.morganMiddleware);
 
-    server.set("trust proxy", 1);
+  server.set("trust proxy", 1);
 
-    server.use(
-      helmet({
-        contentSecurityPolicy: {
-          useDefaults: false,
-          directives: {
-            "default-src": ["'self'"],
-            "base-uri": ["'self'"],
-            "connect-src": [
-              "'self'",
-              "https://sentry.gc.nav.no",
-              "https://graph.microsoft.com",
-            ],
-            "font-src": ["'self'", "https://cdn.nav.no", "data:"],
-            "img-src": ["'self'", "data:"],
-            "style-src": ["'self'", "'unsafe-inline'"],
-            "frame-src": ["'self'"],
-            "child-src": ["'self'"],
-            "media-src": ["'none'"],
-            "object-src": ["'none'"],
-          },
+  server.use(
+    helmet({
+      contentSecurityPolicy: {
+        useDefaults: false,
+        directives: {
+          "default-src": ["'self'"],
+          "base-uri": ["'self'"],
+          "connect-src": [
+            "'self'",
+            "https://sentry.gc.nav.no",
+            "https://graph.microsoft.com",
+          ],
+          "font-src": ["'self'", "https://cdn.nav.no", "data:"],
+          "img-src": ["'self'", "data:"],
+          "style-src": ["'self'", "'unsafe-inline'"],
+          "frame-src": ["'self'"],
+          "child-src": ["'self'"],
+          "media-src": ["'none'"],
+          "object-src": ["'none'"],
         },
-        referrerPolicy: { policy: "origin" },
-        hidePoweredBy: true,
-        noSniff: true,
-      }),
-    );
+      },
+      referrerPolicy: { policy: "origin" },
+      hidePoweredBy: true,
+      noSniff: true,
+    }),
+  );
 
-    // CORS konfig
-    server.use(
-      cors({
-        origin: config.server.host,
-        methods: config.cors.allowedMethods,
-        exposedHeaders: config.cors.exposedHeaders,
-        allowedHeaders: config.cors.allowedHeaders,
-      }),
-    );
+  // CORS konfig
+  server.use(
+    cors({
+      origin: config.server.host,
+      methods: config.cors.allowedMethods,
+      exposedHeaders: config.cors.exposedHeaders,
+      allowedHeaders: config.cors.allowedHeaders,
+    }),
+  );
 
-    // Liveness and readiness probes for Kubernetes / nais
-    server.get(["/health/isAlive", "/health/isReady"], (req, res) => {
-      res.status(200).send("Alive");
+  // Liveness and readiness probes for Kubernetes / nais
+  server.get(["/health/isAlive", "/health/isReady"], (req, res) => {
+    res.status(200).send("Alive");
+  });
+
+  server.get(["/oauth2/login"], async (req, res) => {
+    res.status(502).send({
+      message: "Wonderwall must handle /oauth2/login",
     });
+  });
 
-    server.get(["/oauth2/login"], async (req, res) => {
-      res.status(502).send({
-        message: "Wonderwall must handle /oauth2/login",
-      });
-    });
+  // The routes below require the user to be authenticated
+  server.use(verifyToken);
 
-    // The routes below require the user to be authenticated
-    server.use(verifyToken);
+  server.get(["/logout"], async (req, res) => {
+    if (req.headers.authorization) {
+      res.redirect("/oauth2/logout");
+    }
+  });
 
-    server.get(["/logout"], async (req, res) => {
-      if (req.headers.authorization) {
-        res.redirect("/oauth2/logout");
-      }
-    });
+  // return user info fetched from the Microsoft Graph API
+  server.get("/me", async (req, res, next) => {
+    try {
+      const userInfo = await getUserInfoFromGraphApi(
+        req.headers.authorization ?? "Fant ikke authorization header",
+      );
+      return res.json(userInfo);
+    } catch (error) {
+      return next(error);
+    }
+  });
 
-    // return user info fetched from the Microsoft Graph API
-    server.get("/me", async (req, res, next) => {
-      try {
-        const userInfo = await getUserInfoFromGraphApi(
-          req.headers.authorization ?? "Fant ikke authorization header",
-        );
-        return res.json(userInfo);
-      } catch (error) {
-        return next(error);
-      }
-    });
+  // return groups that the user is a member of from the Microsoft Graph API
+  server.get("/me/memberOf", async (req, res, next) => {
+    try {
+      const userInfo = await getUserGroups(
+        req.headers.authorization ?? "Fant ikke authorization header",
+      );
+      return res.json(userInfo);
+    } catch (error) {
+      return next(error);
+    }
+  });
 
-    // return groups that the user is a member of from the Microsoft Graph API
-    server.get("/me/memberOf", async (req, res, next) => {
-      try {
-        const userInfo = await getUserGroups(
-          req.headers.authorization ?? "Fant ikke authorization header",
-        );
-        return res.json(userInfo);
-      } catch (error) {
-        return next(error);
-      }
-    });
+  setupProxies(server);
 
-    setupProxies(server);
+  serveViteMode(server, { port: "9010" });
 
-    serveViteMode(server, { port: "9010" });
+  // serve static files
+  server.use(express.static("./"));
+  server.use("*", (request, response) => {
+    response.sendFile("index.html");
+  });
 
-    // serve static files
-    server.use(express.static("./"));
-    server.use("*", (request, response) => {
-      response.sendFile("index.html");
-    });
+  // TODO: her var det en error handler. Jeg tror ikke vi trenger da det fanges av default handler: https://expressjs.com/en/guide/error-handling.html
 
-    // TODO: her var det en error handler. Jeg tror ikke vi trenger da det fanges av default handler: https://expressjs.com/en/guide/error-handling.html
-
-    server.listen(port, () => logger.info(`Listening on port ${port}`));
-  } catch (error) {
-    logger.error("Error during start-up: ", error);
-  }
+  server.listen(port, () => logger.info(`Listening on port ${port}`));
 }
 
 try {
