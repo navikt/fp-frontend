67c081b6b3154fa786a23cc5345f59bd61115981
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-11-15 14:17:33 +0100

    FAGSYSTEM-350135: Utvider listen med endelige journalpost statuser. (#6042)

--- packages/journalforing/src/kodeverk/journalpostTilstand.ts
+++ packages/journalforing/src/kodeverk/journalpostTilstand.ts
@@ -2,10 +2,13 @@ enum JournalpostTilstand {
   MOTTATT = 'MOTTATT',
   JOURNALFOERT = 'JOURNALFOERT',
   FEILREGISTRERT = 'FEILREGISTRERT',
+  EKSPEDERT = 'EKSPEDERT',
+  FERDIGSTILT = 'FERDIGSTILT',
   UTGAAR = 'UTGAAR',
 }
 
-const ENDELIGE_TILSTANDER = [JournalpostTilstand.FEILREGISTRERT, JournalpostTilstand.JOURNALFOERT];
+const ENDELIGE_TILSTANDER = [JournalpostTilstand.FEILREGISTRERT, JournalpostTilstand.JOURNALFOERT,
+  JournalpostTilstand.EKSPEDERT, JournalpostTilstand.FERDIGSTILT];
 
 export const erEndeligJournalført = (tilstand: JournalpostTilstand | undefined): boolean =>
   !!tilstand && ENDELIGE_TILSTANDER.includes(tilstand);
