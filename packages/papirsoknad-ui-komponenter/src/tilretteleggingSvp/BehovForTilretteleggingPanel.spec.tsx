6a8be57db280d5345091fd44f31fab693bdb8065
Siri Mykland <siri.mykland@nav.no>
2024-11-21 16:57:09 +0100

    oppdater BehovForTilrettelegging panel i papirsoknad

--- packages/papirsoknad-ui-komponenter/src/tilretteleggingSvp/BehovForTilretteleggingPanel.spec.tsx
+++ packages/papirsoknad-ui-komponenter/src/tilretteleggingSvp/BehovForTilretteleggingPanel.spec.tsx
@@ -1,6 +1,8 @@
 import { composeStories } from '@storybook/react';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
+import { TilretteleggingType } from '@navikt/fp-kodeverk';
 
 import * as stories from './BehovForTilretteleggingPanel.stories';
 
@@ -33,7 +35,7 @@ describe('<BehovForTilretteleggingPanel>', () => {
     expect(lagre).not.toHaveBeenCalled();
   });
 
-  it('skal velge at det søkes om svangerskapspenger som selvstendig næringsdrivende', async () => {
+  it('skal velge delvis fylle ut skjema', async () => {
     const lagre = vi.fn();
 
     await Default.run({
@@ -45,29 +47,81 @@ describe('<BehovForTilretteleggingPanel>', () => {
     expect(await screen.findByText('Behov for tilrettelegging')).toBeInTheDocument();
 
     await userEvent.click(screen.getAllByText('Ja')[0]);
-    await userEvent.click(screen.getAllByText('Nei')[1]);
-    await userEvent.click(screen.getAllByText('Nei')[2]);
-
-    expect(
-      await screen.findByText('Jordmor/lege oppgir at tilrettelegging er nødvendig fra og med'),
-    ).toBeInTheDocument();
+    await userEvent.click(screen.getAllByText('Ja')[1]);
+    await userEvent.click(screen.getAllByText('Ja')[2]);
 
     await userEvent.click(screen.getByText('Lagreknapp (Kun for test)'));
 
-    expect(await screen.findAllByText('Feltet må fylles ut')).toHaveLength(4);
+    expect(await screen.findAllByText('Feltet må fylles ut')).toHaveLength(13);
 
-    const tilretteleggingFom = screen.getByLabelText('Jordmor/lege oppgir at tilrettelegging er nødvendig fra og med');
-    await userEvent.type(tilretteleggingFom, '01.05.2022');
-    fireEvent.blur(tilretteleggingFom);
+    expect(lagre).toHaveBeenCalledTimes(0);
+  });
 
-    await userEvent.selectOptions(screen.getByLabelText('Behov for tilrettelegging'), 'HEL_TILRETTELEGGING');
+  it('skal velge at det søkes om tilrettelegging for alle arbeidstyper', async () => {
+    const lagre = vi.fn();
 
-    const fraDato = screen.getByLabelText('Fra dato');
-    await userEvent.type(fraDato, '30.05.2022');
-    fireEvent.blur(fraDato);
+    await Default.run({
+      parameters: {
+        submitCallback: lagre,
+      },
+    });
 
-    const stillingsprosent = screen.getByLabelText('Stillingsprosent');
-    await userEvent.type(stillingsprosent, '100');
+    expect(await screen.findByText('Behov for tilrettelegging')).toBeInTheDocument();
+
+    await userEvent.click(screen.getAllByText('Ja')[0]);
+    await userEvent.click(screen.getAllByText('Ja')[1]);
+    await userEvent.click(screen.getAllByText('Ja')[2]);
+
+    await userEvent.click(screen.getByText('Lagreknapp (Kun for test)'));
+
+    expect(await screen.findAllByText('Feltet må fylles ut')).toHaveLength(13);
+
+    // Selvstendig næringsdrivende
+    const næringsdrivende = within(
+      screen.getByText('Søkes det om svangerskapspenger som selvstendig næringsdrivende?').closest('fieldset')!,
+    );
+
+    await userEvent.type(
+      næringsdrivende.getByLabelText('Jordmor/lege oppgir at tilrettelegging er nødvendig fra og med'),
+      '11.01.2024',
+    );
+    await userEvent.selectOptions(
+      næringsdrivende.getByLabelText('Behov for tilrettelegging'),
+      TilretteleggingType.HEL_TILRETTELEGGING,
+    );
+    await userEvent.type(næringsdrivende.getByLabelText('Fra dato'), '01.01.2024');
+    await userEvent.type(næringsdrivende.getByLabelText('Stillingsprosent'), '80');
+
+    // Frilans
+    const frilans = within(screen.getByText('Søkes det om svangerskapspenger som frilanser?').closest('fieldset')!);
+
+    await userEvent.type(
+      frilans.getByLabelText('Jordmor/lege oppgir at tilrettelegging er nødvendig fra og med'),
+      '12.02.2024',
+    );
+    await userEvent.selectOptions(
+      frilans.getByLabelText('Behov for tilrettelegging'),
+      TilretteleggingType.DELVIS_TILRETTELEGGING,
+    );
+    await userEvent.type(frilans.getByLabelText('Fra dato'), '02.02.2024');
+    await userEvent.type(frilans.getByLabelText('Stillingsprosent'), '90');
+
+    // Arbeidstaker
+    const arbeidstaker = within(
+      screen.getByText('Søkes det om svangerskapspenger som arbeidstaker?').closest('fieldset')!,
+    );
+
+    await userEvent.type(arbeidstaker.getByLabelText('Arbeidsgivers orgnr/fnr'), '000000000');
+    await userEvent.type(
+      arbeidstaker.getByLabelText('Jordmor/lege oppgir at tilrettelegging er nødvendig fra og med'),
+      '13.03.2024',
+    );
+    await userEvent.selectOptions(
+      arbeidstaker.getByLabelText('Behov for tilrettelegging'),
+      TilretteleggingType.INGEN_TILRETTELEGGING,
+    );
+    await userEvent.type(arbeidstaker.getByLabelText('Fra dato'), '03.03.2024');
+    await userEvent.type(arbeidstaker.getByLabelText('Stillingsprosent'), '100');
 
     await userEvent.click(screen.getByText('Lagreknapp (Kun for test)'));
 
@@ -75,13 +129,36 @@ describe('<BehovForTilretteleggingPanel>', () => {
     expect(lagre).toHaveBeenCalledWith({
       tilretteleggingArbeidsforhold: [
         {
-          '@type': 'SN',
-          behovsdato: '2022-05-01',
+          '@type': 'VI',
+          behovsdato: '2024-03-13',
+          organisasjonsnummer: '000000000',
           tilrettelegginger: [
             {
-              dato: '2022-05-30',
+              dato: '2024-03-03',
               stillingsprosent: '100',
-              tilretteleggingType: 'HEL_TILRETTELEGGING',
+              tilretteleggingType: TilretteleggingType.INGEN_TILRETTELEGGING,
+            },
+          ],
+        },
+        {
+          '@type': 'FR',
+          behovsdato: '2024-02-12',
+          tilrettelegginger: [
+            {
+              dato: '2024-02-02',
+              stillingsprosent: '90',
+              tilretteleggingType: TilretteleggingType.DELVIS_TILRETTELEGGING,
+            },
+          ],
+        },
+        {
+          '@type': 'SN',
+          behovsdato: '2024-01-11',
+          tilrettelegginger: [
+            {
+              dato: '2024-01-01',
+              stillingsprosent: '80',
+              tilretteleggingType: TilretteleggingType.HEL_TILRETTELEGGING,
             },
           ],
         },
---
+++ packages/papirsoknad-ui-komponenter/src/tilretteleggingSvp/components/TilretteleggingForArbeidsgiverFieldArray.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react';
+import { useFieldArray, useFormContext } from 'react-hook-form';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+import { XMarkIcon } from '@navikt/aksel-icons';
+import { Button, HStack, VStack } from '@navikt/ds-react';
+import { Datepicker, InputField } from '@navikt/ft-form-hooks';
+import { hasNoWhiteSpace, hasValidOrgNumberOrFodselsnr, required } from '@navikt/ft-form-validators';
+import { AvsnittSkiller } from '@navikt/ft-ui-komponenter';
+
+import {
+  BEHOV_FOR_TILRETTELEGGING_FIELD_ARRAY_NAME,
+  TILRETTELEGGING_FOR_ARBEIDSGIVER_FIELD_ARRAY_NAME,
+  TILRETTELEGGING_NAME_PREFIX,
+} from '../constants';
+import { FormValues } from '../types';
+import { BehovForTilretteleggingFieldArray } from './BehovForTilretteleggingFieldArray';
+
+const defaultArbeidsgiver = {
+  organisasjonsnummer: '',
+  behovsdato: '',
+};
+
+interface Props {
+  readOnly: boolean;
+}
+
+const FA_PREFIX = `${TILRETTELEGGING_NAME_PREFIX}.${TILRETTELEGGING_FOR_ARBEIDSGIVER_FIELD_ARRAY_NAME}`;
+const getPrefix = (index: number) => `${FA_PREFIX}.${index}`;
+
+/*
+ * TilretteleggingForArbeidsgiverFieldArray
+ *
+ * Form som brukes for registrere om det er behov for tilrettelegging for arbeidsgiver.
+ */
+export const TilretteleggingForArbeidsgiverFieldArray = ({ readOnly }: Props) => {
+  const intl = useIntl();
+
+  const { control } = useFormContext<FormValues>();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: FA_PREFIX,
+  });
+
+  const leggTilArbeidsgiver = () => {
+    append(defaultArbeidsgiver);
+  };
+
+  useEffect(() => {
+    if (fields.length === 0) {
+      leggTilArbeidsgiver();
+    }
+  }, []);
+
+  return (
+    <>
+      {fields.map((field, index: number) => (
+        <div key={field.id}>
+          {index > 0 && <AvsnittSkiller spaceUnder spaceAbove />}
+          <HStack wrap={false} justify="space-between">
+            <VStack>
+              <HStack gap="4">
+                <InputField
+                  readOnly={readOnly}
+                  name={`${getPrefix(index)}.organisasjonsnummer`}
+                  label={intl.formatMessage({ id: 'TilretteleggingForArbeidsgiverFieldArray.OrgNr' })}
+                  validate={[required, (value: any) => hasNoWhiteSpace(value.toString()), hasValidOrgNumberOrFodselsnr]}
+                  maxLength={99}
+                />
+
+                <Datepicker
+                  name={`${getPrefix(index)}.behovsdato`}
+                  label={intl.formatMessage({ id: 'TilretteleggingForArbeidsgiverFieldArray.TilretteleggingFra' })}
+                  validate={[required]}
+                  isReadOnly={readOnly}
+                />
+              </HStack>
+
+              <BehovForTilretteleggingFieldArray
+                name={`${getPrefix(index)}.${BEHOV_FOR_TILRETTELEGGING_FIELD_ARRAY_NAME}`}
+                readOnly={readOnly}
+              />
+            </VStack>
+            {!readOnly && index > 0 && (
+              <div>
+                <Button type="button" variant="tertiary-neutral" icon={<XMarkIcon />} onClick={() => remove(index)} />
+              </div>
+            )}
+          </HStack>
+        </div>
+      ))}
+
+      <Button size="small" variant="secondary" onClick={leggTilArbeidsgiver} type="button">
+        <FormattedMessage id="TilretteleggingForArbeidsgiverFieldArray.LeggTilArbeidsgiver" />
+      </Button>
+    </>
+  );
+};
