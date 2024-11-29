659b915ea0ebe1571178093bf5f5668e784183e0
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2024-11-29 14:37:37 +0100

    oppdater AndreYtelserPanel i papirsoknad (#6067)

---
+++ packages/papirsoknad-ui-komponenter/src/andreYtelserPanel/constants.ts
@@ -0,0 +1,3 @@
+export const ANDRE_YTELSER_NAME_PREFIX = 'andreYtelser';
+export const ANDRE_YTELSER_PERIODER_NAME = 'andreYtelserPerioder';
+export const ANDRE_YTELSER_TYPER_NAME = 'andreYtelserTyper';
