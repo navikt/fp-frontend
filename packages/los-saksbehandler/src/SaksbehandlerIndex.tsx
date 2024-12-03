cbc779b87f41fc7e1133c2c6cfe34afc696dd316
tor-nav <tor.roneid@nav.no>
2024-12-03 12:21:51 +0100

    FIX

--- packages/los-saksbehandler/src/SaksbehandlerIndex.tsx
+++ packages/los-saksbehandler/src/SaksbehandlerIndex.tsx
@@ -4,7 +4,9 @@ import { RawIntlProvider } from 'react-intl';
 import { createIntl } from '@navikt/ft-utils';
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { HTTPError } from 'ky';
 
+import { EventType } from '@navikt/fp-rest-api';
 import { useRestApiErrorDispatcher } from '@navikt/fp-rest-api-hooks';
 
 import { SaksbehandlerDashboard } from './SaksbehandlerDashboard';
@@ -21,9 +23,25 @@ const createQueryClient = (addErrorMessage: (data: any) => void) =>
       },
     },
     queryCache: new QueryCache({
-      onError: e => {
-        // FIXME (TOR) Her må ein formatera tekst på same måte som før
-        addErrorMessage(e.message);
+      onError: error => {
+        // TODO Dette er ein forenkela kopi av dagens feilhåndtering. Refaktorer og flytt når Tanstack Query blir brukt over alt
+        if (error instanceof HTTPError) {
+          if (error.response.status === 403) {
+            addErrorMessage({ type: EventType.REQUEST_FORBIDDEN, feilmelding: error.message });
+          } else if (error.response.status === 401) {
+            addErrorMessage({ type: EventType.REQUEST_UNAUTHORIZED, feilmelding: error.message });
+          } else if (error.response.status === 504 || error.response.status === 404) {
+            addErrorMessage({
+              type: EventType.REQUEST_GATEWAY_TIMEOUT_OR_NOT_FOUND,
+              //@ts-expect-error
+              location: error.response?.config?.url,
+            });
+          } else {
+            addErrorMessage({ type: EventType.REQUEST_ERROR, feilmelding: error.message });
+          }
+        } else {
+          addErrorMessage({ type: EventType.REQUEST_ERROR, feilmelding: error.message });
+        }
       },
     }),
   });
