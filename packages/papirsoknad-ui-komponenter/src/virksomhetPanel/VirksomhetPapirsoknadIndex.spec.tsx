e8c43a874acd6ccd5e6c7fde53a2849476e7ff8e
tor-nav <tor.roneid@nav.no>
2024-12-09 09:18:29 +0100

    FIX

--- packages/papirsoknad-ui-komponenter/src/virksomhetPanel/VirksomhetPapirsoknadIndex.spec.tsx
+++ packages/papirsoknad-ui-komponenter/src/virksomhetPanel/VirksomhetPapirsoknadIndex.spec.tsx
@@ -37,7 +37,7 @@ describe('<VirksomhetPapirsoknadIndex>', () => {
     });
   });
 
-  it('skal velge at søker har arbeidet i egen næringsvirksomhet', async () => {
+  it('skal velge at søker har arbeidet i egen næringsvirksomhet', { timeout: 30000 }, async () => {
     const lagre = vi.fn();
 
     await Default.run({
