517ca5b8773b2cfff11d94a15312fd4917c919b2
tor-nav <tor.roneid@nav.no>
2024-12-12 13:34:25 +0100

    FIX mergefeil

--- packages/los-avdelingsleder/src/data/useLosKodeverk.ts
+++ packages/los-avdelingsleder/src/data/useLosKodeverk.ts
@@ -7,17 +7,9 @@ import { losKodeverkOptions } from './fplosAvdelingslederApi';
 /**
  * Hook som henter et gitt kodeverk fra respons som allerede er hentet fra backend.
  */
-<<<<<<< Updated upstream
-function useLosKodeverk<T = KodeverkMedNavn>(kodeverkType: string): T[] {
-  const alleKodeverk = restApiHooks.useGlobalStateRestApiData(RestApiGlobalStatePathsKeys.KODEVERK_LOS);
-  // @ts-expect-error Fiks
-  return alleKodeverk?.[kodeverkType] ?? [];
-}
-=======
 export const useLosKodeverk = <T = KodeverkMedNavn>(kodeverkType: string): T[] => {
   const alleKodeverk = useQuery(losKodeverkOptions()).data;
->>>>>>> Stashed changes
 
   //@ts-expect-error Fiks denne
-  return alleKodeverk && alleKodeverk[kodeverkType] ? alleKodeverk[kodeverkType] : [];
+  return alleKodeverk?.[kodeverkType] ?? [];
 };
