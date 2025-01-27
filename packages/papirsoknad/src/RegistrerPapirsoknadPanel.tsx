8e7df3707da30050c297ddddb325b34bc046dbad
tor-nav <tor.roneid@nav.no>
2025-01-27 16:14:17 +0100

    Oppdatert deps

--- packages/papirsoknad/src/RegistrerPapirsoknadPanel.tsx
+++ packages/papirsoknad/src/RegistrerPapirsoknadPanel.tsx
@@ -2,10 +2,10 @@ import React, { useCallback, useState } from 'react';
 import { FormattedMessage, RawIntlProvider } from 'react-intl';
 
 import { Heading, VStack } from '@navikt/ds-react';
-import { FagsakYtelseType } from '@navikt/ft-kodeverk';
 import { AksjonspunktHelpTextHTML } from '@navikt/ft-ui-komponenter';
 import { createIntl } from '@navikt/ft-utils';
 
+import { FagsakYtelseType } from '@navikt/fp-kodeverk';
 import { SoknadData } from '@navikt/fp-papirsoknad-ui-komponenter';
 import { AlleKodeverk, Fagsak } from '@navikt/fp-types';
 
