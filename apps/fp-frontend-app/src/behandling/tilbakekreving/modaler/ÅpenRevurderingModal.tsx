fcd287163b6041ed72dc6ec6e9f7458a855c00b7
Siri Mykland <siri.mykland@nav.no>
2025-01-28 13:34:01 +0100

    legg til test for ÅpenRevurderingModal

--- apps/fp-frontend-app/src/behandling/tilbakekreving/modaler/ÅpenRevurderingModal.tsx
+++ apps/fp-frontend-app/src/behandling/tilbakekreving/modaler/ÅpenRevurderingModal.tsx
@@ -6,8 +6,8 @@ import { BodyShort, Button, Heading, HStack, Modal } from '@navikt/ds-react';
 
 export const ÅpenRevurderingModal = ({ harÅpenRevurdering }: { harÅpenRevurdering: boolean }) => {
   const intl = useIntl();
-
   const ref = useRef<HTMLDialogElement>(null);
+
   useEffect(() => {
     if (harÅpenRevurdering) {
       ref.current?.showModal();
@@ -20,7 +20,7 @@ export const ÅpenRevurderingModal = ({ harÅpenRevurdering }: { harÅpenRevurde
       width="small"
       aria-label={intl.formatMessage({ id: 'ÅpenRevurderingModal.ApenRevurderingHeader' })}
     >
-      <Modal.Header>
+      <Modal.Header closeButton={false}>
         <HStack gap="4" align="center">
           <ExclamationmarkTriangleFillIcon width={35} height={35} color="var(--a-orange-600)" />
           <Heading size="small">{intl.formatMessage({ id: 'ÅpenRevurderingModal.ApenRevurderingHeader' })}</Heading>
@@ -31,7 +31,7 @@ export const ÅpenRevurderingModal = ({ harÅpenRevurdering }: { harÅpenRevurde
       </Modal.Body>
       <Modal.Footer>
         <Button size="small" variant="primary" onClick={() => ref.current?.close()} autoFocus type="button">
-          OK
+          {intl.formatMessage({ id: 'ÅpenRevurderingModal.Ok' })}
         </Button>
       </Modal.Footer>
     </Modal>
