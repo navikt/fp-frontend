fec83cffa6abd03155ec15ca7ab856b1f33f61d3
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-01-28 13:42:24 +0100

    erstatt ft-ui-komponent > WarningModal med aksel sin Modal (#6158)
    * erstatt ft-ui-komponent > WarningModal med aksel sin Modal
    
    * legg til test for ÅpenRevurderingModal

---
+++ apps/fp-frontend-app/src/behandling/tilbakekreving/modaler/ÅpenRevurderingModal.spec.tsx
@@ -0,0 +1,30 @@
+import { composeStories } from '@storybook/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { beforeEach, expect } from 'vitest';
+
+import * as stories from './ÅpenRevurderingModal.stories';
+
+const { Default } = composeStories(stories);
+
+describe('ÅpenRevurderingModal', () => {
+  beforeEach(() => {
+    HTMLDialogElement.prototype.showModal = vi.fn();
+    HTMLDialogElement.prototype.close = vi.fn();
+  });
+
+  it('skal vise og lukke modal for åpen revurdering', async () => {
+    await Default.run({ args: { harÅpenRevurdering: true } });
+    expect(HTMLDialogElement.prototype.showModal).toHaveBeenCalledTimes(1);
+
+    expect(screen.getByText('Åpen revurdering')).toBeInTheDocument();
+    expect(screen.getByText(/Det finnes en åpen revurdering som kan påvirke/)).toBeInTheDocument();
+    await userEvent.click(screen.getByText('OK'));
+    expect(HTMLDialogElement.prototype.close).toHaveBeenCalledTimes(1);
+  });
+
+  it('skal ikke vise modal når ingen åpen revurdering', async () => {
+    await Default.run({ args: { harÅpenRevurdering: false } });
+    expect(HTMLDialogElement.prototype.showModal).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ apps/fp-frontend-app/src/behandling/tilbakekreving/modaler/ÅpenRevurderingModal.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react';
+import { useIntl } from 'react-intl';
+
+import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
+import { BodyShort, Button, Heading, HStack, Modal } from '@navikt/ds-react';
+
+export const ÅpenRevurderingModal = ({ harÅpenRevurdering }: { harÅpenRevurdering: boolean }) => {
+  const intl = useIntl();
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (harÅpenRevurdering) {
+      ref.current?.showModal();
+    }
+  }, [harÅpenRevurdering]);
+
+  return (
+    <Modal
+      ref={ref}
+      width="small"
+      aria-label={intl.formatMessage({ id: 'ÅpenRevurderingModal.ApenRevurderingHeader' })}
+    >
+      <Modal.Header closeButton={false}>
+        <HStack gap="4" align="center">
+          <ExclamationmarkTriangleFillIcon width={35} height={35} color="var(--a-orange-600)" />
+          <Heading size="small">{intl.formatMessage({ id: 'ÅpenRevurderingModal.ApenRevurderingHeader' })}</Heading>
+        </HStack>
+      </Modal.Header>
+      <Modal.Body>
+        <BodyShort size="small">{intl.formatMessage({ id: 'ÅpenRevurderingModal.ApenRevurdering' })}</BodyShort>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button size="small" variant="primary" onClick={() => ref.current?.close()} autoFocus type="button">
+          {intl.formatMessage({ id: 'ÅpenRevurderingModal.Ok' })}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
