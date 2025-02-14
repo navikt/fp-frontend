9bd1730e633e55c04ab3296d1c7b0aca49c85fd0
thao1 <thao.than.nguyen@nav.no>
2025-02-14 12:22:54 +0100

    TFP-5954: Lagt til Tooltip for lukk/åpne sidemeny.

--- apps/fp-frontend-app/src/fagsak/components/SidePanelKnapp.tsx
+++ apps/fp-frontend-app/src/fagsak/components/SidePanelKnapp.tsx
@@ -1,5 +1,5 @@
 import { SidebarRightIcon } from '@navikt/aksel-icons';
-import { Button } from '@navikt/ds-react';
+import { Button, Tooltip } from '@navikt/ds-react';
 
 interface Props {
   toggleSideMeny: () => void;
@@ -8,13 +8,15 @@ interface Props {
 
 export const SidePanelKnapp = ({ toggleSideMeny, visSideMeny }: Props) => {
   return (
-    <Button
-      icon={<SidebarRightIcon fontSize="1.7rem" aria-hidden />}
-      aria-label={visSideMeny ? 'Skjul sidepanel' : 'Vis sidepanel'}
-      variant="tertiary-neutral"
-      size="small"
-      onClick={toggleSideMeny}
-      style={{ marginTop: '18px', backgroundColor: 'white' }}
-    />
+    <Tooltip content="Åpne sidemeny" placement="bottom">
+      <Button
+        icon={<SidebarRightIcon fontSize="1.7rem" aria-hidden />}
+        aria-label={visSideMeny ? 'Skjul sidepanel' : 'Vis sidepanel'}
+        variant="tertiary-neutral"
+        size="small"
+        onClick={toggleSideMeny}
+        style={{ marginTop: '18px', backgroundColor: 'white' }}
+      />
+    </Tooltip>
   );
 };
