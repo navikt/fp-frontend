0528c1ec6e4d60f99856da73e4b9b12518520ef5
Siri Mykland <siri.mykland@nav.no>
2025-02-17 10:46:47 +0100

    fiks ugyldig org nummer i test

--- packages/fakta/fakta-verge/src/VergeFaktaIndex.spec.tsx
+++ packages/fakta/fakta-verge/src/VergeFaktaIndex.spec.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { AksjonspunktKode } from '@navikt/fp-kodeverk';
@@ -23,7 +23,7 @@ describe('VergeFaktaIndex', () => {
     await userEvent.type(navnInput, 'Espen Utvikler');
 
     const organisasjonsnummerInput = utils.getByLabelText('Organisasjonsnummer');
-    await userEvent.type(organisasjonsnummerInput, '2322323233');
+    await userEvent.type(organisasjonsnummerInput, '232232323');
 
     const fomInput = utils.getByLabelText('Periode f.o.m.');
     await userEvent.type(fomInput, '14.09.2022');
@@ -38,7 +38,6 @@ describe('VergeFaktaIndex', () => {
 
     await userEvent.click(screen.getByText('Bekreft og fortsett'));
 
-    await waitFor(() => expect(lagre).toHaveBeenCalledTimes(1));
     expect(lagre).toHaveBeenNthCalledWith(1, {
       begrunnelse: 'Dette er en begrunnelse',
       fnr: undefined,
@@ -46,7 +45,7 @@ describe('VergeFaktaIndex', () => {
       gyldigTom: '2022-09-24',
       kode: AksjonspunktKode.AVKLAR_VERGE,
       navn: 'Espen Utvikler',
-      organisasjonsnummer: '2322323233',
+      organisasjonsnummer: '232232323',
       vergeType: 'ADVOKAT',
     });
   });
