16c35b42dea886b2c3d0b28a307864b91c76fcef
Tor Røneid <tor.roneid@nav.no>
2025-03-10 11:27:52 +0100

    Oppdatert sentry-dep (#6253)

--- apps/fp-frontend-app/src/main.tsx
+++ apps/fp-frontend-app/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { type Event, type EventHint, init, Integrations } from '@sentry/browser';
+import { breadcrumbsIntegration, type ErrorEvent, type EventHint, init } from '@sentry/browser';
 import dayjs from 'dayjs';
 
 import { AppIndexWrapper } from './app/AppIndex';
@@ -24,8 +24,8 @@ init({
   dsn: isDevelopment ? 'http://dev@localhost:9010/1' : 'https://d1b7de8cc42949569da03849b47d3ea1@sentry.gc.nav.no/17',
   release: import.meta.env['SENTRY_RELEASE'] || 'unknown',
   environment,
-  integrations: [new Integrations.Breadcrumbs({ console: false })],
-  beforeSend: (event: Event, hint: EventHint) => {
+  integrations: [breadcrumbsIntegration({ console: false })],
+  beforeSend: (event: ErrorEvent, hint: EventHint) => {
     const exception = hint.originalException;
     // @ts-expect-error
     if (exception.isAxiosError) {
