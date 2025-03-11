ff494bcf778c9203304b0052cd015bae624667d1
tor-nav <tor.roneid@nav.no>
2025-03-11 08:41:54 +0100

    TFP-6018 Gå til vanleg behandlingsvindu om ein vel ufullstendig papirsøknad

--- apps/fp-frontend-app/src/behandling/papirsoknad/BehandlingPapirsoknadIndex.tsx
+++ apps/fp-frontend-app/src/behandling/papirsoknad/BehandlingPapirsoknadIndex.tsx
@@ -1,37 +1,30 @@
 import { use, useState } from 'react';
-import { useNavigate } from 'react-router';
 
 import { AksjonspunktKode } from '@navikt/fp-kodeverk';
-import { ApiPollingStatus } from '@navikt/fp-konstanter';
 import { RegistrerPapirsoknadPanel, SoknadRegistrertModal } from '@navikt/fp-papirsoknad';
-import type { Aksjonspunkt, Behandling, Fagsak } from '@navikt/fp-types';
+import type { Aksjonspunkt } from '@navikt/fp-types';
 
-import { BehandlingPaVent } from '../felles/modaler/paVent/BehandlingPaVent';
 import { BehandlingDataContext } from '../felles/utils/behandlingDataContext';
 
 /**
  * BehandlingPapirsoknadIndex
  *
- * Har ansvar for å sette opp skjemaet for registrering av papirsøknad for engangsstønad eller foreldrepenger.
- * Komponenten tilpasser skjemaet til valgt søknadstype (engagnsstønad eller foreldrepenger), valgt søknadtema (fødsel, adopsjon eller omsorg)
+ * Har ansvar for å sette opp skjemaet for registrering av papirsøknad for engangsstønad eller foreldrepenger og svangerskapspenger.
+ * Komponenten tilpasser skjemaet til valgt søknadstype, valgt søknadtema (fødsel, adopsjon eller omsorg)
  * og valgt foreldretype (mor, far/medmor eller tredjepart).
  */
 const BehandlingPapirsoknadIndex = () => {
   const [erAksjonspunktLagret, setErAksjonspunktLagret] = useState(false);
 
-  const { alleKodeverk, behandling, rettigheter, fagsak, lagreAksjonspunkter } = use(BehandlingDataContext);
+  const { alleKodeverk, behandling, rettigheter, fagsak, setSkalOppdatereEtterBekreftelseAvAp } =
+    use(BehandlingDataContext);
 
-  const navigate = useNavigate();
-  const opneSokeside = () => {
-    navigate('/');
-  };
+  const isReadOnly = !rettigheter.writeAccess.isEnabled || behandling.behandlingPaaVent;
 
-  const readOnly = !rettigheter.writeAccess.isEnabled || behandling.behandlingPaaVent;
-
-  const lagre = lagLagreFunksjon(behandling, fagsak, lagreAksjonspunkter, setErAksjonspunktLagret);
+  const lagrePapirsøknad = useLagrePapirsøknad(setErAksjonspunktLagret, setSkalOppdatereEtterBekreftelseAvAp);
 
   const lagreUfullstendig = (fagsakYtelseType: string, familieHendelseType: string, foreldreType: string) =>
-    lagre({ ufullstendigSoeknad: true }, fagsakYtelseType, familieHendelseType, foreldreType);
+    lagrePapirsøknad({ ufullstendigSoeknad: true }, fagsakYtelseType, familieHendelseType, foreldreType);
 
   const erEndringssøknad = behandling.aksjonspunkt.some(
     ap => ap.definisjon === AksjonspunktKode.REGISTRER_PAPIR_ENDRINGSØKNAD_FORELDREPENGER,
@@ -39,21 +32,55 @@ const BehandlingPapirsoknadIndex = () => {
 
   return (
     <>
-      <BehandlingPaVent behandling={behandling} kodeverk={alleKodeverk} opneSokeside={opneSokeside} />
       <SoknadRegistrertModal isOpen={erAksjonspunktLagret} />
       <RegistrerPapirsoknadPanel
         fagsak={fagsak}
         kodeverk={alleKodeverk}
-        readOnly={readOnly}
+        readOnly={isReadOnly}
         lagreUfullstendig={lagreUfullstendig}
-        lagreFullstendig={lagre}
+        lagreFullstendig={lagrePapirsøknad}
         erEndringssøknad={erEndringssøknad}
       />
     </>
   );
 };
 
-const getAktivtPapirsoknadApKode = (aksjonspunkter: Aksjonspunkt[]): string =>
+const useLagrePapirsøknad = (
+  setErAksjonspunktLagret: (erLagret: boolean) => void,
+  setSkalOppdatereEtterBekreftelseAvAp: (skalOppdatere: boolean) => void,
+) => {
+  const { behandling, fagsak, lagreAksjonspunkter } = use(BehandlingDataContext);
+
+  return async (formValues: any, fagsakYtelseType: string, familieHendelseType: string, foreldreType: string) => {
+    const bekreftedeAksjonspunktDtoer = [
+      {
+        '@type': getAktivPapirsøknadApKode(behandling.aksjonspunkt),
+        tema: familieHendelseType,
+        soknadstype: fagsakYtelseType,
+        soker: foreldreType,
+        ...formValues,
+      },
+    ];
+
+    const oppdatertBehandling = await lagreAksjonspunkter({
+      saksnummer: fagsak.saksnummer,
+      behandlingUuid: behandling.uuid,
+      behandlingVersjon: behandling.versjon,
+      bekreftedeAksjonspunktDtoer,
+    });
+
+    if (formValues.ufullstendigSoeknad) {
+      window.scrollTo(0, 0);
+    } else {
+      setSkalOppdatereEtterBekreftelseAvAp(false);
+      setErAksjonspunktLagret(true);
+    }
+
+    return oppdatertBehandling;
+  };
+};
+
+const getAktivPapirsøknadApKode = (aksjonspunkter: Aksjonspunkt[]): string =>
   aksjonspunkter
     .map(ap => ap.definisjon)
     .filter(
@@ -63,37 +90,6 @@ const getAktivtPapirsoknadApKode = (aksjonspunkter: Aksjonspunkt[]): string =>
         kode === AksjonspunktKode.REGISTRER_PAPIR_ENDRINGSØKNAD_FORELDREPENGER ||
         kode === AksjonspunktKode.REGISTRER_PAPIRSOKNAD_SVANGERSKAPSPENGER,
     )[0];
-
-const lagLagreFunksjon =
-  (
-    behandling: Behandling,
-    fagsak: Fagsak,
-    lagreAksjonspunkt: (params?: any) => Promise<Behandling>,
-    setAksjonspunktLagret: (erApLagret: boolean) => void,
-  ) =>
-  (formValues: any, fagsakYtelseType: string, familieHendelseType: string, foreldreType: string) => {
-    const manuellRegistreringDtoList = [
-      {
-        '@type': getAktivtPapirsoknadApKode(behandling.aksjonspunkt),
-        tema: familieHendelseType,
-        soknadstype: fagsakYtelseType,
-        soker: foreldreType,
-        ...formValues,
-      },
-    ];
-
-    const params = {
-      saksnummer: fagsak.saksnummer,
-      behandlingUuid: behandling.uuid,
-      behandlingVersjon: behandling.versjon,
-      bekreftedeAksjonspunktDtoer: manuellRegistreringDtoList,
-    };
-    return lagreAksjonspunkt(params).then(returnertBehandling => {
-      if (returnertBehandling?.taskStatus?.status !== ApiPollingStatus.HALTED) {
-        setAksjonspunktLagret(true);
-      }
-    });
-  };
 
 // Default export grunna React.lazy
 // eslint-disable-next-line import/no-default-export
