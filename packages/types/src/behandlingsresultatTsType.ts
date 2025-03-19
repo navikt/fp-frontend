04502a14721e5faef1249ea4b115ac49dbff8cc7
tor-nav <tor.roneid@nav.no>
2025-03-19 09:21:45 +0100

    FIX

--- packages/types/src/behandlingsresultatTsType.ts
+++ packages/types/src/behandlingsresultatTsType.ts
@@ -6,6 +6,8 @@ export type Behandlingsresultat = Readonly<{
   rettenTil?: string;
   konsekvenserForYtelsen?: string[];
   vedtaksbrev?: string;
+  overskrift?: string;
+  fritekstbrev?: string;
   erRevurderingMedUendretUtfall?: boolean;
   skjæringstidspunkt?: {
     dato: string;
