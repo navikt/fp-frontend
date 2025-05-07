1317da230e33982e49e7a6a8a480242cc7e251bf
Siri Mykland <siri.mykland@nav.no>
2025-05-07 15:03:26 +0200

    endre oppbyggingen av Form visningen i fakta-fødsel

--- packages/fakta/fodsel/src/components/AvklartBarnFieldArray.tsx
+++ packages/fakta/fodsel/src/components/AvklartBarnFieldArray.tsx
@@ -2,7 +2,7 @@ import { useFieldArray, useFormContext } from 'react-hook-form';
 import { useIntl } from 'react-intl';
 
 import { TrashIcon } from '@navikt/aksel-icons';
-import { Box, Button, HStack, Label } from '@navikt/ds-react';
+import { BodyShort, Button, HStack, VStack } from '@navikt/ds-react';
 import { Datepicker, PeriodFieldArray } from '@navikt/ft-form-hooks';
 import { dateBeforeOrEqualToToday, hasValidDate, required } from '@navikt/ft-form-validators';
 
@@ -31,44 +31,48 @@ export const AvklartBarnFieldArray = ({ readOnly }: Props) => {
   const avklartBarn = watch(FIELD_ARRAY_NAME);
 
   return (
-    <PeriodFieldArray
-      fields={fields}
-      emptyPeriodTemplate={defaultAntallBarn}
-      readOnly={readOnly}
-      titleText={intl.formatMessage({ id: 'AvklartBarnFieldArray.FyllInnDokumenterteOpplysninger' })}
-      bodyText={intl.formatMessage({ id: 'AvklartBarnFieldArray.LeggTilBarn' })}
-      shouldShowAddButton={avklartBarn.length < 9}
-      remove={remove}
-      append={append}
-    >
-      {(field, index) => (
-        <HStack gap="2" align="end" key={field.id}>
-          <Box marginBlock="1">{fields.length > 1 && <Label size="small">{index + 1} </Label>}</Box>
-          <Datepicker
-            name={`${FIELD_ARRAY_NAME}.${index}.fodselsdato`}
-            label={index === 0 && intl.formatMessage({ id: 'AvklartBarnFieldArray.Title' })}
-            validate={[hasValidDate, required, dateBeforeOrEqualToToday]}
-            isReadOnly={readOnly}
-          />
-          <Datepicker
-            name={`${FIELD_ARRAY_NAME}.${index}.dodsdato`}
-            label={index === 0 && intl.formatMessage({ id: 'AvklartBarnFieldArray.Dodsdato' })}
-            validate={[hasValidDate, dateBeforeOrEqualToToday]}
-            isReadOnly={readOnly}
-          />
-          {!readOnly && avklartBarn.length > 1 && (
-            <div>
-              <Button
-                size="small"
-                type="button"
-                variant="tertiary-neutral"
-                icon={<TrashIcon />}
-                onClick={() => remove(index)}
-              />
-            </div>
-          )}
-        </HStack>
-      )}
-    </PeriodFieldArray>
+    <VStack paddingBlock="3">
+      <PeriodFieldArray
+        fields={fields}
+        emptyPeriodTemplate={defaultAntallBarn}
+        readOnly={readOnly}
+        titleText={intl.formatMessage({ id: 'AvklartBarnFieldArray.FyllInnDokumenterteOpplysninger' })}
+        bodyText={intl.formatMessage({ id: 'AvklartBarnFieldArray.LeggTilBarn' })}
+        shouldShowAddButton={avklartBarn.length < 9}
+        remove={remove}
+        append={append}
+      >
+        {(field, index) => (
+          <HStack gap="4" align="center" key={field.id} paddingBlock="2 0">
+            {fields.length > 1 && <BodyShort size="medium">Barn {index + 1}:</BodyShort>}
+            <Datepicker
+              size="medium"
+              name={`${FIELD_ARRAY_NAME}.${index}.fodselsdato`}
+              label={intl.formatMessage({ id: 'AvklartBarnFieldArray.Title' })}
+              validate={[hasValidDate, required, dateBeforeOrEqualToToday]}
+              isReadOnly={readOnly}
+            />
+            <Datepicker
+              size="medium"
+              name={`${FIELD_ARRAY_NAME}.${index}.dodsdato`}
+              label={intl.formatMessage({ id: 'AvklartBarnFieldArray.Dodsdato' })}
+              validate={[hasValidDate, dateBeforeOrEqualToToday]}
+              isReadOnly={readOnly}
+            />
+            {!readOnly && avklartBarn.length > 1 && (
+              <div>
+                <Button
+                  size="small"
+                  type="button"
+                  variant="tertiary-neutral"
+                  icon={<TrashIcon />}
+                  onClick={() => remove(index)}
+                />
+              </div>
+            )}
+          </HStack>
+        )}
+      </PeriodFieldArray>
+    </VStack>
   );
 };
