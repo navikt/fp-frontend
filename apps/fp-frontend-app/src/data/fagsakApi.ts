fc2e4bda7520534c4603ef0f97f425845c75ecb4
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-05-20 12:33:51 +0200

    fjern msw warning om searchparam i requestHandler path (#6398)

--- apps/fp-frontend-app/src/data/fagsakApi.ts
+++ apps/fp-frontend-app/src/data/fagsakApi.ts
@@ -72,7 +72,7 @@ const kyExtended = ky.extend({
 
 //MÅ være en gyldig URL for at KY skal fungere i test
 const isTest = import.meta.env.MODE === 'test';
-export const wrapUrl = (url: string): string => (isTest ? `http://www.test.com${url}` : url);
+export const wrapUrl = (url: string): string => (isTest ? `https://www.test.com${url}` : url);
 
 const getUrlFromRel = (rel: keyof typeof FagsakRel, links: ApiLink[] = []): string => {
   const link = links.find(l => l.rel === FagsakRel[rel]);
