b247a0f0ca16837edca620e2f477b90d9fa2b883
Tor Røneid <tor.roneid@nav.no>
2025-06-24 12:39:19 +0200

    FIX Fagsak skal hentast på nytt etter merking av hastesak (Ikkje beha… (#6457)
    * FIX Fagsak skal hentast på nytt etter merking av hastesak

--- apps/fp-frontend-app/src/behandlingmenu/modaler/MerkSomHasterMenyModal.tsx
+++ apps/fp-frontend-app/src/behandlingmenu/modaler/MerkSomHasterMenyModal.tsx
@@ -1,9 +1,10 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { MenyMerkSomHasterIndex } from '@navikt/fp-sak-meny-merk-som-haster';
 import type { Behandling } from '@navikt/fp-types';
 
 import { useBehandlingApi } from '../../data/behandlingApi';
+import { FagsakRel } from '../../data/fagsakApi';
 
 interface Props {
   behandling: Behandling;
@@ -13,10 +14,20 @@ interface Props {
 
 export const MerkSomHasterMenyModal = ({ behandling, hentOgSettBehandling, lukkModal }: Props) => {
   const api = useBehandlingApi(behandling);
+  const queryClient = useQueryClient();
 
   const { mutate: merkSomHaster } = useMutation({
     mutationFn: () => api.merkSomHaster(behandling.uuid, behandling.versjon),
-    onSuccess: () => hentOgSettBehandling(),
+    onSuccess: () => {
+      hentOgSettBehandling();
+
+      queryClient.invalidateQueries({
+        queryKey: [FagsakRel.FETCH_FAGSAK],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [FagsakRel.FETCH_FAGSAKDATA_FPTILBAKE],
+      });
+    },
   });
 
   return <MenyMerkSomHasterIndex merkSomHaster={merkSomHaster} lukkModal={lukkModal} />;
