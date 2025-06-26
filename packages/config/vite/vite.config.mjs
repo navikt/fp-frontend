895910cfd9044129a72d6170bcb9b6c1f352e0cd
tor-nav <tor.roneid@nav.no>
2025-06-26 13:53:59 +0200

    Pakke for felles vite-config

---
+++ packages/config/vite/vite.config.mjs
@@ -0,0 +1,43 @@
+import react from '@vitejs/plugin-react';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import { defineConfig, mergeConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const createSharedAppConfig = setupFileDirName =>
+  mergeConfig(createConfig(setupFileDirName), {
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        plugins: [sourcemaps({ exclude: /@sentry/ })],
+      },
+    },
+  });
+
+export const createSharedPackagesConfig = setupFileDirName => createConfig(setupFileDirName);
+
+export const createConfig = setupFileDirName =>
+  defineConfig({
+    plugins: [
+      react({
+        include: '**/*.{ts,tsx}',
+      }),
+    ],
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+      },
+    },
+    test: {
+      deps: { interopDefault: true },
+      environment: 'jsdom',
+      css: false,
+      globals: true,
+      setupFiles: setupFileDirName || path.resolve(__dirname, './vitest-setup'),
+      watch: false,
+      testTimeout: 20000,
+    },
+  });
