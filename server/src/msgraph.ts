0d45d2dd2c9a73e7be580cd2d92bf78d962180aa
johannbm <johannes.moskvil@gmail.com>
2025-07-28 13:52:04 +0200

    fjern axios

--- server/src/msgraph.ts
+++ server/src/msgraph.ts
@@ -1,5 +1,4 @@
 import { requestAzureOboToken } from "@navikt/oasis";
-import axios from "axios";
 
 import config from "./config.js";
 import logger from "./logger.js";
@@ -14,11 +13,19 @@ const getGraphRequest = async (bearerToken: string, graphUrl: string) => {
     throw new Error(errorMessage);
   }
 
-  const response = await axios.get(graphUrl, {
-    headers: { Authorization: `Bearer ${obo.token}` },
+  const response = await fetch(graphUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${obo.token}`,
+    },
   });
 
-  return response.data;
+  if (!response.ok) {
+    const text = await response.text();
+    throw new Error(`Kall mot MS-graph feilet: ${response.status}: ${text}`);
+  }
+
+  return await response.json();
 };
 
 export const getUserInfoFromGraphApi = async (bearerToken: string) => {
