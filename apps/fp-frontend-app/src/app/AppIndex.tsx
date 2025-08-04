326f583f4e7bba1a5cf99c710a8b3bb4ea640568
tor-nav <tor.roneid@nav.no>
2025-08-04 14:57:28 +0200

    FIX

--- apps/fp-frontend-app/src/app/AppIndex.tsx
+++ apps/fp-frontend-app/src/app/AppIndex.tsx
@@ -23,8 +23,7 @@ import '../globalCss/global.module.css';
 
 import messages from '../../i18n/nb_NO.json';
 
-import '@navikt/ds-css';
-import '@navikt/ds-css/darkside';
+import '../globalCss/global.css';
 import '@navikt/ds-css-internal';
 import '@navikt/ft-form-hooks/dist/style.css';
 import '@navikt/ft-plattform-komponenter/dist/style.css';
