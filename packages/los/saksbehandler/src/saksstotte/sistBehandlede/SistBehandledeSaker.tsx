37ba3d8915b5da5ebaeea2e8a4fff7bdd45279b3
Steffen Pøhner <steffen.h.pohner@nav.no>
2025-08-27 10:38:57 +0200

    TFP-6364: legger til kun åpne behandlinger-toggle for siste-reserverte (#6569)

--- packages/los/saksbehandler/src/saksstotte/sistBehandlede/SistBehandledeSaker.tsx
+++ packages/los/saksbehandler/src/saksstotte/sistBehandlede/SistBehandledeSaker.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import {
@@ -10,7 +11,8 @@ import {
   PencilIcon,
   PersonEnvelopeIcon,
 } from '@navikt/aksel-icons';
-import { BodyShort, Heading, HStack, Table, VStack } from '@navikt/ds-react';
+import { BodyShort, Heading, HStack, Spacer, Switch, Table, VStack } from '@navikt/ds-react';
+import { LoadingPanel } from '@navikt/ft-ui-komponenter';
 import { useQuery } from '@tanstack/react-query';
 
 import { OppgaveBehandlingStatus } from '@navikt/fp-kodeverk';
@@ -20,8 +22,6 @@ import { behandlendeOppgaverOptions } from '../../data/fplosSaksbehandlerApi';
 import { useLosKodeverk } from '../../data/useLosKodeverk';
 
 import styles from './sistBehandledeSaker.module.css';
-
-const EMPTY_ARRAY: Oppgave[] = [];
 
 interface Props {
   åpneFagsak: (saksnummer: string, behandlingUuid?: string) => void;
@@ -33,7 +33,8 @@ interface Props {
  * Denne komponenten viser de siste fagsakene en nav-ansatt har behandlet.
  */
 export const SistBehandledeSaker = ({ åpneFagsak }: Props) => {
-  const { data: sistBehandledeSaker = EMPTY_ARRAY } = useQuery(behandlendeOppgaverOptions());
+  const [ kunÅpne, setkunÅpne ] = useState<boolean>(false);
+  const { data: sisteReserverte = [], isFetching } = useQuery(behandlendeOppgaverOptions(kunÅpne));
 
   return (
     <VStack gap="space-8">
@@ -44,78 +45,95 @@ export const SistBehandledeSaker = ({ åpneFagsak }: Props) => {
         <Heading size="xsmall" level="2">
           <FormattedMessage id="SistBehandledeSaker.SistBehandledeSaker" />
         </Heading>
+        <Spacer />
+        <Switch position="right" size="small" checked={kunÅpne} onChange={e => setkunÅpne(e.target.checked)}>
+          <FormattedMessage id="SistBehandledeSaker.KunÅpne" />
+        </Switch>
       </HStack>
-      {sistBehandledeSaker.length === 0 && (
-        <div className={styles.ingenBehandlinger}>
-          <BodyShort size="medium">
-            <FormattedMessage id="SistBehandledeSaker.IngenBehandlinger" tagName="i" />
-          </BodyShort>
-        </div>
-      )}
-      {sistBehandledeSaker.length > 0 && (
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell scope="col">
-                <FormattedMessage id="SistBehandledeSaker.Soker" />
-              </Table.HeaderCell>
-              <Table.HeaderCell scope="col">
-                <FormattedMessage id="SistBehandledeSaker.Saksnr" />
-              </Table.HeaderCell>
-              <Table.HeaderCell scope="col">
-                <FormattedMessage id="SistBehandledeSaker.Status" />
-              </Table.HeaderCell>
-              <Table.HeaderCell scope="col">
-                <FormattedMessage id="SistBehandledeSaker.ErReservert" />
-              </Table.HeaderCell>
-              <Table.HeaderCell scope="col" />
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {sistBehandledeSaker.map(sbs => (
-              <Table.Row key={sbs.id} onClick={() => åpneFagsak(sbs.saksnummer, sbs.behandlingId)}>
-                <Table.DataCell scope="row">{sbs.navn}</Table.DataCell>
-                <Table.DataCell scope="row">{sbs.saksnummer}</Table.DataCell>
-                <Table.DataCell scope="row">
-                  <StatusIcon oppgave={sbs} />
-                </Table.DataCell>
-                <Table.DataCell scope="row">
-                  {sbs.reservasjonStatus.erReservert && (
-                    <CheckmarkIcon title={sbs.reservasjonStatus.reservertAvNavn} fontSize="1.5rem" />
-                  )}
-                </Table.DataCell>
-                <Table.DataCell scope="row">
-                  <ChevronRightCircleFillIcon aria-hidden className={styles.pointerIcon} />
-                </Table.DataCell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      )}
+      <SistBehandledeSakerContent isFetching={isFetching} sisteReserverte={sisteReserverte} åpneFagsak={åpneFagsak} />
     </VStack>
+  );
+};
+
+const SistBehandledeSakerContent = ({
+  isFetching,
+  sisteReserverte,
+  åpneFagsak,
+}: {
+  isFetching: boolean;
+  sisteReserverte: Oppgave[];
+  åpneFagsak: (saksnummer: string, behandlingUuid?: string) => void;
+}) => {
+  if (isFetching) return <LoadingPanel />;
+
+  if (!sisteReserverte.length) {
+    return (
+      <div className={styles.ingenBehandlinger}>
+        <BodyShort size="medium">
+          <FormattedMessage id="SistBehandledeSaker.IngenBehandlinger" tagName="i" />
+        </BodyShort>
+      </div>
+    );
+  }
+
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell scope="col">
+            <FormattedMessage id="SistBehandledeSaker.Soker" />
+          </Table.HeaderCell>
+          <Table.HeaderCell scope="col">
+            <FormattedMessage id="SistBehandledeSaker.Saksnr" />
+          </Table.HeaderCell>
+          <Table.HeaderCell scope="col">
+            <FormattedMessage id="SistBehandledeSaker.Status" />
+          </Table.HeaderCell>
+          <Table.HeaderCell scope="col">
+            <FormattedMessage id="SistBehandledeSaker.ErReservert" />
+          </Table.HeaderCell>
+          <Table.HeaderCell scope="col" />
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {sisteReserverte.map(sbs => (
+          <Table.Row key={sbs.id} onClick={() => åpneFagsak(sbs.saksnummer, sbs.behandlingId)}>
+            <Table.DataCell scope="row">{sbs.navn}</Table.DataCell>
+            <Table.DataCell scope="row">{sbs.saksnummer}</Table.DataCell>
+            <Table.DataCell scope="row">
+              <StatusIcon oppgave={sbs} />
+            </Table.DataCell>
+            <Table.DataCell scope="row">
+              {sbs.reservasjonStatus.erReservert && (
+                <CheckmarkIcon title={sbs.reservasjonStatus.reservertAvNavn} fontSize="1.5rem" />
+              )}
+            </Table.DataCell>
+            <Table.DataCell scope="row">
+              <ChevronRightCircleFillIcon aria-hidden className={styles.pointerIcon} />
+            </Table.DataCell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 const StatusIcon = ({ oppgave }: { oppgave: Oppgave }) => {
   const oppgaveBehandlingStatuser = useLosKodeverk('OppgaveBehandlingStatus');
+  const statusNavn = oppgaveBehandlingStatuser.find(obs => obs.kode === oppgave.oppgaveBehandlingStatus)?.navn ?? '-';
 
-  const statusNavn = oppgaveBehandlingStatuser.find(obs => obs.kode === oppgave.oppgaveBehandlingStatus)?.navn || '-';
-
-  if (oppgave.oppgaveBehandlingStatus === OppgaveBehandlingStatus.UNDER_ARBEID) {
-    return <PencilIcon title={statusNavn} fontSize="1.5rem" />;
+  switch (oppgave.oppgaveBehandlingStatus) {
+    case OppgaveBehandlingStatus.UNDER_ARBEID:
+      return <PencilIcon title={statusNavn} fontSize="1.5rem" />;
+    case OppgaveBehandlingStatus.FERDIG:
+      return <CheckmarkCircleIcon title={statusNavn} fontSize="1.5rem" color="var(--ax-success-500)" />;
+    case OppgaveBehandlingStatus.PÅ_VENT:
+      return <HourglassTopFilledIcon title={statusNavn} fontSize="1.5rem" color="var(--ax-success-500)" />;
+    case OppgaveBehandlingStatus.RETURNERT_FRA_BESLUTTER:
+      return <ArrowCirclepathIcon title={statusNavn} fontSize="1.5rem" color="var(--ax-warning-500)" />;
+    case OppgaveBehandlingStatus.TIL_BESLUTTER:
+      return <PersonEnvelopeIcon title={statusNavn} fontSize="1.5rem" />;
+    default:
+      throw new Error('Ukjent status i statusfeltet til "Dine siste reserverte behandlinger');
   }
-  if (oppgave.oppgaveBehandlingStatus === OppgaveBehandlingStatus.FERDIG) {
-    return <CheckmarkCircleIcon title={statusNavn} fontSize="1.5rem" color="var(--ax-success-500)" />;
-  }
-  if (oppgave.oppgaveBehandlingStatus === OppgaveBehandlingStatus.PÅ_VENT) {
-    return <HourglassTopFilledIcon title={statusNavn} fontSize="1.5rem" color="var(--ax-success-500)" />;
-  }
-  if (oppgave.oppgaveBehandlingStatus === OppgaveBehandlingStatus.RETURNERT_FRA_BESLUTTER) {
-    return <ArrowCirclepathIcon title={statusNavn} fontSize="1.5rem" color="var(--ax-warning-500)" />;
-  }
-  if (oppgave.oppgaveBehandlingStatus === OppgaveBehandlingStatus.TIL_BESLUTTER) {
-    return <PersonEnvelopeIcon title={statusNavn} fontSize="1.5rem" />;
-  }
-
-  throw new Error('Ukjent status i statusfeltet til "Dine siste reserverte behandlinger');
 };
