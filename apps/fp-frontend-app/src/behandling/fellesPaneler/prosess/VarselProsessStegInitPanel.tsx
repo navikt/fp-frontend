50d86af5b4804ed40749f1c652b6ed2654ef5763
Anders Palfi <anders.palfi@nav.no>
2025-08-27 14:29:26 +0200

    TFP-6071 rydder bort lite brukt visning av familiehendelser i varsel om revurdering. Mye kode for veldig få aksjonspunkt (#6578)

--- apps/fp-frontend-app/src/behandling/fellesPaneler/prosess/VarselProsessStegInitPanel.tsx
+++ apps/fp-frontend-app/src/behandling/fellesPaneler/prosess/VarselProsessStegInitPanel.tsx
@@ -2,9 +2,8 @@ import { use } from 'react';
 import { useIntl } from 'react-intl';
 import { useNavigate } from 'react-router-dom';
 
-import { LoadingPanel } from '@navikt/ft-ui-komponenter';
 import { forhandsvisDokument } from '@navikt/ft-utils';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { AksjonspunktKode } from '@navikt/fp-kodeverk';
 import { ProsessStegCode } from '@navikt/fp-konstanter';
@@ -14,7 +13,7 @@ import {
 } from '@navikt/fp-prosess-varsel-om-revurdering';
 import type { ProsessAksjonspunkt } from '@navikt/fp-types-avklar-aksjonspunkter';
 
-import { forhåndsvisMelding, useBehandlingApi } from '../../../data/behandlingApi';
+import { forhåndsvisMelding } from '../../../data/behandlingApi';
 import { ProsessDefaultInitPanel } from '../../felles/prosess/ProsessDefaultInitPanel';
 import { skalViseProsessPanel } from '../../felles/prosess/skalViseProsessPanel';
 import { useStandardProsessPanelProps } from '../../felles/prosess/useStandardProsessPanelProps';
@@ -55,15 +54,6 @@ export const VarselProsessStegInitPanel = () => {
 
   const skalPanelVisesIMeny = skalViseProsessPanel(standardPanelProps.aksjonspunkter);
 
-  const api = useBehandlingApi(behandling);
-
-  const { data: familiehendelse } = useQuery(api.familiehendelseOptions(behandling, skalPanelVisesIMeny));
-  const { data: søknad } = useQuery(api.søknadOptions(behandling));
-  const { data: familiehendelseOrigninalBehandling } = useQuery(
-    api.familiehendelseOrigninalBehandlingOptions(behandling),
-  );
-  const { data: søknadOriginalBehandling } = useQuery(api.søknadOriginalBehandlingOptions(behandling));
-
   const { mutate: forhåndsvis } = useMutation({
     mutationFn: (values: VarselOmRevurderingForhåndsvisData) =>
       forhåndsvisMelding({
@@ -80,17 +70,7 @@ export const VarselProsessStegInitPanel = () => {
       prosessPanelMenyTekst={intl.formatMessage({ id: 'Behandlingspunkt.CheckVarselRevurdering' })}
       skalPanelVisesIMeny={skalPanelVisesIMeny}
     >
-      {familiehendelse && søknad && familiehendelseOrigninalBehandling && søknadOriginalBehandling ? (
-        <VarselOmRevurderingProsessIndex
-          familiehendelse={familiehendelse}
-          soknad={søknad}
-          familiehendelseOriginalBehandling={familiehendelseOrigninalBehandling}
-          soknadOriginalBehandling={søknadOriginalBehandling}
-          previewCallback={forhåndsvis}
-        />
-      ) : (
-        <LoadingPanel />
-      )}
+      <VarselOmRevurderingProsessIndex previewCallback={forhåndsvis} />
     </ProsessDefaultInitPanel>
   );
 };
