23d6b1a9bc483c54211fbcb2e70efa8b99396da3
johannbm <johannes.moskvil@gmail.com>
2025-08-27 14:57:42 +0200

    wip

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from '@hey-api/openapi-ts';
 export default defineConfig({
   input: './swagger.json',
   output: 'temp-types',
-  plugins: ['@hey-api/client-fetch'],
+  plugins: [
+    '@hey-api/client-fetch',
+    {
+      name: '@hey-api/typescript',
+      definitions: {
+        case: 'preserve',
+      },
+    },
+  ],
 });
