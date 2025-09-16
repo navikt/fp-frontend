b61a744cd86ecf7bcda6b42608014940901ac341
Siri Mykland <siri.mykland@nav.no>
2025-09-16 12:24:14 +0200

    lint

--- packages/fakta/omsorg-og-rett/src/OmsorgOgRettFaktaIndex.spec.tsx
+++ packages/fakta/omsorg-og-rett/src/OmsorgOgRettFaktaIndex.spec.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/react';
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { expect } from 'vitest';
 
