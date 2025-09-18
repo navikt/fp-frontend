c8b37a5c003d8f3d0fce069447ce17be8db01126
johannbm <johannes.moskvil@gmail.com>
2025-09-18 12:58:10 +0200

    fiks

--- apps/fp-frontend/src/app/components/Home.spec.tsx
+++ apps/fp-frontend/src/app/components/Home.spec.tsx
@@ -4,7 +4,7 @@ import { applyRequestHandlers, type MswParameters } from 'msw-storybook-addon';
 
 import * as stories from './Home.stories';
 
-const { VisAktør, VisSøk,VisLosIkkeTilgjengelig, VisSideIkkeFunnet } = composeStories(stories);
+const { VisAktør, VisSøk, VisSideIkkeFunnet } = composeStories(stories);
 
 describe('Home', () => {
   it('skal rendre aktør-panel', async () => {
@@ -17,12 +17,6 @@ describe('Home', () => {
     applyRequestHandlers(VisSøk.parameters['msw'] as MswParameters['msw']);
     render(<VisSøk />);
     expect(await screen.findByText('Søk på sak eller person')).toBeInTheDocument();
-  });
-
-  it('skal rendre los-ikke-tilgjengelig-panel', async () => {
-    applyRequestHandlers(VisLosIkkeTilgjengelig.parameters['msw'] as MswParameters['msw']);
-    render(<VisLosIkkeTilgjengelig />);
-    expect(await screen.findByText('FPLOS er ikke tilgjengelig')).toBeInTheDocument();
   });
 
   it('skal rendre side-ikke-funnet-panel', async () => {
