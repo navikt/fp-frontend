f68769b9680a0b67b54f656b4030f4e5357b4c4f
johannbm <johannes.moskvil@gmail.com>
2025-09-23 13:31:53 +0200

    fiks sjekk mot undefined string

--- packages/fakta/uttak/src/components/UttakFaktaDetailForm.tsx
+++ packages/fakta/uttak/src/components/UttakFaktaDetailForm.tsx
@@ -84,7 +84,7 @@ const transformValues = (values: FormValues): KontrollerFaktaPeriodeMedApMarkeri
   arbeidsforhold: values.arbeidsgiverId
     ? {
         arbeidsgiverReferanse:
-          values.arbeidsgiverId.split('-')[0] === 'null' ? undefined : values.arbeidsgiverId.split('-')[0],
+          values.arbeidsgiverId.split('-')[0] === 'undefined' ? undefined : values.arbeidsgiverId.split('-')[0],
         arbeidType: values.arbeidsgiverId.split('-')[1] as UttakArbeidType,
       }
     : undefined,
