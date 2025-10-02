75ad685cd34861f6508fb6fd8621eb09487f25af
tor røneid <tor.roneid@nav.no>
2025-10-02 13:51:47 +0200

    FIX

--- packages/types/src/sivilstandType.ts
+++ packages/types/src/sivilstandType.ts
@@ -1,18 +1,3 @@
-// export enum SivilstandType {
-//   SKILT = 'SKIL',
-//   ENKE = 'ENKE',
-//   GIFT = 'GIFT',
-//   GJENNLEVENDEPARTNER = 'GJPA',
-//   GIFTLEVERADSKILT = 'GLAD',
-//   UOPPGITT = 'NULL',
-//   REGISTRERTPARTNER = 'REPA',
-//   SAMBOER = 'SAMB',
-//   SEPARERTPARTNER = 'SEPA',
-//   SEPARERT = 'SEPR',
-//   SKILTPARTNER = 'SKPA',
-//   UGIFT = 'UGIF',
-// }
-
 import type { foreldrepenger_behandlingslager_behandling_personopplysning_SivilstandType } from '@navikt/fp-types';
 
 export type SivilstandType = foreldrepenger_behandlingslager_behandling_personopplysning_SivilstandType;
