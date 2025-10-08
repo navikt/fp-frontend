002f910f62fdf50649ac1a87a16fe2a6d87ac38e
tor røneid <tor.roneid@nav.no>
2025-10-08 13:56:21 +0200

    FIX

--- packages/los/felles/src/endreReservasjon/OppgaveReservasjonEndringDatoModal.spec.tsx
+++ packages/los/felles/src/endreReservasjon/OppgaveReservasjonEndringDatoModal.spec.tsx
@@ -17,7 +17,6 @@ describe('OppgaveReservasjonEndringDatoModal', () => {
     expect(await screen.findByText('Velg dato som reservasjonen avsluttes')).toBeInTheDocument();
 
     const datoInput = screen.getByRole('textbox', { hidden: true });
-
     await userEvent.type(datoInput, dayjs().format(DDMMYYYY_DATE_FORMAT));
 
     expect(await screen.findByText('OK')).toBeInTheDocument();
