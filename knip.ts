3503a8c514456d73ccdfdf225f077919a1079e42
Siri Mykland <siri.mykland@nav.no>
2025-10-15 16:34:54 +0200

    fjern knip feil

--- knip.ts
+++ knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  ignore: ['.storybook/**', 'server/**', 'scripts/sentry-release.cjs', 'openapi-ts.config.ts'],
+  ignore: ['.storybook/**', 'server/**', 'scripts/sentry-release.cjs', 'openapi-ts.config.ts','packages/types/src/apiDtoGenerert.ts'],
   ignoreBinaries: ['docker-compose'],
   ignoreDependencies: ['glob', '@sentry/cli', '@hey-api/openapi-ts'],
 };
