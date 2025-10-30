792c5c431b21a0f13fb54f01ba3ed5b5ab469b7f
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2025-10-30 16:07:54 +0100

    TFP-6401: ny visning for legacy APer i fakta-omsorgsovertakelse (#6823)
    * ny visning for legacy APer i fakta-omsorgsovertakelse
    
    * fiks intl test for stringunion id i formatMessage
    
    * lint
    
    * fiks sonarfeil

--- packages/fakta/omsorgsovertakelse/i18n/intl.spec.ts
+++ packages/fakta/omsorgsovertakelse/i18n/intl.spec.ts
@@ -1,10 +1,24 @@
 import { extract } from '@formatjs/cli-lib/lib_esnext';
 import glob from 'fast-glob';
+import { expect } from 'vitest';
 
 import nb from './nb_NO.json';
 
 // eslint-disable-next-line no-console
 const writeToConsole = (text: string) => console.log(text);
+
+const NOT_FOUND_BECAUSE_ID_IS_STRING_UNION_IN_CODE = new Set([
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5008',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5054',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5006',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5004',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5005',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5013',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5014',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.5011',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.6004',
+  'LegacyAksjonspunktVisning.Aksjonspunkt.6010',
+]);
 
 describe('intl', () => {
   it('Check that i18n strings in code and in language file match', async () => {
@@ -13,25 +27,33 @@ describe('intl', () => {
     const foundTranslations = await extract(files, {
       idInterpolationPattern: '[sha512:contenthash:base64:6]',
     });
-    const stringsInCode = Object.keys(JSON.parse(foundTranslations) as Record<string, string>);
 
-    const missingKeysBokmål = stringsInCode.filter(key => !Object.keys(nb).includes(key));
-    if (missingKeysBokmål.length > 0) {
+    const stringsInCode = new Set(Object.keys(JSON.parse(foundTranslations) as Record<string, string>));
+    const stringsInFile = new Set(Object.keys(nb));
+
+    const missingKeysBokmål = stringsInCode.difference(stringsInFile);
+    if (missingKeysBokmål.size > 0) {
       writeToConsole('Not found in nb_NO.json:');
     }
     for (const key of missingKeysBokmål) {
       writeToConsole(`key ${key}`);
     }
 
-    const missingKeysCode = Object.keys(nb).filter(key => !stringsInCode.includes(key));
-    if (missingKeysCode.length > 0) {
+    const missingKeysCode = stringsInFile.difference(stringsInCode);
+
+    expect(missingKeysCode.size).toBe(NOT_FOUND_BECAUSE_ID_IS_STRING_UNION_IN_CODE.size);
+    for (const key of NOT_FOUND_BECAUSE_ID_IS_STRING_UNION_IN_CODE) {
+      missingKeysCode.delete(key);
+    }
+
+    if (missingKeysCode.size > 0) {
       writeToConsole('Not found in code:');
     }
     for (const key of missingKeysCode) {
       writeToConsole(`key ${key}`);
     }
 
-    expect(missingKeysBokmål.length).toBe(0);
-    expect(missingKeysCode.length).toBe(0);
+    expect(missingKeysBokmål.size).toBe(0);
+    expect(missingKeysCode.size).toBe(0);
   });
 });
