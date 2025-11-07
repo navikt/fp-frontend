a03824bb10dc6061dc1829db9bb1d8a96d32ef91
Siri Mykland <siri.mykland@nav.no>
2025-11-07 10:13:23 +0100

    fiks knip warnings

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   ignore: ['.storybook/**', 'server/**', 'openapi-ts.config.ts', 'packages/types/src/apiDtoGenerert.ts'],
   ignoreBinaries: ['docker-compose'],
-  ignoreDependencies: ['glob', '@hey-api/openapi-ts'],
+  ignoreDependencies: ['glob'],
 };
 
 export default config;
