c6e6b27f8216bf3e8afa966107abe7f3e7dab44f
Siri Mykland <siri.mykland@nav.no>
2025-11-20 10:15:17 +0100

    fiks vite env variable forwarding i turbo.json

--- apps/fp-journalforing/vite.config.ts
+++ apps/fp-journalforing/vite.config.ts
@@ -31,8 +31,8 @@ export default mergeConfig(createSharedAppConfig(), {
   plugins: [
     // Put the Sentry vite plugin after all other plugins
     sentryVitePlugin({
-      authToken: process.env.SENTRY_AUTH_TOKEN, // Kommer fra Github organization secrets
-      disable: !process.env.SENTRY_AUTH_TOKEN, // Ikke last opp source maps hvis token ikke er satt. Token er bare satt når det bygges fra master branch
+      authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Kommer fra Github organization secrets
+      disable: !process.env.VITE_SENTRY_AUTH_TOKEN, // Ikke last opp source maps hvis token ikke er satt. Token er bare satt når det bygges fra master branch
       org: 'nav',
       project: 'fp-journalforing',
       url: 'https://sentry.gc.nav.no',
