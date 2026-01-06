48088812c439fa3fc38e25b0292176015b17280b
johannbm <johannes.moskvil@gmail.com>
2026-01-06 13:33:59 +0100

    wip

--- packages/sak/dekorator/src/components/FeilmeldingsdetaljerModal.tsx
+++ packages/sak/dekorator/src/components/FeilmeldingsdetaljerModal.tsx
@@ -1,11 +1,9 @@
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
-import { BodyShort, Button, Detail, Heading, HStack, Modal, VStack } from '@navikt/ds-react';
+import { BodyShort, Button, Detail, Dialog, Heading, HStack, VStack } from '@navikt/ds-react';
 
 import type { Feilmelding } from '../typer/feilmeldingTsType';
-
-import styles from './feilmeldingsdetaljerModal.module.css';
 
 const capitalizeFirstLetters = (key: string): string => key.charAt(0).toUpperCase() + key.substring(1);
 
@@ -21,44 +19,42 @@ interface Props {
  * Modal som viser en feildetaljer.
  */
 export const FeilmeldingsdetaljerModal = ({ skalViseModal, lukkModal, feilmeldingsdetaljer }: Props) => {
-  const intl = useIntl();
   return (
-    <Modal
-      width="medium"
-      open={skalViseModal}
-      aria-label={intl.formatMessage({ id: 'FeilmeldingsdetaljerModal.ErrorDetails' })}
-      onClose={lukkModal}
-    >
-      <Modal.Header>
-        <HStack gap="space-16">
-          <ExclamationmarkTriangleFillIcon className={styles['image']} />
-          <Heading size="small" level="2">
-            <FormattedMessage id="FeilmeldingsdetaljerModal.ErrorDetails" />
-          </Heading>
-        </HStack>
-      </Modal.Header>
-      <Modal.Body>
-        <HStack gap="space-40">
-          <div />
-          <VStack gap="space-16">
-            {feilmeldingsdetaljer &&
-              Object.keys(feilmeldingsdetaljer).map(edKey => (
-                <div key={edKey}>
-                  <Detail>{`${capitalizeFirstLetters(edKey)}:`}</Detail>
-                  <div>
-                    {/* @ts-expect-error Fiks. Dette vil kreve at vi lager en rekursiv rendering av objectet */}
-                    <BodyShort size="small">{feilmeldingsdetaljer[edKey]}</BodyShort>
+    <Dialog open={skalViseModal} onOpenChange={lukkModal}>
+      <Dialog.Popup>
+        <Dialog.Header>
+          <Dialog.Title>
+            <HStack gap="space-16">
+              <ExclamationmarkTriangleFillIcon aria-hidden width={30} height={30} color="var(--ax-warning-600)" />
+              <Heading size="small" level="2">
+                <FormattedMessage id="FeilmeldingsdetaljerModal.ErrorDetails" />
+              </Heading>
+            </HStack>
+          </Dialog.Title>
+        </Dialog.Header>
+        <Dialog.Body>
+          <HStack gap="space-40">
+            <div />
+            <VStack gap="space-16">
+              {feilmeldingsdetaljer &&
+                Object.keys(feilmeldingsdetaljer).map(edKey => (
+                  <div key={edKey}>
+                    <Detail>{`${capitalizeFirstLetters(edKey)}:`}</Detail>
+                    <div>
+                      {/* @ts-expect-error Fiks. Dette vil kreve at vi lager en rekursiv rendering av objectet */}
+                      <BodyShort size="small">{feilmeldingsdetaljer[edKey]}</BodyShort>
+                    </div>
                   </div>
-                </div>
-              ))}
-          </VStack>
-        </HStack>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button size="small" variant="secondary" onClick={lukkModal} type="button">
-          <FormattedMessage id="FeilmeldingsdetaljerModal.Close" />
-        </Button>
-      </Modal.Footer>
-    </Modal>
+                ))}
+            </VStack>
+          </HStack>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Button size="small" variant="secondary" onClick={lukkModal} type="button">
+            <FormattedMessage id="FeilmeldingsdetaljerModal.Close" />
+          </Button>
+        </Dialog.Footer>
+      </Dialog.Popup>
+    </Dialog>
   );
 };
