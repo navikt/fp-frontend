7b4707e5fcc37b7d2d4e46eb6cef3b689f47d6c1
johannbm <johannes.moskvil@gmail.com>
2026-01-07 10:20:05 +0100

    wip

--- packages/fakta/uttaksdokumentasjon/src/components/DelOppPeriode/DelOppPeriodeModal.tsx
+++ packages/fakta/uttaksdokumentasjon/src/components/DelOppPeriode/DelOppPeriodeModal.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import { FormattedMessage, type IntlShape, useIntl } from 'react-intl';
 
 import { ScissorsIcon } from '@navikt/aksel-icons';
-import { BodyShort, Box, Button, Heading, HStack, Label, Modal, VStack } from '@navikt/ds-react';
+import { BodyShort, Box, Button, Dialog, HStack, Label, VStack } from '@navikt/ds-react';
 import { RhfDatepicker, RhfForm } from '@navikt/ft-form-hooks';
 import { hasValidDate, required } from '@navikt/ft-form-validators';
 import { PeriodLabel } from '@navikt/ft-ui-komponenter';
@@ -11,8 +11,6 @@ import dayjs from 'dayjs';
 
 import type { VurderingsBehovPeriode } from '../../types/FormValues';
 import { getFormatertPeriode, splitPeriodePåDato } from '../../utils/periodeUtils';
-
-import styles from './delOppPeriodeModal.module.css';
 
 const validerInnenforIntervall = (fom: string, tom: string, intl: IntlShape) => (dato: string) => {
   if (!dayjs(dato).isBefore(fom) && dayjs(dato).isBefore(tom)) {
@@ -36,70 +34,72 @@ export const DelOppPeriodeModal = ({ periode, cancel, submit }: Props) => {
   const perioder = splittDato ? splitPeriodePåDato(periode, splittDato) : null;
 
   return (
-    <RhfForm formMethods={formMethods} onSubmit={values => submit(values.dato)}>
-      <Modal
-        open
-        aria-label={intl.formatMessage({ id: 'DelOppPeriodeModal.Periode' })}
-        onClose={cancel}
-        className={styles['modal']}
-      >
-        <Modal.Header>
-          <Heading size="small" level="2">
-            <HStack gap="space-4" align="center">
-              <ScissorsIcon />
-              <FormattedMessage id="DelOppPeriodeModal.Tittel" />
-            </HStack>
-          </Heading>
-        </Modal.Header>
-        <Modal.Body>
-          <VStack gap="space-24">
-            <BodyShort>
-              <FormattedMessage id="DelOppPeriodeModal.Beskrivelse" />
-            </BodyShort>
-            <VStack gap="space-4">
-              <Label>
-                <FormattedMessage id="DelOppPeriodeModal.PeriodeLabel" />
-              </Label>
-              <PeriodLabel dateStringFom={periode.fom} dateStringTom={periode.tom} />
+    <Dialog open onOpenChange={cancel} size="small">
+      <Dialog.Popup width="small" closeOnOutsideClick={false}>
+        <RhfForm formMethods={formMethods} onSubmit={values => submit(values.dato)}>
+          <Dialog.Header>
+            <Dialog.Title>
+              <HStack gap="space-4" align="center">
+                <ScissorsIcon />
+                <FormattedMessage id="DelOppPeriodeModal.Tittel" />
+              </HStack>
+            </Dialog.Title>
+          </Dialog.Header>
+          <Dialog.Body>
+            <VStack gap="space-24">
+              <BodyShort>
+                <FormattedMessage id="DelOppPeriodeModal.Beskrivelse" />
+              </BodyShort>
+              <VStack gap="space-4">
+                <Label>
+                  <FormattedMessage id="DelOppPeriodeModal.PeriodeLabel" />
+                </Label>
+                <PeriodLabel dateStringFom={periode.fom} dateStringTom={periode.tom} />
+              </VStack>
+              <RhfDatepicker
+                name="dato"
+                control={formMethods.control}
+                size="medium"
+                description={intl.formatMessage({ id: 'DelOppPeriodeModal.DatePickerBeskrivelse' })}
+                label={<FormattedMessage id="DelOppPeriodeModal.DatePickerTittel" />}
+                validate={[required, hasValidDate, validerInnenforIntervall(periode.fom, periode.tom, intl)]}
+                fromDate={dayjs(periode.fom, ISO_DATE_FORMAT).toDate()}
+                toDate={dayjs(periode.tom, ISO_DATE_FORMAT).subtract(1, 'day').toDate()}
+              />
+              {perioder && (
+                <Box.New
+                  background="neutral-moderate"
+                  padding="2"
+                  borderRadius="large"
+                  style={{ width: 'max-content' }}
+                >
+                  <VStack gap="space-4">
+                    <Label>
+                      <FormattedMessage id="DelOppPeriodeModal.NyePeriodeLabel" />
+                    </Label>
+                    {perioder.map((p, index) => (
+                      <BodyShort key={p.fom}>
+                        <FormattedMessage
+                          id="UttakDokumentasjonFaktaDetailForm.PeriodeMedInnhold"
+                          values={{ index: index + 1, periode: getFormatertPeriode(p) }}
+                        />
+                      </BodyShort>
+                    ))}
+                  </VStack>
+                </Box.New>
+              )}
             </VStack>
-            <RhfDatepicker
-              name="dato"
-              control={formMethods.control}
-              size="medium"
-              description={intl.formatMessage({ id: 'DelOppPeriodeModal.DatePickerBeskrivelse' })}
-              label={<FormattedMessage id="DelOppPeriodeModal.DatePickerTittel" />}
-              validate={[required, hasValidDate, validerInnenforIntervall(periode.fom, periode.tom, intl)]}
-              fromDate={dayjs(periode.fom, ISO_DATE_FORMAT).toDate()}
-              toDate={dayjs(periode.tom, ISO_DATE_FORMAT).subtract(1, 'day').toDate()}
-            />
-            {perioder && (
-              <Box.New background="neutral-moderate" padding="2" borderRadius="large" style={{ width: 'max-content' }}>
-                <VStack gap="space-4">
-                  <Label>
-                    <FormattedMessage id="DelOppPeriodeModal.NyePeriodeLabel" />
-                  </Label>
-                  {perioder.map((p, index) => (
-                    <BodyShort key={p.fom}>
-                      <FormattedMessage
-                        id="UttakDokumentasjonFaktaDetailForm.PeriodeMedInnhold"
-                        values={{ index: index + 1, periode: getFormatertPeriode(p) }}
-                      />
-                    </BodyShort>
-                  ))}
-                </VStack>
-              </Box.New>
-            )}
-          </VStack>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button type="submit" variant="primary">
-            <FormattedMessage id="DelOppPeriodeModal.Submit" />
-          </Button>
-          <Button variant="secondary" onClick={cancel}>
-            <FormattedMessage id="DelOppPeriodeModal.Avbryt" />
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </RhfForm>
+          </Dialog.Body>
+          <Dialog.Footer>
+            <Button variant="secondary" onClick={cancel} type="button">
+              <FormattedMessage id="DelOppPeriodeModal.Avbryt" />
+            </Button>
+            <Button type="submit" variant="primary">
+              <FormattedMessage id="DelOppPeriodeModal.Submit" />
+            </Button>
+          </Dialog.Footer>
+        </RhfForm>
+      </Dialog.Popup>
+    </Dialog>
   );
 };
