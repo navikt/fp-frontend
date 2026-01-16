b8eeeda2d6d99bbe7c5d68861cb3cad18c2eb4b3
espenwaaga <espen.waaga@nav.no>
2026-01-16 12:31:17 +0100

    Legger også til muligheten til å velge tidsintervall på grafen. Gir nå 3 muligheter.

--- packages/los/felles/src/graf/AktiveOgTilgjengligeOppgaverGraf.tsx
+++ packages/los/felles/src/graf/AktiveOgTilgjengligeOppgaverGraf.tsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
+
+import { ToggleGroup } from '@navikt/ds-react';
+import { ToggleGroupItem } from '@navikt/ds-react/ToggleGroup';
+
 import { ReactECharts } from '../ReactECharts.tsx';
 import type { OppgaveFilterStatistikk } from '../typer/oppgaveFilterStatistikk.ts';
 
+type Tidsintervall = 'dag' | 'uke' | 'måned';
 
 interface Props {
   aktiveOgLedigeTidslinje: OppgaveFilterStatistikk[];
@@ -12,82 +18,124 @@ export const AktiveOgTilgjengligeOppgaverGraf = ({ aktiveOgLedigeTidslinje }: Pr
   //const påVentLabel = 'Behandlinger på vent';
   const reserverteLabel = 'Reserverte';
 
-  // Sorter oppgaver etter tidspunkt
-  const sortedOppgaver = [...aktiveOgLedigeTidslinje].sort(
+  const [tidsintervall, setTidsintervall] = useState<Tidsintervall>('dag');
+  const filtrertTidslinje = filtererTidslinjeBasertPåValgIntervall(aktiveOgLedigeTidslinje, tidsintervall);
+  const sortertOgFiltrertTidslinje = filtrertTidslinje.toSorted(
     (a, b) => new Date(a.tidspunkt).getTime() - new Date(b.tidspunkt).getTime(),
   );
-
   // Bruk sampling på tidspunkter og data
-  const granularitet = 50;
+  const granularitet = 20;
   const sampledTidspunkter = sampleData(
-    sortedOppgaver.map(o => o.tidspunkt),
+    sortertOgFiltrertTidslinje.map(o => o.tidspunkt),
     granularitet,
   );
   const sampledLedigeData = sampleData(
-    sortedOppgaver.map(o => o.aktiveLedige),
+    sortertOgFiltrertTidslinje.map(o => o.aktiveLedige),
     granularitet,
   );
   const sampledReserverteData = sampleData(
-    sortedOppgaver.map(o => o.aktive - o.aktiveLedige),
+    sortertOgFiltrertTidslinje.map(o => o.aktive - o.aktiveLedige),
     granularitet,
   );
 
   return (
-    <ReactECharts
-      key={`chart-${aktiveOgLedigeTidslinje.length}`}
-      height={height}
-      option={{
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'shadow',
-          },
-        },
-        legend: {
-          data: [aktivLabel, reserverteLabel],
-        },
-        xAxis: {
-          name: 'Tid',
-          type: 'category',
-          data: sampledTidspunkter,
-          boundaryGap: true,
-          axisLabel: {
-            formatter: (value: string) => {
-              const date = new Date(value);
-              return date.toLocaleTimeString('no-NO', {
-                hour: '2-digit',
-                minute: '2-digit',
-              });
+    <>
+      <ToggleGroup size="small" value={tidsintervall} onChange={value => setTidsintervall(value as Tidsintervall)}>
+        <ToggleGroupItem value="måned">Siste måned</ToggleGroupItem>
+        <ToggleGroupItem value="uke">Siste uke</ToggleGroupItem>
+        <ToggleGroupItem value="dag">Siste døgn</ToggleGroupItem>
+      </ToggleGroup>
+      <ReactECharts
+        key={`chart-${aktiveOgLedigeTidslinje.length}`}
+        height={height}
+        option={{
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'shadow',
             },
           },
-        },
-        yAxis: {
-          type: 'value',
-          name: 'Antall',
-          scale: true,
-        },
-        series: [
-          {
-            name: reserverteLabel,
-            type: 'line',
-            data: sampledReserverteData,
-            stack: 'total',
-            areaStyle: {},
+          legend: {
+            data: [aktivLabel, reserverteLabel],
           },
-          {
-            name: aktivLabel,
-            type: 'line',
-            data: sampledLedigeData,
-            stack: 'total',
-            areaStyle: {},
+          xAxis: {
+            name: tidsintervall === 'dag' ? 'Tid (hh:mm)' : 'Dato (dd.mm)',
+            type: 'category',
+            data: sampledTidspunkter,
+            boundaryGap: true,
+            axisLabel: {
+              formatter: (value: string) => {
+                const date = new Date(value);
+                if (tidsintervall === 'dag') {
+                  return date.toLocaleTimeString('no-NO', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  });
+                } else if (tidsintervall === 'uke') {
+                  return date.toLocaleDateString('no-NO', {
+                    weekday: 'short',
+                    day: '2-digit',
+                    month: '2-digit',
+                  });
+                } else {
+                  return date.toLocaleDateString('no-NO', {
+                    day: '2-digit',
+                    month: 'short',
+                  });
+                }
+              },
+            },
           },
-        ],
-        color: ['#38a161', '#9ad6bd', '#337c9b'],
-      }}
-    />
+          yAxis: {
+            type: 'value',
+            name: 'Antall',
+            scale: true,
+          },
+          series: [
+            {
+              name: reserverteLabel,
+              type: 'line',
+              data: sampledReserverteData,
+              stack: 'total',
+              areaStyle: {},
+            },
+            {
+              name: aktivLabel,
+              type: 'line',
+              data: sampledLedigeData,
+              stack: 'total',
+              areaStyle: {},
+            },
+          ],
+          color: ['#38a161', '#9ad6bd', '#337c9b'],
+        }}
+      />
+    </>
   );
 };
 
+const filtererTidslinjeBasertPåValgIntervall = (
+  aktiveOgLedigeTidslinje: OppgaveFilterStatistikk[],
+  timeRange: Tidsintervall,
+) => {
+  return aktiveOgLedigeTidslinje.filter(o => {
+    const oppgaveDate = new Date(o.tidspunkt);
+    const diffMs = Date.now() - oppgaveDate.getTime();
+    const diffHours = diffMs / (1000 * 60 * 60);
+    const diffDays = diffHours / 24;
+
+    switch (timeRange) {
+      case 'dag':
+        return diffHours <= 24;
+      case 'uke':
+        return diffDays <= 7;
+      case 'måned':
+        return diffDays <= 30;
+      default:
+        return true;
+    }
+  });
+};
 
 // Funksjon for å sample data ned til maks antall punkter
 const sampleData = (data: (string | number)[], maxPoints: number) => {
