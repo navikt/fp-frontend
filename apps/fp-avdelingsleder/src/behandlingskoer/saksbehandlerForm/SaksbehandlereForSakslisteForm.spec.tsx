5be3ab36b99fb5a7d4daa10d703301ad3763b82a
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2026-02-17 13:38:35 +0100

    TFP-6573: Legger til mulighet for å filtrere  på navn for reservasjoner + velge hele gruppen i behandlingskøer (#7063)

--- apps/fp-avdelingsleder/src/behandlingskoer/saksbehandlerForm/SaksbehandlereForSakslisteForm.spec.tsx
+++ apps/fp-avdelingsleder/src/behandlingskoer/saksbehandlerForm/SaksbehandlereForSakslisteForm.spec.tsx
@@ -1,5 +1,6 @@
 import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { applyRequestHandlers, type MswParameters } from 'msw-storybook-addon';
 
 import * as stories from './SaksbehandlereForSakslisteForm.stories';
@@ -29,10 +30,73 @@ describe('SaksbehandlereForSakslisteForm', () => {
     expect(screen.getByText('Saksbehandlere')).toBeInTheDocument();
     expect(screen.getByText('Gruppe 1')).toBeInTheDocument();
     expect(screen.getByText('Antall saksbehandlere')).toBeInTheDocument();
-    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(screen.getAllByText('1')).toHaveLength(3);
 
-    expect(screen.getAllByText('Steffen')).toHaveLength(2);
-    expect(screen.getAllByText('Espen Utvikler')).toHaveLength(2);
+    expect(screen.getAllByText('Steffen')).toHaveLength(4);
+    expect(screen.getAllByText('Espen Utvikler')).toHaveLength(4);
     expect(screen.getByText('Eirik')).toBeInTheDocument();
+  });
+
+  it('skal huke av alle saksbehandler i gruppe ved gruppe avhukning og fjerne ved avhuking', async () => {
+    applyRequestHandlers(SaksbehandlereSomErGruppert.parameters['msw'] as MswParameters['msw']);
+    render(<SaksbehandlereSomErGruppert />);
+
+    expect(await screen.findByText('Gruppenavn')).toBeInTheDocument();
+
+    const gruppeCheckbox = screen.getByRole('checkbox', { name: '1001-checkbox' });
+    await userEvent.click(gruppeCheckbox);
+
+    // Expander gruppe 1 for å vise medlemmene
+    const expandGruppeKnapp = screen.getByRole('cell', { name: /Gruppe 1/i });
+    await userEvent.click(expandGruppeKnapp);
+
+    // Verifiserer at alle medlemmer av Gruppe 1 er huket
+    expect(screen.getByRole('checkbox', { name: 'Steffen' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Espen Utvikler' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Anders Utvikler' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Jens-Otto Techlead' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Morgan Designer' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Tor på Spor' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Marte' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Kari Kanari' })).toBeChecked();
+
+    // Klikker gruppe-checkboxen andre gang for å avhuke alle
+    await userEvent.click(gruppeCheckbox);
+
+    // Verifiserer at alle medlemmer av Gruppe 1 er avhuket
+    expect(screen.getByRole('checkbox', { name: 'Steffen' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Espen Utvikler' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Anders Utvikler' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Jens-Otto Techlead' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Morgan Designer' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Tor på Spor' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Marte' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Kari Kanari' })).not.toBeChecked();
+  });
+
+  it('skal vise indeterminate tilstand på gruppe-checkbox når en eller flere er valgt, men ikke alle', async () => {
+    applyRequestHandlers(SaksbehandlereSomErGruppert.parameters['msw'] as MswParameters['msw']);
+    render(<SaksbehandlereSomErGruppert />);
+
+    expect(await screen.findByText('Gruppenavn')).toBeInTheDocument();
+    const ekspanderKnappForGruppe = screen.getByRole('cell', { name: 'Drammen' });
+    await userEvent.click(ekspanderKnappForGruppe);
+
+    const gruppeCheckbox = screen.getByRole('checkbox', { name: '1002-checkbox' });
+    expect(gruppeCheckbox).not.toBeChecked();
+    expect(gruppeCheckbox).not.toHaveAttribute('data-state', 'indeterminate');
+    expect(screen.getByRole('checkbox', { name: 'Kari Kanari' })).not.toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Marte' })).not.toBeChecked();
+
+    // Velger en av saksbehandlerene i gruppen
+    const førsteCheckboxIGruppen = screen.getByRole('checkbox', { name: 'Kari Kanari' });
+    await userEvent.click(førsteCheckboxIGruppen);
+
+    // Verifiserer at steffen er valgt, men ingen av de andre
+    expect(screen.getByRole('checkbox', { name: 'Kari Kanari' })).toBeChecked();
+    expect(screen.getByRole('checkbox', { name: 'Marte' })).not.toBeChecked();
+
+    // Verifiserer at gruppe-checkboxen er i indeterminate tilstand
+    expect(screen.getByRole<HTMLInputElement>('checkbox', { name: '1002-checkbox' }).indeterminate).toBeTruthy();
   });
 });
