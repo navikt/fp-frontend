eeabe746d4c6fdb108e9aa9e125b7dfc45a80e15
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-02-24 11:47:08 +0100

    TFP-6522 LOS avd.leder nøkkeltall behandling opprettet/avsluttet (#7091)
    * TFP-6522 LOS avd.leder nøkkeltall behandling opprettet/avsluttet
    
    * TFP-6522 farger
    
    * finpuss

--- apps/fp-avdelingsleder/src/reservasjoner/ReservasjonerTabell.tsx
+++ apps/fp-avdelingsleder/src/reservasjoner/ReservasjonerTabell.tsx
@@ -7,7 +7,7 @@ import { DateTimeLabel } from '@navikt/ft-ui-komponenter';
 import { useMutation, useQuery } from '@tanstack/react-query';
 
 import { FlyttReservasjonModal, OppgaveReservasjonEndringDatoModal } from '@navikt/fp-los-felles';
-import type { ReservasjonDto } from '@navikt/fp-types';
+import type { AvdelingReservasjonDto } from '@navikt/fp-types';
 
 import {
   endreReservasjon,
@@ -26,7 +26,7 @@ export const ReservasjonerTabell = ({ valgtAvdelingEnhet }: Props) => {
   const intl = useIntl();
   const [showReservasjonEndringDatoModal, setShowReservasjonEndringDatoModal] = useState(false);
   const [showFlyttReservasjonModal, setShowFlyttReservasjonModal] = useState(false);
-  const [valgtReservasjon, setValgtReservasjon] = useState<ReservasjonDto | undefined>(undefined);
+  const [valgtReservasjon, setValgtReservasjon] = useState<AvdelingReservasjonDto | undefined>(undefined);
   const [søketekst, setSøketekst] = useState('');
 
   const behandlingTyper = useLosKodeverk('BehandlingType');
@@ -53,12 +53,12 @@ export const ReservasjonerTabell = ({ valgtAvdelingEnhet }: Props) => {
     mutationFn: (valuesToStore: { brukerIdent: string }) => flyttReservasjonSaksbehandlerSøk(valuesToStore.brukerIdent),
   });
 
-  const showReservasjonEndringDato = (reservasjon: ReservasjonDto): void => {
+  const showReservasjonEndringDato = (reservasjon: AvdelingReservasjonDto): void => {
     setShowReservasjonEndringDatoModal(true);
     setValgtReservasjon(reservasjon);
   };
 
-  const showFlytteModal = (reservasjon: ReservasjonDto): void => {
+  const showFlytteModal = (reservasjon: AvdelingReservasjonDto): void => {
     setShowFlyttReservasjonModal(true);
     setValgtReservasjon(reservasjon);
   };
