b4dd7a9aff63425ae3f03425e1f29abd61f35082
Siri Mykland <siri.mykland@nav.no>
2026-03-09 15:14:02 +0100

    reverter query client decorator

--- packages/storybook-utils/decorators/withQueryClient.tsx
+++ packages/storybook-utils/decorators/withQueryClient.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 
 import { type ReactRenderer } from '@storybook/react';
-import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { action } from 'storybook/actions';
 import type { DecoratorFunction } from 'storybook/internal/types';
 
 export const withQueryClient: DecoratorFunction<ReactRenderer> = Story => {
@@ -16,14 +15,9 @@ export const withQueryClient: DecoratorFunction<ReactRenderer> = Story => {
               retry: false,
             },
           },
-          queryCache: new QueryCache({
-            onError: action('Query error'),
-          }),
-          mutationCache: new MutationCache({
-            onError: action('Mutation error'),
-          }),
         }),
     );
+
     useEffect(() => {
       // cleanup on unmount
       return () => {
