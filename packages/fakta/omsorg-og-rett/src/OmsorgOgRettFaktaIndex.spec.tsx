0af93e3a35e1469a1ed2fbbc454f1c8685898b72
Siri Mykland <siri.mykland@nav.no>
2026-04-29 09:59:14 +0200

    fiks role på radiogruppe i omsor og rett test

--- packages/fakta/omsorg-og-rett/src/OmsorgOgRettFaktaIndex.spec.tsx
+++ packages/fakta/omsorg-og-rett/src/OmsorgOgRettFaktaIndex.spec.tsx
@@ -161,7 +161,7 @@ describe('OmsorgOgRettFaktaIndex', () => {
     const bekreftOgFortsettKnapp = screen.queryByText('Bekreft og fortsett');
     expect(bekreftOgFortsettKnapp).not.toBeInTheDocument();
 
-    const radiogrupper = screen.getAllByRole('group');
+    const radiogrupper = screen.getAllByRole('radiogroup');
     expect(radiogrupper).toHaveLength(3);
     for (const rg of radiogrupper) {
       expect(rg).toHaveAttribute('aria-readonly', 'true');
@@ -192,7 +192,7 @@ describe('OmsorgOgRettFaktaIndex', () => {
     ).toBeInTheDocument();
     expect(screen.getByLabelText(/Mottar annen forelder uføretrygd, jf. § 14-14 tredje ledd?/)).toBeInTheDocument();
 
-    const radiogrupper = screen.getAllByRole('group');
+    const radiogrupper = screen.getAllByRole('radiogroup');
 
     expect(radiogrupper).toHaveLength(3);
     for (const rg of radiogrupper) {
