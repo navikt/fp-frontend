5e72734bcf79e03c3e4d5d8b4578138ebc3a3af1
Siri Mykland <35257015+sirimykland@users.noreply.github.com>
2026-05-05 15:05:53 +0200

    TFP-6963: gjør AP og overstyring componenter likerer i fakta-saken (#7281)
    * TFP-6963: gjør AP og overstyring componenter likerer i fakta-saken
    
    * oppdater intl og komponent navn
    
    * typo
    
    * skjul edit-knapper ved readonly
    
    * fjern unødvendig endring

--- knip.ts
+++ knip.ts
@@ -8,7 +8,7 @@ const config: KnipConfig = {
     'packages/types/src/fpsak.gen.ts',
     'packages/kodeverk/src/aksjonspunktCodes.ts',
   ],
-  ignoreDependencies: ['glob', '@editorjs/editorjs'],
+  ignoreDependencies: ['glob'],
 };
 
 export default config;
